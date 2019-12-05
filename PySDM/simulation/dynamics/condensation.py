c23b6a3b6dcf675fdc7da36b88fb870f6f6026f3
prbartman <prbartman@gmail.com>
2019-12-05 13:47:01 +0100

    use drhod_dt instead of rho in condensation

--- PySDM/simulation/dynamics/condensation.py
+++ PySDM/simulation/dynamics/condensation.py
@@ -14,44 +14,47 @@ from PySDM.backends.numba.numba import Numba
 import numpy as np
 import numba
 
-idx_qv = 0  # redundant
+idx_rhod = 0
 idx_thd = 1
-idx_rw = 2
+idx_qv = 2
+idx_rw = 3
 
 
 class _ODESystem:
-    def __init__(self, rhod, kappa, xd: np.ndarray, n: np.ndarray, dqv_dt, dthd_dt, m_d):
-        self.rhod = rhod
+    def __init__(self, kappa, xd: np.ndarray, n: np.ndarray, drhod_dt, dthd_dt, dqv_dt, m_d):
         self.kappa = kappa
         self.rd = phys.radius(volume=xd)
         self.n = n
         self.dqv_dt = dqv_dt
         self.dthd_dt = dthd_dt
+        self.drhod_dt = drhod_dt
         self.m_d = m_d
 
     def __call__(self, t, y):
+        rhod = y[idx_rhod]
         thd = y[idx_thd]
         qv = y[idx_qv]
         rw = y[idx_rw:]
 
-        T, p, RH = Numba.temperature_pressure_RH(self.rhod, thd, qv)
+        T, p, RH = Numba.temperature_pressure_RH(rhod, thd, qv)
 
         dy_dt = np.empty_like(y)
 
-        foo(dy_dt, rw, T, p, self.n, RH, self.kappa, self.rd, qv, self.dqv_dt, self.dthd_dt, self.m_d)
+        foo(dy_dt, rw, T, p, self.n, RH, self.kappa, self.rd, qv, self.drhod_dt, self.dthd_dt, self.dqv_dt, self.m_d)
 
         return dy_dt
 
 # TODO
 @numba.njit()
-def foo(dy_dt, rw, T, p, n, RH, kappa, rd, qv, dqv_dt, dthd_dt, m_d):
+def foo(dy_dt, rw, T, p, n, RH, kappa, rd, qv, dot_rhod, dot_thd, dot_qv, m_d):
+    dy_dt[idx_qv] = dot_qv
+    dy_dt[idx_thd] = dot_thd
+    dy_dt[idx_rhod] = dot_rhod
+
     for i in range(len(rw)):
         dy_dt[idx_rw + i] = phys.dr_dt_MM(rw[i], T, p, RH - 1, kappa, rd[i])
-    dy_dt[idx_qv] = -4 * np.pi * np.sum(n * rw ** 2 * dy_dt[idx_rw:]) * rho_w / m_d
-    dy_dt[idx_thd] = - phys.lv(T) * dy_dt[idx_qv] / phys.c_p(qv) * (p1000 / p) ** (Rd / c_pd)
-
-    dy_dt[idx_qv] += dqv_dt
-    dy_dt[idx_thd] += dthd_dt
+    dy_dt[idx_qv] += -4 * np.pi * np.sum(n * rw ** 2 * dy_dt[idx_rw:]) * rho_w / m_d
+    dy_dt[idx_thd] += - phys.lv(T) * dy_dt[idx_qv] / phys.c_p(qv) * (p1000 / p) ** (Rd / c_pd)
 
 
 def compute_cell_start(cell_start, cell_id, idx, sd_num):
@@ -101,18 +104,19 @@ class Condensation:
                 if n_sd_in_cell == 0:
                     continue
 
-                y0 = np.empty(n_sd_in_cell + 2)
+                y0 = np.empty(n_sd_in_cell + idx_rw)
+                y0[idx_rhod] = self.environment['rhod'][cell_id]
                 y0[idx_thd] = self.environment['thd'][cell_id]
                 y0[idx_qv] = self.environment['qv'][cell_id]
                 y0[idx_rw:] = phys.radius(volume=v[state.idx[cell_start:cell_end]])
                 integ = ode.solve_ivp(
                     _ODESystem(
-                        self.environment.get_predicted("rhod")[cell_id],
                         self.kappa,
                         vdry[state.idx[cell_start:cell_end]],
                         n[state.idx[cell_start:cell_end]],
-                        (self.environment.get_predicted('qv')[cell_id] - self.environment['qv'][cell_id]) / self.dt,
+                        (self.environment.get_predicted("rhod")[cell_id] - self.environment['rhod'][cell_id]) / self.dt,
                         (self.environment.get_predicted('thd')[cell_id] - self.environment['thd'][cell_id]) / self.dt,
+                        (self.environment.get_predicted('qv')[cell_id] - self.environment['qv'][cell_id]) / self.dt,
                         self.environment.get_predicted("rhod")[cell_id] * self.environment.dv
                     ),
                     (0., self.dt),
