e45689539395478cd9bec50c858b8cd3c69aaaea
prbartman <prbartman@gmail.com>
2020-02-18 11:06:12 +0100

    fixing RH/S mistmatch in wet radius init

--- PySDM/simulation/initialisation/r_wet_init.py
+++ PySDM/simulation/initialisation/r_wet_init.py
@@ -4,15 +4,15 @@ from PySDM.simulation.physics import formulae
 
 
 class _MinFun:
-    def __init__(self, T, p, S, kappa, r_d):
+    def __init__(self, T, p, RH, kappa, r_d):
         self.T = T
         self.p = p
-        self.S = S
+        self.RH = RH
         self.kappa = kappa
         self.r_d = r_d
 
     def __call__(self, r_w):
-        return formulae.dr_dt_MM(r_w, self.T, self.p, self.S, self.kappa, self.r_d)
+        return formulae.dr_dt_MM(r_w, self.T, self.p, self.RH, self.kappa, self.r_d)
 
 
 def r_wet_init(r_dry: np.ndarray, ambient_air, cell_id: np.ndarray, kappa):
@@ -27,7 +27,7 @@ def r_wet_init(r_dry: np.ndarray, ambient_air, cell_id: np.ndarray, kappa):
         f = _MinFun(
             ambient_air['T'][cid],
             ambient_air['p'][cid],
-            np.minimum(0, ambient_air['RH'][cid] - 1),
+            np.minimum(1, ambient_air['RH'][cid]),
             kappa,
             r_d
         )
