1afb44852119695543e99d6601d49373b60a05bc
prbartman <prbartman@gmail.com>
2020-05-03 14:28:38 +0200

    ThrustRTC For created only once in _algorithmic_step_methods.py & _maths_methods.py

--- PySDM/backends/thrustRTC/_maths_methods.py
+++ PySDM/backends/thrustRTC/_maths_methods.py
@@ -18,88 +18,100 @@ class MathsMethods:
     def add(output, addend):
         trtc.Transform_Binary(addend, output, output, trtc.Plus())
 
+    __column_modulo_body = trtc.For(['output', 'divisor', 'col_num'], "i", '''
+        int d = i % col_num;
+        output[i] %= divisor[d];
+        ''')
+
     @staticmethod
     def column_modulo(output, divisor):
-        loop = trtc.For(['output', 'divisor', 'col_num'], "i", '''
-                            int d = i % col_num;
-                            output[i] %= divisor[d];
-                        ''')
         col_num = trtc.DVInt64(divisor.size())
-        loop.launch_n(output.size(), [output, divisor, col_num])
+        MathsMethods.__column_modulo_body.launch_n(output.size(), [output, divisor, col_num])
+
+    __floor_body = trtc.For(['arr'], "i", '''
+        if (arr[i] >= 0) 
+            arr[i] = (long) arr[i];
+        else
+        {
+            auto old = arr[i];
+            arr[i] = (long) arr[i];
+            if (old != arr[i])
+                arr[i] -= 1;
+        }
+        ''')
 
     @staticmethod
     def floor(output):
-        loop = trtc.For(['arr'], "i", '''
-                    if (arr[i] >= 0) 
-                        arr[i] = (long) arr[i];
-                    else
-                    {
-                        auto old = arr[i];
-                        arr[i] = (long) arr[i];
-                        if (old != arr[i])
-                            arr[i] -= 1;
-                    }
-                ''')
-        loop.launch_n(output.size(), [output])
+        MathsMethods.__floor_body.launch_n(output.size(), [output])
+
+    __floor_out_of_place_body = trtc.For(['output', 'input_data'], "i", '''
+        if (input_data[i] >= 0) 
+            output[i] = (long) input_data[i];
+        else
+        {
+            output[i] = (long) input_data[i];
+            if (input_data[i] != output[i])
+                output[i] -= 1;
+        }
+        ''')
 
     @staticmethod
     def floor_out_of_place(output, input_data):
-        loop = trtc.For(['output', 'input_data'], "i", '''
-                            if (input_data[i] >= 0) 
-                                output[i] = (long) input_data[i];
-                            else
-                            {
-                                output[i] = (long) input_data[i];
-                                if (input_data[i] != output[i])
-                                    output[i] -= 1;
-                            }
-                        ''')
-        loop.launch_n(output.size(), [output, input_data])
+        MathsMethods.__floor_out_of_place_body.launch_n(output.size(), [output, input_data])
+
+    __multiply_elementwise_body = trtc.For(['output', 'multiplier'], "i", '''
+        output[i] *= multiplier[i];
+        ''')
+
+    __multiply_body = trtc.For(['output', 'multiplier'], "i", '''
+        output[i] *= multiplier;
+        ''')
 
     @staticmethod
     def multiply(output, multiplier):
         if isinstance(multiplier, StorageMethods.storage):
-            loop = trtc.For(['output', 'multiplier'], "i", "output[i] *= multiplier[i];")
+            loop = MathsMethods.__multiply_elementwise_body
             device_multiplier = multiplier
         elif isinstance(multiplier, float):
-            loop = trtc.For(['output', 'multiplier'], "i", "output[i] *= multiplier;")
+            loop = MathsMethods.__multiply_body
             device_multiplier = trtc.DVDouble(multiplier)
         elif isinstance(multiplier, int):
-            loop = trtc.For(['output', 'multiplier'], "i", "output[i] *= multiplier;")
+            loop = MathsMethods.__multiply_body
             device_multiplier = trtc.DVInt64(multiplier)
         else:
             raise NotImplementedError()
         loop.launch_n(output.size(), [output, device_multiplier])
 
+    __multiply_out_of_place_elementwise_body = trtc.For(['output', 'multiplicand', 'multiplier'], "i", '''
+        output[i] = multiplicand[i] * multiplier[i];
+        ''')
+
+    __multiply_out_of_place_body = trtc.For(['output', 'multiplicand', 'multiplier'], "i", '''
+            output[i] = multiplicand[i] * multiplier;
+            ''')
+
     @staticmethod
     def multiply_out_of_place(output, multiplicand, multiplier):
         if isinstance(multiplier, StorageMethods.storage):
-            loop = trtc.For(['output', 'multiplicand', 'multiplier'], "i",
-                            "output[i] = multiplicand[i] * multiplier[i];")
+            loop = MathsMethods.__multiply_out_of_place_elementwise_body
             device_multiplier = multiplier
         elif isinstance(multiplier, float):
-            loop = trtc.For(['output', 'multiplicand', 'multiplier'], "i",
-                            "output[i] = multiplicand[i] * multiplier;")
+            loop = MathsMethods.__multiply_out_of_place_body
             device_multiplier = trtc.DVDouble(multiplier)
         else:
             raise NotImplementedError()
         loop.launch_n(output.size(), [output, multiplicand, device_multiplier])
 
+    __power_body = trtc.For(['output', 'exponent'], "i", '''
+        output[i] = pow(output[i], exponent);
+        ''')
+
     @staticmethod
     def power(output, exponent):
-        if isinstance(exponent, float):
-            if exponent == 1.:
-                return
-            loop = trtc.For(['output', 'exponent'], "i", "output[i] = pow(output[i], exponent);")
-            device_multiplier = trtc.DVDouble(exponent)
-        elif isinstance(exponent, int):
-            if exponent == 1:
-                return
-            loop = trtc.For(['output', 'exponent'], "i", "output[i] = pow(output[i], exponent);")
-            device_multiplier = trtc.DVDouble(exponent)
-        else:
-            raise NotImplementedError()
-        loop.launch_n(output.size(), [output, device_multiplier])
+        if exponent == 1:
+            return
+        device_multiplier = trtc.DVDouble(exponent)
+        MathsMethods.__power_body.launch_n(output.size(), [output, device_multiplier])
 
     @staticmethod
     def subtract(output, subtrahend):
@@ -109,7 +121,7 @@ class MathsMethods:
     def urand(data, seed=None):
         # TODO: print("Numpy import!: ThrustRTC.urand(...)")
 
-        # np.random.seed(seed)
+        np.random.seed(seed)
         output = np.random.uniform(0, 1, data.shape)
         StorageMethods.upload(output, data)
 
