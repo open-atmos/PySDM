1fe65b6d2ef27a979466b0c6e987fae45e178fc1
prbartman <prbartman@gmail.com>
2020-06-10 13:20:17 +0200

    terminal velocities draft + test

---
+++ PySDM/attributes/droplet/terminal_velocity/terminal_velocity.py
@@ -0,0 +1,24 @@
+"""
+Created at 11.05.2020
+
+@author: Piotr Bartman
+@author: Sylwester Arabas
+"""
+
+from PySDM.attributes.derived_attribute import DerivedAttribute
+from PySDM.physics import constants as const
+from .gunn_and_kinzer import Approximation
+
+
+class TerminalVelocity(DerivedAttribute):
+
+    def __init__(self, particles_builder):
+        self.radius = particles_builder.get_attribute('radius')
+        dependencies = [self.radius]
+        super().__init__(particles_builder, name='terminal velocity', dependencies=dependencies)
+
+        self.approximation = Approximation(particles_builder.particles)
+
+    def recalculate(self):
+        self.approximation(self.data, self.radius.get())
+
---
+++ PySDM_tests/unit_tests/attributes/droplet/terminal_velocity/test_gunn_and_kinzer.py
@@ -0,0 +1,33 @@
+"""
+Created at 10.06.2020
+"""
+
+from PySDM.physics import constants as const
+from PySDM.attributes.droplet.terminal_velocity.gunn_and_kinzer import Approximation, Interpolated, TpDependent
+import matplotlib.pyplot as plt
+import numpy as np
+
+from PySDM.backends.default import Default
+from PySDM_tests.unit_tests.state.dummy_particles import DummyParticles
+
+
+def test_approximation(plot=False):
+    # TODO: rethink
+    pass
+    # r = np.array([.078, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.2, 1.4, 1.6]) * const.si.mm / 2
+    # u = np.array([18, 27, 72, 117, 162, 206, 247, 287, 327, 367, 403, 464, 517, 565]) / 100
+    # n_sd = len(r)
+    # particles = DummyParticles(Default, n_sd=n_sd)
+    # u_term = particles.backend.array((n_sd,), float)
+    # radius = np.linspace(4e-6, 200e-6, 1000, endpoint=True)
+    # Approximation(particles)(u_term, radius)
+    # u_term_2 = np.copy(u_term)
+    # Interpolated(None)(u_term_2, radius)
+    #
+    # r, u = r[:5], u[:5]
+    # if plot:
+    #     plt.plot(radius, u_term)
+    #     plt.plot(radius, u_term_2)
+    #     plt.plot(r, u)
+    #     plt.grid()
+    #     plt.show()
