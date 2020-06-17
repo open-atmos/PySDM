f007e553a92e5d8aa251776db82bd6ab96b32f87
Aleksandra Talar <aleksandra.talar@student.uj.edu.pl>
2020-06-17 21:35:43 +0200

    Titan constants Displacement product Meter unit in Fake Units Constants for different planets

--- PySDM_examples/Toon_et_al_1980_Fig_1/const.py
+++ PySDM_examples/Toon_et_al_1980_Fig_1/const.py
@@ -7,12 +7,48 @@ si = pint.UnitRegistry()
 if not DIMENSIONAL_ANALYSIS:
     si = FakeUnitRegistry(si)
 
-def g(z):
+'''def g(z):
     return g0 * (Rg / (Rg + z))**2
 
 g0 = 1.17 * si.centimeter / si.second **2
 Rg = 2.8 * 10**8 * si.centimeter
 R_str = sci.R * si.joule / si.kelvin / si.mole
 
-u = 16 * si.kilogram / si.mol
-Rd = R_str / u
+u = 16 * si.gram / si.mol
+Rd = R_str / u'''
+# ================== NOWE =====================================
+
+
+class PlanetData():
+    def __init__(self, g0, Rg, Rd):
+        self.g0 = g0
+        self.Rg = Rg
+        self.Rd = Rd
+
+    def g(self, z):
+        acc = self.g0 * (self.Rg / (self.Rg + z)) ** 2
+        return acc
+
+
+class Planets():
+    def __init__(self):
+        self.__add_earth()
+        self.__add_titan()
+
+    def __add_earth(self):
+        "Dane dla Ziemi"
+        self.Earth = PlanetData(1, 2, 3)
+
+    def __add_titan(self):
+        g0 = 1.17 * si.centimeter / si.second ** 2
+        Rg = 2.8 * 10 ** 8 * si.centimeter
+        R_str = sci.R * si.joule / si.kelvin / si.mole
+        u = 16 * si.gram / si.mol
+        Rd = R_str / u
+        self.Titan = PlanetData(g0, Rg, Rd)
+
+
+#const2 = Planets()
+#print(const2.Titan.Rd)
+# print(const.Earth.R)
+
