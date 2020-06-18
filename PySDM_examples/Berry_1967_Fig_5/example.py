30ecaf0bdb9e77d42c379f8def73086fb2b28c9a
prbartman <prbartman@gmail.com>
2020-06-18 23:14:58 +0200

    make berry example works with adaptive steps

--- PySDM_examples/Berry_1967_Fig_5/example.py
+++ PySDM_examples/Berry_1967_Fig_5/example.py
@@ -1,8 +1,5 @@
 """
 Created at 08.08.2019
-
-@author: Piotr Bartman
-@author: Sylwester Arabas
 """
 
 import numpy as np
@@ -11,11 +8,11 @@ from PySDM.particles_builder import ParticlesBuilder
 from PySDM.environments import Box
 from PySDM.dynamics import Coalescence
 from PySDM.initialisation.spectral_sampling import constant_multiplicity
-from PySDM.dynamics.coalescence.kernels import Golovin, Gravitational
-
+from PySDM.dynamics.coalescence.kernels import Gravitational
 from PySDM_examples.Berry_1967_Fig_5.setup import Setup
 from PySDM_examples.Berry_1967_Fig_5.spectrum_plotter import SpectrumPlotter
 from PySDM.state.products.particles_volume_spectrum import ParticlesVolumeSpectrum
+from PySDM.attributes.droplet.terminal_velocity import gunn_and_kinzer
 
 
 def run(setup):
@@ -27,7 +24,8 @@ def run(setup):
                                                                   (setup.init_x_min, setup.init_x_max))
     products = {ParticlesVolumeSpectrum: {}}
     particles = particles_builder.get_particles(attributes, products)
-    particles.state.whole_attributes['terminal velocity'].atype = setup.atype
+    particles.state.whole_attributes['terminal velocity'].approximation = setup.u_term_approx(particles)
+    particles.dynamics[str(Coalescence)].adaptive = setup.adaptive
 
     vals = {}
     for step in setup.steps:
@@ -39,57 +37,60 @@ def run(setup):
 
 
 def main(plot: bool):
-    with np.errstate(all='raise'):
+    with np.errstate(all='ignore'):
 
-        atypes = ('inter',)
-        dts = (1,)
+        u_term_approxs = (gunn_and_kinzer.Interpolation,)
+        dts = (1, 10, 'adaptive',)
         setups = {}
 
-        for atype in atypes:
-            setups[atype] = {}
+        for u_term_approx in u_term_approxs:
+            setups[u_term_approx] = {}
             for dt in dts:
-                setups[atype][dt] = {}
+                setups[u_term_approx][dt] = {}
 
                 sweepout = Setup()
-                sweepout.atype = atype
-                sweepout.dt = dt
+                sweepout.u_term_approx = u_term_approx
+                sweepout.dt = 10 if dt == 'adaptive' else dt
+                sweepout.adaptive = dt == 'adaptive'
                 sweepout.kernel = Gravitational(collection_efficiency=1)
                 sweepout.steps = [0, 100, 200, 300, 400, 500, 600, 700, 750, 800, 850]
                 sweepout.steps = [int(step/sweepout.dt) for step in sweepout.steps]
-                setups[atype][dt]['sweepout'] = sweepout
+                setups[u_term_approx][dt]['sweepout'] = sweepout
 
                 electro = Setup()
-                electro.atype = atype
-                electro.dt = dt
+                electro.u_term_approx = u_term_approx
+                electro.dt = 10 if dt == 'adaptive' else dt
+                electro.adaptive = dt == 'adaptive'
                 electro.kernel = Gravitational(collection_efficiency='3000V/cm')
                 electro.steps = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
                 electro.steps = [int(step / electro.dt) for step in electro.steps]
-                setups[atype][dt]['3000V/cm'] = electro
+                setups[u_term_approx][dt]['3000V/cm'] = electro
 
                 hydro = Setup()
-                hydro.atype = atype
-                hydro.dt = dt
+                hydro.u_term_approx = u_term_approx
+                hydro.dt = 10 if dt == 'adaptive' else dt
+                hydro.adaptive = dt == 'adaptive'
                 hydro.kernel = Gravitational(collection_efficiency='hydrodynamic')
                 hydro.steps = [1600, 1800, 2000, 2200]
                 hydro.steps = [int(step / hydro.dt) for step in hydro.steps]
-                setups[atype][dt]['hydrodynamic'] = hydro
+                setups[u_term_approx][dt]['hydrodynamic'] = hydro
 
         states = {}
-        for atype in setups:
-            states[atype] = {}
-            for dt in setups[atype]:
-                states[atype][dt] = {}
-                for kernel in setups[atype][dt]:
-                    states[atype][dt][kernel] = run(setups[atype][dt][kernel])[0]
+        for u_term_approx in setups:
+            states[u_term_approx] = {}
+            for dt in setups[u_term_approx]:
+                states[u_term_approx][dt] = {}
+                for kernel in setups[u_term_approx][dt]:
+                    states[u_term_approx][dt][kernel] = run(setups[u_term_approx][dt][kernel])[0]
 
-    with np.errstate(invalid='ignore'):
-        for atype in setups:
-            for dt in setups[atype]:
-                for kernel in setups[atype][dt]:
-                    plotter = SpectrumPlotter(setups[atype][dt][kernel])
-                    for step, vals in states[atype][dt][kernel].items():
-                        plotter.plot(vals, step * setups[atype][dt][kernel].dt)
-                    plotter.show(title=f"{atype[:3]}{dt}{kernel[:3]}", legend=True)
+    if plot:
+        for u_term_approx in setups:
+            for dt in setups[u_term_approx]:
+                for kernel in setups[u_term_approx][dt]:
+                    plotter = SpectrumPlotter(setups[u_term_approx][dt][kernel])
+                    for step, vals in states[u_term_approx][dt][kernel].items():
+                        plotter.plot(vals, step * setups[u_term_approx][dt][kernel].dt)
+                    plotter.show(title=f"{u_term_approx.__name__} {dt} {kernel}", legend=True)
 
 
 if __name__ == '__main__':
