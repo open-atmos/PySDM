bfd9b9c8910130e586c57138e7d4ba7117ab6ad8
prbartman <prbartman@gmail.com>
2020-07-03 22:21:49 +0200

    maximum adaptive time step in condensation and fixed full test_just_do_it.py

--- PySDM_tests/smoke_tests/Arabas_and_Shima_2017_Fig_5/test_just_do_it.py
+++ PySDM_tests/smoke_tests/Arabas_and_Shima_2017_Fig_5/test_just_do_it.py
@@ -1,3 +1,7 @@
+"""
+Created at 2019
+"""
+
 from PySDM_examples.Arabas_and_Shima_2017_Fig_5.simulation import Simulation
 from PySDM_examples.Arabas_and_Shima_2017_Fig_5.setup import setups
 from PySDM_tests.smoke_tests.utils import bdf
@@ -5,11 +9,12 @@ from PySDM_tests.smoke_tests.utils import bdf
 import pytest
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 
-rtols = [1e-6, 1e-7]
+rtols = [1e-3, 1e-7]
 schemes = ['default', 'BDF']
-setups_num = len(setups)-4
+setups_num = len(setups)
 
 
 @pytest.fixture(scope='module')
@@ -33,28 +38,31 @@ def data():
                 simulation = Simulation(setup)
                 if scheme == 'BDF':
                     bdf.patch_particles(simulation.particles, setup.coord, rtol=1e-4)
-                data[scheme][rtol].append(simulation.run())
+                results = simulation.run()
+                data[scheme][rtol].append(results)
     return data
 
 
-# @pytest.mark.skip
-def test_plot(data, plot=False):
-    if not plot:
-        return
-    fig, axs = plt.subplots(len(setups)-4, len(rtols),
-                            sharex=True, sharey=True, figsize=(6, 15))
-    for setup_idx in range(setups_num):
-        for rtol_idx in range(len(rtols)):
-            ax = axs[setup_idx, rtol_idx]
-            for scheme in schemes:
-                datum = data[scheme][rtols[rtol_idx]][setup_idx]
-                S = datum['S']
-                z = datum['z']
-                ax.plot(S, z, label=scheme)
-            ax.set_title(f"setup: {setup_idx}; rtol: {rtols[rtol_idx]}")
-            ax.set_xlim(-.0075, .0075)
-    ax.legend()
-    plt.show()
+def test_plot(data, plot=False, save=False):
+    if plot:
+        fig, axs = plt.subplots(setups_num, len(rtols),
+                                sharex=True, sharey=True, figsize=(10, 15))
+        for setup_idx in range(setups_num):
+            for rtol_idx in range(len(rtols)):
+                ax = axs[setup_idx, rtol_idx]
+                for scheme in schemes:
+                    datum = data[scheme][rtols[rtol_idx]][setup_idx]
+                    S = datum['S']
+                    z = datum['z']
+                    ax.plot(S, z, label=scheme, color='Black' if scheme == 'BDF' else 'grey')
+                _rtol = '$r_{tol}$'
+                ax.set_title(f"setup: {setup_idx}; {_rtol}: {rtols[rtol_idx]}")
+                ax.set_xlim(-7.5e-3, 7.5e-3)
+                ax.get_xaxis().set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+                plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+        if save:
+            plt.savefig('ADAPTIVEvsBDF.pdf', format='pdf')
+        plt.show()
 
 
 def split(arg1, arg2):
