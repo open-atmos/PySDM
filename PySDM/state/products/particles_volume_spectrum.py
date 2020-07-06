41d086b7186f83d0e483f7410a53bc11dfafba1a
prbartman <prbartman@gmail.com>
2020-07-06 13:04:02 +0200

    fixed particles_volume_spectrum.py

--- PySDM/state/products/particles_volume_spectrum.py
+++ PySDM/state/products/particles_volume_spectrum.py
@@ -11,17 +11,19 @@ class ParticlesVolumeSpectrum(MomentProduct):
 
     def __init__(self):
         super().__init__(
-            core=particles_builder.core,
-            shape=particles_builder.core.mesh.grid,
             name='dv/dlnr',
             unit='1/(unit dr/r)',
             description='Particles volume distribution',
             scale='linear',
             range=[20, 50]
         )
-        self.moment_0 = particles_builder.core.backend.Storage.empty(1, dtype=int)
-        self.moments = particles_builder.core.backend.Storage.empty((1, 1), dtype=float)
+        self.moment_0 = None
+        self.moments = None
 
+    def register(self, builder):
+        super().register(builder)
+        self.moment_0 = builder.core.backend.Storage.empty(1, dtype=int)
+        self.moments = builder.core.backend.Storage.empty((1, 1), dtype=float)
 
     def get(self, radius_bins_edges):
         volume_bins_edges = phys.volume(radius_bins_edges)
@@ -33,5 +35,5 @@ class ParticlesVolumeSpectrum(MomentProduct):
             self.download_moment_to_buffer(attr='volume', rank=0,
                                            attr_range=(volume_bins_edges[i], volume_bins_edges[i + 1]))
             vals[i] *= self.buffer[0]
-        vals *= 1 / np.diff(np.log(radius_bins_edges)) / self.particles.mesh.dv
+        vals *= 1 / np.diff(np.log(radius_bins_edges)) / self.core.mesh.dv
         return vals
