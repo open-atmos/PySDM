5901934e60c01369dcba3182418a9e2131e22496
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-09-07 00:01:29 +0200

    Update setup.py

--- PySDM_examples/Shima_et_al_2009_Fig_2/setup.py
+++ PySDM_examples/Shima_et_al_2009_Fig_2/setup.py
@@ -5,7 +5,7 @@ Created at 08.08.2019
 import numpy as np
 from PySDM.initialisation.spectra import Exponential
 from PySDM.dynamics.coalescence.kernels import Golovin
-from PySDM.backends import Default
+from PySDM.backends import CPU
 from PySDM.physics.constants import si
 from PySDM.physics import formulae as phys
 
@@ -35,4 +35,4 @@ class SetupA:
 
     radius_bins_edges = np.logspace(np.log10(10 * si.um), np.log10(5e3 * si.um), num=128, endpoint=True)
 
-    backend = Default
+    backend = CPU
