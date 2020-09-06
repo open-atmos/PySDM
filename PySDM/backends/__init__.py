112bf800dd736fab2a797c0d9b4a06ab470bf8f6
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-09-06 10:54:08 +0200

    fixing lack of GPU definition if ThrustRTC import failed + refactor

--- PySDM/backends/__init__.py
+++ PySDM/backends/__init__.py
@@ -3,11 +3,12 @@ Created at 24.07.2019
 """
 
 from .numba.numba import Numba
-from .numba.numba import Numba as CPU
 
 try:
     from .thrustRTC.thrustRTC import ThrustRTC
-    from .thrustRTC.thrustRTC import ThrustRTC as GPU
 except ImportError:
     class ThrustRTC:
         ENABLE = False
+   
+CPU = Numba
+GPU = ThrustRTC
