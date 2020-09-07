62c29ea42893560aa8daf686e3b3ed9e8ec148c3
prbartman <prbartman@gmail.com>
2020-09-07 12:10:05 +0200

    removing mpdata and threads logic from env kinematic 2d

--- PySDM/environments/kinematic_2d/mpdata.py
+++ PySDM/environments/kinematic_2d/mpdata.py
@@ -1,37 +1,64 @@
 """
 Created at 04.09.2020
 """
+
 import numpy as np
+from threading import Thread
 from MPyDATA import Options, Stepper, VectorField, ScalarField, Solver
 from MPyDATA.arakawa_c.boundary_condition.periodic_boundary_condition import PeriodicBoundaryCondition
 
 
-def make_advection_solver(*, grid, dt, field_values, g_factor: np.ndarray, rho_times_courant,
-                          mpdata_iters, mpdata_infinite_gauge, mpdata_flux_corrected_transport,
-                          mpdata_third_order_terms, mpdata_n_threads):
-    options = Options(
-        n_iters=mpdata_iters,
-        infinite_gauge=mpdata_infinite_gauge,
-        flux_corrected_transport=mpdata_flux_corrected_transport,
-        third_order_terms=mpdata_third_order_terms
-    )
-    stepper = Stepper(options=options, grid=grid, non_unit_g_factor=True, n_threads=mpdata_n_threads)
+class MPDATA:
+    def __init__(self, *, grid, dt, field_values, g_factor: np.ndarray, advector,
+                 mpdata_iters, mpdata_infinite_gauge, mpdata_flux_corrected_transport,
+                 mpdata_third_order_terms):
 
-    # CFL condition
-    for d in range(len(rho_times_courant)):
-        np.testing.assert_array_less(np.abs(rho_times_courant[d]), 1)
+        self.asynchronous = True
+        self.thread: (Thread, None) = None
 
-    advector = VectorField(rho_times_courant, halo=options.n_halo, boundary_conditions=(PeriodicBoundaryCondition(), PeriodicBoundaryCondition()))
+        options = Options(
+            n_iters=mpdata_iters,
+            infinite_gauge=mpdata_infinite_gauge,
+            flux_corrected_transport=mpdata_flux_corrected_transport,
+            third_order_terms=mpdata_third_order_terms
+        )
+        stepper = Stepper(options=options, grid=grid, non_unit_g_factor=True)
 
-    # nondivergence (of velocity field, hence dt)
-    assert np.amax(abs(advector.div((dt, dt)).get())) < 5e-9
+        # CFL condition
+        for d in range(len(advector)):
+            np.testing.assert_array_less(np.abs(advector[d]), 1)
 
-    g_factor = ScalarField(g_factor.astype(dtype=options.dtype), halo=options.n_halo,
-                           boundary_conditions=(PeriodicBoundaryCondition(), PeriodicBoundaryCondition()))
-    mpdatas = {}
-    for k, v in field_values.items():
-        advectee = ScalarField(np.full(grid, v, dtype=options.dtype), halo=options.n_halo,
+        self.advector = advector
+        advector_impl = VectorField(advector, halo=options.n_halo,
+                                    boundary_conditions=(PeriodicBoundaryCondition(), PeriodicBoundaryCondition()))
+
+        # nondivergence (of velocity field, hence dt)
+        assert np.amax(abs(advector_impl.div((dt, dt)).get())) < 5e-9
+
+        self.g_factor = g_factor
+        g_factor_impl = ScalarField(g_factor.astype(dtype=options.dtype), halo=options.n_halo,
                                boundary_conditions=(PeriodicBoundaryCondition(), PeriodicBoundaryCondition()))
-        mpdatas[k] = Solver(stepper=stepper, advectee=advectee, advector=advector, g_factor=g_factor)
+        self.mpdatas = {}
+        for k, v in field_values.items():
+            advectee = ScalarField(np.full(grid, v, dtype=options.dtype), halo=options.n_halo,
+                                   boundary_conditions=(PeriodicBoundaryCondition(), PeriodicBoundaryCondition()))
+            self.mpdatas[k] = Solver(stepper=stepper, advectee=advectee, advector=advector_impl, g_factor=g_factor_impl)
 
-    return advector, mpdatas
+    def __getitem__(self, item):
+        return self.mpdatas[item]
+
+    def __call__(self):
+        if self.asynchronous:
+            self.thread = Thread(target=self.step, args=())
+            self.thread.start()
+        else:
+            self.step()
+
+    def wait(self):
+        if self.asynchronous:
+            if self.thread is not None:
+                self.thread.join()
+
+    def step(self):
+        for mpdata in self.mpdatas.values():
+            mpdata.advance(1)
--- PySDM/environments/moist_eulerian_2d_kinematic.py
+++ PySDM/environments/moist_eulerian_2d_kinematic.py
@@ -2,76 +2,39 @@
 Created at 06.11.2019
 """
 
-from threading import Thread  # TODO
-
-import numpy as np
-
 from ._moist_eulerian import _MoistEulerian
 from PySDM.mesh import Mesh
 from .kinematic_2d.arakawa_c import nondivergent_vector_field_2d, make_rhod, courant_field
-from .kinematic_2d.mpdata import make_advection_solver
 
 
 class MoistEulerian2DKinematic(_MoistEulerian):
-    def __init__(self, dt, grid, size, stream_function, field_values, rhod_of,
-                 mpdata_iters, mpdata_iga, mpdata_fct, mpdata_tot):
+    def __init__(self, dt, grid, size, rhod_of, eulerian_advection_solvers):
         super().__init__(dt, Mesh(grid, size), [])
 
         self.rhod_of = rhod_of
-
-        grid = self.mesh.grid
-        self.rhod = make_rhod(grid, rhod_of)
-        self.rho_times_courant = nondivergent_vector_field_2d(grid, size, dt, stream_function)
-        self.advector, self.mpdatas = make_advection_solver(
-            grid=self.mesh.grid, dt=self.dt,
-            field_values=dict((key, np.full(grid, value)) for key, value in field_values.items()),
-            g_factor=self.rhod,
-            rho_times_courant=self.rho_times_courant,
-            mpdata_iters=mpdata_iters,
-            mpdata_infinite_gauge=mpdata_iga,
-            mpdata_flux_corrected_transport=mpdata_fct,
-            mpdata_third_order_terms=mpdata_tot
-        )
-
-        self.asynchronous = False
-        self.thread: (Thread, None) = None
+        self.eulerian_advection_solvers = eulerian_advection_solvers
 
     def register(self, builder):
         super().register(builder)
-        rhod = builder.core.Storage.from_ndarray(self.rhod.ravel())
+        rhod = builder.core.Storage.from_ndarray(self.eulerian_advection_solvers.g_factor.ravel())
         self._values["current"]["rhod"] = rhod
         self._tmp["rhod"] = rhod
-        delattr(self, 'rhod')  # TODO
 
         super().sync()
         self.notify()
 
     def _get_thd(self):
-        return self.mpdatas['th'].advectee.get()
+        return self.eulerian_advection_solvers['th'].advectee.get()
 
     def _get_qv(self):
-        return self.mpdatas['qv'].advectee.get()
-
-    def mpdata_step(self):
-        for mpdata in self.mpdatas.values():
-            mpdata.advance(1)
+        return self.eulerian_advection_solvers['qv'].advectee.get()
 
     def step(self):
-        if self.asynchronous:
-            self.thread = Thread(target=self.mpdata_step, args=())
-            self.thread.start()
-        else:
-            self.mpdata_step()
-
-    def wait(self):
-        if self.asynchronous:
-            if self.thread is not None:
-                self.thread.join()
+        self.eulerian_advection_solvers()
 
     def sync(self):
-        self.wait()
+        self.eulerian_advection_solvers.wait()
         super().sync()
 
     def get_courant_field_data(self):
-        return courant_field(self.rhod_times_courant, self.rhod_of, self.core.mesh.grid)
-
+        return courant_field(self.eulerian_advection_solvers.advector, self.rhod_of, self.core.mesh.grid)
