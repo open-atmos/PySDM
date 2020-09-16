c69110b2de836bc08fdd8a00763765f2a1fe6e01
prbartman <prbartman@gmail.com>
2020-09-16 12:18:18 +0200

    fixed GPU import

--- PySDM/backends/__init__.py
+++ PySDM/backends/__init__.py
@@ -3,9 +3,9 @@ Created at 24.07.2019
 """
 
 from .numba.numba import Numba
-import numba
+from numba import cuda
 
-if numba.cuda.is_available():
+if cuda.is_available():
     from .thrustRTC.thrustRTC import ThrustRTC
 else:
     class ThrustRTC:
