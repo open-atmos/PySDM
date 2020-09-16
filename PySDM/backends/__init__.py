b0fa295f49dcc940717439da00f92f8d07e507da
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-09-16 09:09:08 +0000

    avoiding using exceptions for standard control flow and supressing Thrust stderr warnings

--- PySDM/backends/__init__.py
+++ PySDM/backends/__init__.py
@@ -3,10 +3,11 @@ Created at 24.07.2019
 """
 
 from .numba.numba import Numba
+import numba
 
-try:
+if numba.cuda.is_available():
     from .thrustRTC.thrustRTC import ThrustRTC
-except ImportError:
+else:
     class ThrustRTC:
         ENABLE = False
    
