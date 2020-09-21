30e5422abdb42f41d3e17a5c097d05addc8d67f1
prbartman <prbartman@gmail.com>
2020-09-21 09:31:36 +0200

    refactor of spatial sampling

--- PySDM/initialisation/spatial_sampling.py
+++ PySDM/initialisation/spatial_sampling.py
@@ -2,22 +2,18 @@
 Created at 24.10.2019
 """
 
-# http://ww2.ii.uj.edu.pl/~arabas/workshop_2019/files/talk_Shima.pdf
-
 import numpy as np
 
-
-def grid(grid, n_sd):
-    raise NotImplementedError()
-
-
-def pseudorandom(grid, n_sd):
-    dimension = len(grid)
-    positions = np.random.rand(dimension, n_sd)
-    for dim in range(dimension):
-        positions[dim, :] *= grid[dim]
-    return positions
+# TODO QUASIRANDOM & GRID
+#  http://ww2.ii.uj.edu.pl/~arabas/workshop_2019/files/talk_Shima.pdf
 
 
-def quasirandom(grid, n_sd):
-    raise NotImplementedError()
+class Pseudorandom:
+
+    @staticmethod
+    def sample(grid, n_sd):
+        dimension = len(grid)
+        positions = np.random.rand(dimension, n_sd)
+        for dim in range(dimension):
+            positions[dim, :] *= grid[dim]
+        return positions
