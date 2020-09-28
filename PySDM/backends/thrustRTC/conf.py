3d9609a5d1c1bb13fd0447a764ed828ed458938e
prbartman <prbartman@gmail.com>
2020-09-28 14:48:23 +0200

    fixed imports in conf

--- PySDM/backends/thrustRTC/conf.py
+++ PySDM/backends/thrustRTC/conf.py
@@ -4,14 +4,16 @@ Created at 20.05.2020
 
 from PySDM.backends.thrustRTC.fakeThrustRTC._flag import fakeThrustRTC
 
-# noinspection PyUnresolvedReferences
-import CURandRTC as rndrtc
 if not fakeThrustRTC:
     # noinspection PyUnresolvedReferences
     import ThrustRTC as trtc
+    # noinspection PyUnresolvedReferences
+    import CURandRTC as rndrtc
 else:
     # noinspection PyUnresolvedReferences
     from .fakeThrustRTC.fakeThrustRTC import FakeThrustRTC as trtc
+    # noinspection PyUnresolvedReferences
+    rndrtc = None
 
 NICE_THRUST_FLAGS = dict(
     wait=False,
