147666e22d784bdabd91aa3d12e3d6640de9723f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-10-18 19:14:54 +0200

    adding temperature and pressure to netCDF output

--- PySDM/products/environments/__init__.py
+++ PySDM/products/environments/__init__.py
@@ -5,4 +5,6 @@ Created at 28.08.2020
 from .dry_air_potential_temperature import DryAirPotentialTemperature
 from .water_vapour_mixing_ratio import WaterVapourMixingRatio
 from .relative_humidity import RelativeHumidity
+from .pressure import Pressure
+from .temperature import Temperature
 from .dry_air_density import DryAirDensity
