b2119009a1223c32bfebf60c593459a0be45fe88
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-10-27 22:01:28 +0100

    fixing paths

--- PySDM_examples/Bartman_2020_MasterThesis/plot_all.py
+++ PySDM_examples/Bartman_2020_MasterThesis/plot_all.py
@@ -2,8 +2,8 @@
 Created at 20.08.2020
 """
 
-from PySDM_examples.MasterThesis.fig_5_BDF_VS_ADAPTIVE import main as fig_5
-from PySDM_examples.MasterThesis.fig_4_adaptive_sdm import main as fig_4
+from PySDM_examples.Bartman_2020_MasterThesis.fig_5_BDF_VS_ADAPTIVE import main as fig_5
+from PySDM_examples.Bartman_2020_MasterThesis.fig_4_adaptive_sdm import main as fig_4
 
 
 if __name__ == '__main__':
