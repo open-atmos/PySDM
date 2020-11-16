8d2cd3d3648a10141153f3493ec2792c713f56bd
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-11-16 12:37:10 +0100

    Update __init__.py

--- PySDM/backends/numba/__init__.py
+++ PySDM/backends/numba/__init__.py
@@ -1,3 +1,11 @@
 """
 Created at 04.11.2019
 """
+
+from numba.np.ufunc.parallel import _launch_threads
+from numba.core.errors import NumbaWarning
+
+try:
+    _launch_threads()
+except (NumbaWarning)
+    pass
