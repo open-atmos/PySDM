cf015f09f5722115a988e8075235eed4d1766340
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-11-16 12:19:19 +0100

    import ignoring NumbaWarnings

--- PySDM/backends/numba/numba.py
+++ PySDM/backends/numba/numba.py
@@ -2,24 +2,43 @@
 Created at 24.07.2019
 """
 
-from numba.core.errors import NumbaWarning
-imports_done = False
-try: # python -We (warnings for incompatible versions of TBB which is optional) 
+from numba.core.errors import NumbaWarning # python -We (warnings for incompatible versions of TBB which is optional) 
+try: 
     from PySDM.backends.numba.impl._algorithmic_methods import AlgorithmicMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.impl._algorithmic_step_methods import AlgorithmicStepMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.impl._storage_methods import StorageMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.impl._maths_methods import MathsMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.impl._physics_methods import PhysicsMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.impl.condensation_methods import CondensationMethods
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.storage.storage import Storage as ImportedStorage
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.storage.indexed_storage import IndexedStorage as ImportedIndexedStorage
+except (NumbaWarning):
+    pass
+try: 
     from PySDM.backends.numba.random import Random as ImportedRandom
 except (NumbaWarning):
     pass
-else:
-    imports_done = True
-finally:
-    assert imports_done
 
 class Numba(
     AlgorithmicMethods,
