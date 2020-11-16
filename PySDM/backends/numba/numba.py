21cb9e4e0def012e2c2f0c987a0a0cc1033d41a4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2020-11-16 11:33:44 +0100

    Update numba.py

--- PySDM/backends/numba/numba.py
+++ PySDM/backends/numba/numba.py
@@ -2,16 +2,24 @@
 Created at 24.07.2019
 """
 
-from PySDM.backends.numba.impl._algorithmic_methods import AlgorithmicMethods
-from PySDM.backends.numba.impl._algorithmic_step_methods import AlgorithmicStepMethods
-from PySDM.backends.numba.impl._storage_methods import StorageMethods
-from PySDM.backends.numba.impl._maths_methods import MathsMethods
-from PySDM.backends.numba.impl._physics_methods import PhysicsMethods
-from PySDM.backends.numba.impl.condensation_methods import CondensationMethods
-from .storage.storage import Storage as ImportedStorage
-from PySDM.backends.numba.storage.indexed_storage import IndexedStorage as ImportedIndexedStorage
-from PySDM.backends.numba.random import Random as ImportedRandom
-
+from numba.core.errors import NumbaWarning
+imports_done = False
+try: # python -We (warnings for incompatible versions of TBB which is optional) 
+    from PySDM.backends.numba.impl._algorithmic_methods import AlgorithmicMethods
+    from PySDM.backends.numba.impl._algorithmic_step_methods import AlgorithmicStepMethods
+    from PySDM.backends.numba.impl._storage_methods import StorageMethods
+    from PySDM.backends.numba.impl._maths_methods import MathsMethods
+    from PySDM.backends.numba.impl._physics_methods import PhysicsMethods
+    from PySDM.backends.numba.impl.condensation_methods import CondensationMethods
+    from PySDM.backends.numba.storage.storage import Storage as ImportedStorage
+    from PySDM.backends.numba.storage.indexed_storage import IndexedStorage as ImportedIndexedStorage
+    from PySDM.backends.numba.random import Random as ImportedRandom
+except (NumbaWarning):
+    pass
+else:
+    imports_done = True
+finally:
+    assert imports_done
 
 class Numba(
     AlgorithmicMethods,
