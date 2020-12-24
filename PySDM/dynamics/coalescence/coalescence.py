9e85fc19e6164cd3320713543425a1557116b3dd
prbartman <prbartman@gmail.com>
2020-12-24 17:06:58 +0100

    coalescence further changes and refactoring

--- PySDM/dynamics/coalescence/coalescence.py
+++ PySDM/dynamics/coalescence/coalescence.py
@@ -45,35 +45,50 @@ class Coalescence:
     def __call__(self):
         # TODO dt
         if self.enable:
-            self.actual_length = self.core.particles._Particles__idx.length
-            for s in range(self.n_substep[0]):
-                self.step(s, self.adaptive_memory)
-                self.subs[:] += self.adaptive_memory
-                method1(self.adaptive_memory.data, self.msub.data)
-            self.core.particles._Particles__idx.length = self.actual_length
+            if not self.adaptive:
+                self.step(0, self.adaptive_memory)
+            else:
+                self.actual_length = self.core.particles._Particles__idx.length
+                self.core.particles.cell_idx.data = self.n_substep.data.argsort(kind="stable")[::-1]
+                for s in range(max(self.n_substep.data)):  # range(self.n_substep[0]):
+                    self.step(s, self.adaptive_memory)
+                    self.subs[:] += self.adaptive_memory
+                    method1(self.adaptive_memory.data, self.msub.data)
 
-            if self.adaptive:
+                self.core.particles._Particles__idx.length = self.actual_length
+
                 method2(self.n_substep.data, self.msub.data, self.max_substeps, self.subs.data)
+                self.subs[:] = 0
+                self.msub[:] = 0
+
+                self.core.particles.cell_idx.data = np.arange(len(self.core.particles.cell_idx.data)).astype(
+                    dtype=np.int64)  # TODO
+                self.core.particles._Particles__sort_by_cell_id()
 
     def step(self, s, adaptive_memory):
         pairs_rand, rand = self.rnd_opt.get_random_arrays(s)
         self.toss_pairs(self.is_first_in_pair, pairs_rand, s)
         self.compute_probability(self.prob, self.is_first_in_pair)
         self.compute_gamma(self.prob, rand)
-        adaptive_memory[:] = 1
+        if self.adaptive:
+            adaptive_memory[:] = 1
         self.core.particles.coalescence(gamma=self.prob, adaptive=self.adaptive, subs=self.n_substep,
                                         adaptive_memory=adaptive_memory)
 
     def toss_pairs(self, is_first_in_pair, u01, s):
-        self.core.particles._Particles__idx.length = self.actual_length
+        if self.adaptive:
+            self.core.particles._Particles__idx.length = self.actual_length
         self.core.particles.sanitize()
-        self.actual_length = self.core.particles._Particles__idx.length
+        if self.adaptive:
+            self.actual_length = self.core.particles._Particles__idx.length
+
         self.core.particles.permutation(u01, self.croupier == 'local')
-        if s == 0:
-            self.core.particles.cell_idx.data = self.n_substep.data.argsort(kind="stable")[::-1]
-        end = method3(self.n_substep.data, self.core.mesh.n_cell, self.core.particles.cell_start.data,
-                      self.core.particles.cell_idx.data, s)
-        self.core.particles._Particles__idx.length = end
+
+        if self.adaptive:
+            end = method3(self.n_substep.data, self.core.mesh.n_cell, self.core.particles.cell_start.data,
+                          self.core.particles.cell_idx.data, s)
+            self.core.particles._Particles__idx.length = end
+
         is_first_in_pair.update(
             self.core.particles.cell_start,
             self.core.particles.cell_idx,
@@ -104,15 +119,16 @@ def method1(adaptive_memory, msub):
 @numba.njit()
 def method2(n_substep, msub, max_substeps, subs):
     for i in range(len(n_substep)):
-        n_substep[i] = min(max_substeps, int(((subs[i] / n_substep[i]) + msub[i]) / 2))
+        n_substep[i] = min(max_substeps, ((subs[i] / n_substep[i]) + msub[i]) // 2)
 
 
 @numba.njit()
 def method3(n_substep, n_cell, cell_start, cell_idx, s):
     end = 0
     for i in range(n_cell - 1, -1, -1):
-        if n_substep[i] < s:
+        if n_substep[i] <= s:
             continue
         else:
             end = cell_start[cell_idx[i] + 1]
+            break
     return end
