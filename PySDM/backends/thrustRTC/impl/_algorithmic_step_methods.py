5c7a5d36cb998ef4034b1bf5963a97fdeb4ed81d
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-01-05 18:37:23 +0100

    handling 0-sized arrays in _sort_by_cell_id_and_update_cell_start() debug code; commenting out misleading unused find_pairs in Numba backend; one TODO more

--- PySDM/backends/thrustRTC/impl/_algorithmic_step_methods.py
+++ PySDM/backends/thrustRTC/impl/_algorithmic_step_methods.py
@@ -70,7 +70,7 @@ class AlgorithmicStepMethods:
 
     @staticmethod
     @nice_thrust(**NICE_THRUST_FLAGS)
-    def find_pairs(cell_start, is_first_in_pair, cell_id, cell_idx, idx, length):
+    def find_pairs(cell_start, is_first_in_pair, cell_id, cell_idx, idx, length):  # TODO: handle cell_idx
         perm_cell_id = trtc.DVPermutation(cell_id, idx)
         dvlength = trtc.DVInt64(length)
         AlgorithmicStepMethods.__find_pairs_body.launch_n(length, [cell_start, perm_cell_id, is_first_in_pair, dvlength])
