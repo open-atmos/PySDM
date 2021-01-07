b26e5b90fc5d6b7f55106a0c7671791d31abce89
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-01-08 00:04:31 +0100

    API fixes, commenting out super-ineffective assert

--- PySDM/backends/thrustRTC/impl/_algorithmic_methods.py
+++ PySDM/backends/thrustRTC/impl/_algorithmic_methods.py
@@ -90,7 +90,7 @@ class AlgorithmicMethods:
 
     @staticmethod
     @nice_thrust(**NICE_THRUST_FLAGS)
-    def coalescence(n, volume, idx, length, intensive, extensive, gamma, healthy, adaptive, cell_id, subs, adaptive_memory):
+    def coalescence(n, volume, idx, length, intensive, extensive, gamma, healthy, adaptive, cell_id, cell_idx, subs, adaptive_memory, collision_rate, collision_rate_deficit):
         idx_length = trtc.DVInt64(len(idx))
         intensive_length = trtc.DVInt64(len(intensive))
         extensive_length = trtc.DVInt64(len(extensive))
@@ -262,7 +262,7 @@ class AlgorithmicMethods:
 
     @staticmethod
     @nice_thrust(**NICE_THRUST_FLAGS)
-    def normalize(prob, cell_id, cell_start, norm_factor, dt, dv, n_substeps):
+    def normalize(prob, cell_id, cell_idx, cell_start, norm_factor, dt, dv, n_substeps):
         n_cell = cell_start.shape[0] - 1
         device_dt_div_dv = PrecisionResolver.get_floating_point(dt / dv)
         AlgorithmicMethods.__normalize_body_0.launch_n(n_cell, [cell_start.data, norm_factor.data, device_dt_div_dv])
@@ -305,9 +305,9 @@ class AlgorithmicMethods:
     @nice_thrust(**NICE_THRUST_FLAGS)
     def _sort_by_cell_id_and_update_cell_start(cell_id, cell_idx, cell_start, idx, length):
         # TODO !!!
-        if length > 0:
-            max_cell_id = max(cell_id.to_ndarray())
-            assert max_cell_id == 0
+        # if length > 0:
+        #     max_cell_id = max(cell_id.to_ndarray())
+        #     assert max_cell_id == 0
         trtc.Fill(cell_start.data, trtc.DVInt64(length))
         AlgorithmicMethods.___sort_by_cell_id_and_update_cell_start_body.launch_n(length - 1,
                                                                                   [cell_id.data, cell_start.data, idx.data])
