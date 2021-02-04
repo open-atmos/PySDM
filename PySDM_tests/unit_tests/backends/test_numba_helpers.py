c307bd62c7f90de0b8a075973b480d5693556254
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-02-04 14:24:54 +0100

    adding unit test for pair_indices

--- PySDM_tests/unit_tests/backends/test_numba_helpers.py
+++ PySDM_tests/unit_tests/backends/test_numba_helpers.py
@@ -1,6 +1,7 @@
-from PySDM.backends.numba.numba_helpers import bisec
+from PySDM.backends.numba.numba_helpers import bisec, pair_indices
 import os
 import numpy as np
+import pytest
 
 
 class TestNumbaHelpers:
@@ -17,3 +18,20 @@ class TestNumbaHelpers:
 
         # assert
         np.testing.assert_almost_equal(f(zero), 0)
+
+    @staticmethod
+    @pytest.mark.parametrize("i, idx, is_first_in_pair, expected", [
+        (0, (0, 1), (True, False), (0, 1)),
+        (0, (1, 0), (True, False), (1, 0)),
+        (0, (0, 1, 2), (False, True), (1, 2)),
+    ])
+    def test_pair_indices(i, idx, is_first_in_pair, expected):
+        # Arrange
+        sut = pair_indices if 'NUMBA_DISABLE_JIT' in os.environ else pair_indices.py_func
+
+        # Act
+        actual = sut(i, idx, is_first_in_pair)
+
+        # Assert
+        assert expected == actual
+
