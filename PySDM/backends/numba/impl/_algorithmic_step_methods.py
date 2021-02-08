0e7cd397d02b48d9aee09a7619aa6139e61f53f8
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-02-08 15:44:53 +0100

    reverting accidental changes in numba algorithmic_*methods files done during merge

--- PySDM/backends/numba/impl/_algorithmic_step_methods.py
+++ PySDM/backends/numba/impl/_algorithmic_step_methods.py
@@ -4,7 +4,7 @@ Created at 18.03.2020
 
 import numba
 import numpy as np
-from numba import float64, int64, void, prange
+from numba import f8, i8, void, prange, b1
 
 from PySDM.backends.numba import conf
 
@@ -12,66 +12,43 @@ from PySDM.backends.numba import conf
 class AlgorithmicStepMethods:
 
     @staticmethod
-    @numba.njit([float64(float64[:], int64[:], int64),
-                 int64(int64[:], int64[:], int64)], **conf.JIT_FLAGS)
-    def amax(row, idx, length):
-        result = np.amax(row[idx[:length]])
-        return result
-
-    @staticmethod
-    @numba.njit([float64(float64[:], int64[:], int64),
-                 int64(int64[:], int64[:], int64)], **conf.JIT_FLAGS)
-    def amin(row, idx, length):
-        result = np.amin(row[idx[:length]])
-        return result
-
-    @staticmethod
-    # @numba.njit(**conf.JIT_FLAGS)  # Note: in Numba 0.51 "np.dot() only supported on float and complex arrays"
-    def cell_id_body(cell_id, cell_origin, strides):
-        if len(strides) == 1:
-            cell_id[:] = cell_origin
-        else:
-            cell_id[:] = np.dot(strides, cell_origin)
-
-    @staticmethod
-    def cell_id(cell_id, cell_origin, strides):
-        return AlgorithmicStepMethods.cell_id_body(cell_id.data, cell_origin.data, strides.data)
-
-    @staticmethod
-    @numba.njit(void(float64[:], float64[:], int64[:], int64[:], int64), **conf.JIT_FLAGS)
+    @numba.njit(void(f8[:], f8[:], b1[:], i8[:], i8), **conf.JIT_FLAGS)
     def distance_pair(data_out, data_in, is_first_in_pair, idx, length):
-        # note: silently assumes that data_out is not permuted (i.e. not part of state)
+        data_out[:] = 0
         for i in prange(length - 1):
-            data_out[i] = np.abs(data_in[idx[i]] - data_in[idx[i + 1]]) if is_first_in_pair[i] else 0
+            if is_first_in_pair[i]:
+                data_out[i//2] = np.abs(data_in[idx[i]] - data_in[idx[i + 1]])
 
     @staticmethod
-    @numba.njit(void(int64[:], int64[:], int64[:], int64[:], int64[:], int64), **conf.JIT_FLAGS)
+    @numba.njit(void(i8[:], b1[:], i8[:], i8[:], i8[:], i8), **conf.JIT_FLAGS)
     def find_pairs_body(cell_start, is_first_in_pair, cell_id, cell_idx, idx, length):
         for i in prange(length - 1):
             is_first_in_pair[i] = (
                     cell_id[idx[i]] == cell_id[idx[i + 1]] and
                     (i - cell_start[cell_idx[cell_id[idx[i]]]]) % 2 == 0
             )
-
-    # @staticmethod
-    # def find_pairs(cell_start, cell_idx, is_first_in_pair, cell_id, idx, length):
-    #     return AlgorithmicStepMethods.find_pairs_body(
-    #         cell_start.data, cell_idx.data, is_first_in_pair.data, cell_id.data, idx.data, length)
-    #
+        is_first_in_pair[-1] = False
 
     @staticmethod
-    @numba.njit(void(float64[:], int64[:], int64[:], int64[:], int64), **conf.JIT_FLAGS)
+    def find_pairs(cell_start, is_first_in_pair, cell_id, cell_idx, idx):
+        return AlgorithmicStepMethods.find_pairs_body(
+            cell_start.data, is_first_in_pair.data, cell_id.data, cell_idx.data, idx.data, len(idx))
+
+    @staticmethod
+    @numba.njit([void(f8[:], i8[:], b1[:], i8[:], i8),
+                 void(f8[:], f8[:], b1[:], i8[:], i8)], **conf.JIT_FLAGS)
     def max_pair_body(data_out, data_in, is_first_in_pair, idx, length):
-        # note: silently assumes that data_out is not permuted (i.e. not part of state)
+        data_out[:] = 0
         for i in prange(length - 1):
-            data_out[i] = max(data_in[idx[i]], data_in[idx[i + 1]]) if is_first_in_pair[i] else 0
+            if is_first_in_pair[i]:
+                data_out[i//2] = max(data_in[idx[i]], data_in[idx[i + 1]])
 
     @staticmethod
     def max_pair(data_out, data_in, is_first_in_pair, idx, length):
         return AlgorithmicStepMethods.max_pair_body(data_out.data, data_in.data, is_first_in_pair.data, idx.data, length)
 
     @staticmethod
-    @numba.njit(void(float64[:], float64[:], int64[:], int64[:], int64), **conf.JIT_FLAGS)
+    @numba.njit(void(f8[:], f8[:], b1[:], i8[:], i8), **conf.JIT_FLAGS)
     def sort_pair_body(data_out, data_in, is_first_in_pair, idx, length):
         data_out[:] = 0
         for i in prange(length - 1):
@@ -82,16 +59,30 @@ class AlgorithmicStepMethods:
                     data_out[i], data_out[i + 1] = data_in[idx[i]], data_in[idx[i + 1]]
 
     @staticmethod
-    def sort_pair(data_out, data_in, is_first_in_pair, idx, length):
+    def sort_pair(data_out, data_in, is_first_in_pair, idx):
         return AlgorithmicStepMethods.sort_pair_body(
-            data_out.data, data_in.data, is_first_in_pair.data, idx.data, length)
+            data_out.data, data_in.data, is_first_in_pair.data, idx.data, len(idx))
 
     @staticmethod
-    @numba.njit(void(float64[:], float64[:], int64[:], int64[:], int64), **conf.JIT_FLAGS)
-    def sum_pair_body(data_out, data_in, is_first_in_pair, idx, length):
-        # note: silently assumes that data_out is not permuted (i.e. not part of state)
+    @numba.njit(**conf.JIT_FLAGS)
+    def sort_within_pair_by_attr_body(idx, length, is_first_in_pair, attr):
         for i in prange(length - 1):
-            data_out[i] = (data_in[idx[i]] + data_in[idx[i + 1]]) if is_first_in_pair[i] else 0
+            if is_first_in_pair[i]:
+                if attr[idx[i]] < attr[idx[i + 1]]:
+                    idx[i], idx[i + 1] = idx[i + 1], idx[i]
+
+    @staticmethod
+    def sort_within_pair_by_attr(idx, length, is_first_in_pair, attr):
+        AlgorithmicStepMethods.sort_within_pair_by_attr_body(
+            idx.data, length, is_first_in_pair.indicator.data, attr.data)
+
+    @staticmethod
+    @numba.njit(void(f8[:], f8[:], b1[:], i8[:], i8), **conf.JIT_FLAGS)
+    def sum_pair_body(data_out, data_in, is_first_in_pair, idx, length):
+        data_out[:] = 0
+        for i in prange(length - 1):
+            if is_first_in_pair[i]:
+                data_out[i//2] = (data_in[idx[i]] + data_in[idx[i + 1]])
 
     @staticmethod
     def sum_pair(data_out, data_in, is_first_in_pair, idx, length):
