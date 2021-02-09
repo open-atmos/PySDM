fix make_rhod, rename hydrostatic formula

PySDM/physics/formulae.py
@@ -65,18 +65,18 @@ class ThStd:
 
 
 class Hydrostatic:
-    # TODO: include assumptions in the name
+    # TODO: include assumptions in the name (p vs. pd)
     @staticmethod
     def drhod_dz(p, T, qv):
         rho = MoistAir.rho_of_p_qv_T(p, qv, T)
-        pd = MoistAir.p_d(p, qv)  # TODO: why not p?!
+        pd = MoistAir.p_d(p, qv)
         return - const.g / const.Rd / T * (rho - pd / c_p(qv) / T)
 
-    # TODO: include assumptions in the name
     @staticmethod
-    def p_of_p0_thstd_qv_z(p0, thstd, qv, z):
+    def p_of_z_assuming_const_th_and_qv(p0, thstd, qv, z):
         kappa = const.Rd / const.c_pd
-        arg = np.power(p0/const.p1000, kappa) - z * kappa * const.g / thstd / R(qv)
+        z0 = 0
+        arg = np.power(p0/const.p1000, kappa) - (z-z0) * kappa * const.g / thstd / R(qv)
         return const.p1000 * np.power(arg, 1/kappa)
 
 

PySDM/state/arakawa_c.py
@@ -68,10 +68,10 @@ def z_scalar_coord(grid):
     return zZ
 
 
-def make_rhod(grid, rhod_of):
+def make_rhod(grid, rhod_of_zZ):
     return np.repeat(
-        rhod_of(
-            z_scalar_coord(grid)
+        rhod_of_zZ(
+            z_scalar_coord(grid) / grid[-1]
         ).reshape((1, grid[1])),
         grid[0],
         axis=0

PySDM_examples/Arabas_et_al_2015_Figs_8_9/settings.py
@@ -119,7 +119,7 @@ class Settings:
         return - self.rho_w_max * X / np.pi * np.sin(np.pi * zZ) * np.cos(2 * np.pi * xX)
 
     def rhod(self, zZ):
-        p = phys.Hydrostatic.p_of_p0_thstd_qv_z(self.p0, self.th_std0, self.qv0, z=zZ * self.size[-1])
+        p = phys.Hydrostatic.p_of_z_assuming_const_th_and_qv(self.p0, self.th_std0, self.qv0, z=zZ * self.size[-1])
         rhod = phys.ThStd.rho_d(p, self.qv0, self.th_std0)
         return rhod
 