e853bbfd0d239841c49f4e445040f00a718b8169
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-02-12 02:16:47 +0100

    disabling coalescence adaptivity by default

--- PySDM/dynamics/coalescence/coalescence.py
+++ PySDM/dynamics/coalescence/coalescence.py
@@ -17,7 +17,7 @@ class Coalescence:
                  croupier=None,
                  optimized_random=False,
                  substeps: int = 1,
-                 adaptive: bool = True,
+                 adaptive: bool = False,
                  dt_coal_range=default_dt_coal_range
                  ):
         assert substeps == 1 or adaptive is False
