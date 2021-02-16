cfff23e9fa097d982246eecbae176d39bb92d2b4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-02-16 10:01:34 +0100

    introducing "if not giving_up_with_hope_other_builds_did_it"...

--- PySDM_tests/devops_tests/test_todos_annotated.py
+++ PySDM_tests/devops_tests/test_todos_annotated.py
@@ -4,6 +4,7 @@ import re
 import sys
 import pathlib
 from ghapi.all import GhApi, paged
+from fastcore.net import ExceptionsHTTP
 
 
 # https://stackoverflow.com/questions/7012921/recursive-grep-using-python
@@ -34,13 +35,16 @@ def file(request):
 
 @pytest.fixture(scope='session')
 def gh_issues():
-    api = GhApi(owner='atmos-cloud-sim-uj', repo='PySDM')
-    pages = paged(api.issues.list_for_repo, owner='atmos-cloud-sim-uj', repo='PySDM', state='all', per_page=100)
-    all = {}
-    for page in pages:
-        for item in page.items:
-            all[item.number] = item.state
-    return all
+    res = {}
+    try:
+        api = GhApi(owner='atmos-cloud-sim-uj', repo='PySDM')
+        pages = paged(api.issues.list_for_repo, owner='atmos-cloud-sim-uj', repo='PySDM', state='all', per_page=100)
+        for page in pages:
+            for item in page.items:
+                res[item.number] = item.state
+    except ExceptionsHTTP[403]:
+        pass
+    return res
 
 
 @pytest.mark.skipif("'CI' in os.environ and ('GITHUB_ACTIONS' not in os.environ or sys.version_info.minor < 8)")
@@ -51,8 +55,10 @@ def test_todos_annotated(file, gh_issues):
         match = re.search(r'TODO #(\d+)', line)
         if match is None:
             raise Exception(f"TODO not annotated with issue id ({line})")
-        number = int(match.group(1))
-        if number not in gh_issues.keys():
-            raise Exception(f"TODO annotated with non-existent id ({line})")
-        if gh_issues[number] != 'open':
-            raise Exception(f"TODO remains for a non-open issue ({line})")
+        giving_up_with_hope_other_builds_did_it = len(gh_issues) == 0
+        if not giving_up_with_hope_other_builds_did_it:
+            number = int(match.group(1))
+            if number not in gh_issues.keys():
+                raise Exception(f"TODO annotated with non-existent id ({line})")
+            if gh_issues[number] != 'open':
+                raise Exception(f"TODO remains for a non-open issue ({line})")
