b02b3fd9f322441266757c31b8d66a6b98cd9378
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-03-31 01:26:28 +0200

    fixing import and path [skip ci]

--- PySDM_tests/smoke_tests/Yang_et_al_2018/test_just_do_it.py
+++ PySDM_tests/smoke_tests/Yang_et_al_2018/test_just_do_it.py
@@ -2,8 +2,8 @@
 Created at 2020
 """
 
-from PySDM_examples.Yang_et_al_2018_Fig_2.example import Simulation
-from PySDM_examples.Yang_et_al_2018_Fig_2.settings import Settings
+from PySDM_examples.Yang_et_al_2018.example import Simulation
+from PySDM_examples.Yang_et_al_2018.settings import Settings
 from PySDM.physics.constants import si
 from PySDM.backends.numba import bdf
 import pytest
