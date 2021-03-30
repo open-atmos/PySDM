a284df073e0a81525c4d1b5d462f414f701d9d5d
prbartman <prbartman@gmail.com>
2021-03-30 12:12:23 +0200

    move some requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+
 def get_long_description():
     with open("README.md", "r") as file:
         long_description = file.read()
     return long_description
+
 
 setup(
     name='PySDM',
     description='Pythonic particle-based (super-droplet) cloud microphysics modelling with Jupyter examples',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    install_requires=['numpy','numba','pint','chempy','scipy'],
+    install_requires=['ThrustRTC==0.3.10',
+                      'CURandRTC==0.1.6',
+                      'numba==0.53.0',
+                      'numpy>=1.19.4',
+                      'Pint>=0.11',
+                      'chempy>=0.7.10',
+                      'scipy>=1.4.1'],
     author='https://github.com/atmos-cloud-sim-uj/PySDM/graphs/contributors',
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
@@ -27,7 +35,7 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Physics',
         'Topic :: Software Development :: Libraries'
-    ],  
+    ],
     keywords='physics-simulation, monte-carlo-simulation, gpu-computing, atmospheric-modelling, particle-system, numba, thrust, nvrtc, pint, atmospheric-physics',
-    packages=find_packages(include=['PySDM','PySDM.*'])
+    packages=find_packages(include=['PySDM', 'PySDM.*'])
 )
