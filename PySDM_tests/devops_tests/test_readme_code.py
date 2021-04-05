d3714bdedebfcb0b15baea362fc494c78c50b871
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-04-05 11:00:26 +0200

    bump exdown requirement and use the new encodimg parameter

--- PySDM_tests/devops_tests/test_readme_code.py
+++ PySDM_tests/devops_tests/test_readme_code.py
@@ -1,10 +1,9 @@
 import exdown
 import pathlib
-import pytest
-import sys
 
 
-test_readme = pytest.mark.skipif("sys.getfilesystemencoding() != 'utf-8'")(exdown.pytests(
+test_readme = exdown.pytests(
     pathlib.Path(__file__).parent.parent.parent.joinpath("README.md").absolute(),
-    syntax_filter="Python"
-))
+    syntax_filter="Python",
+    encoding='utf8'
+)
