c746ae01cc35c001eb37743fa1f26a6503eb73fb
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-04-05 02:43:04 +0200

    skipping README test on Windows

--- PySDM_tests/devops_tests/test_readme_code.py
+++ PySDM_tests/devops_tests/test_readme_code.py
@@ -1,7 +1,10 @@
 import exdown
 import pathlib
+import pytest
+import platform
 
-test_readme = exdown.pytests(
+# note: skipping Windows to avoid non-UTF encoding trouble
+test_readme = pytest.mark.skipif('platform.system() == "Windows"')(exdown.pytests(
     pathlib.Path(__file__).parent.parent.parent.joinpath("README.md").absolute(),
     syntax_filter="Python"
-)
+))
