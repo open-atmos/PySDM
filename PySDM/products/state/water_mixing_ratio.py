01172b9943987d192940be028e8087c23e28fab1
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-05-05 23:10:04 +0200

    bump example req; sanitize water_mixing_ratio field initialisation

--- PySDM/products/state/water_mixing_ratio.py
+++ PySDM/products/state/water_mixing_ratio.py
@@ -7,6 +7,7 @@ class WaterMixingRatio(MomentProduct):
 
     def __init__(self, radius_range, name='ql', description_prefix='liquid'):
         self.radius_range = radius_range
+        self.volume_range = None
         super().__init__(
             name=name,
             unit='g/kg',
@@ -18,6 +19,7 @@ class WaterMixingRatio(MomentProduct):
     def register(self, builder):
         super().register(builder)
         self.volume_range = self.formulae.trivia.volume(np.asarray(self.radius_range))
+        self.radius_range = None
 
     def get(self):  # TODO #217
         self.download_moment_to_buffer('volume', rank=0, filter_range=self.volume_range, filter_attr='volume')
