7c606be0c2d45760df9506cfbbcf3901a5e9e9f4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-12-01 17:49:03 -0600

    fixing collision rate product timestep reset issue (#693), moving RateProduct to a common impl file (but still not used in condensation rate products), adding unit test checking timestep count reset

--- PySDM/products/coalescence/collision_rates.py
+++ PySDM/products/coalescence/collision_rates.py
@@ -1,33 +1,11 @@
-from PySDM.products.impl.product import Product
+from PySDM.products.impl.rate_product import RateProduct
 
 
-class _CollisionRateProduct(Product):
-    def __init__(self, name, unit, counter):
-        super().__init__(name=name, unit=unit)
-        self.timestep_count = 0
-        self.counter = counter
-
-    def register(self, builder):
-        super().register(builder)
-        self.counter = getattr(self.particulator.dynamics['Coalescence'], self.counter)
-        self.particulator.observers.append(self)
-
-    def notify(self):
-        self.timestep_count += 1
-
-    def _impl(self, **kwargs):
-        self._download_to_buffer(self.counter)
-        if self.timestep_count != 0:
-            self.buffer[:] /= self.timestep_count * self.particulator.dt
-        self.counter[:] = 0
-        return self.buffer
-
-
-class CollisionRateDeficitPerGridbox(_CollisionRateProduct):
+class CollisionRateDeficitPerGridbox(RateProduct):
     def __init__(self, name=None, unit='s^-1'):
-        super().__init__(name=name, unit=unit, counter='collision_rate_deficit')
+        super().__init__(name=name, unit=unit, counter='collision_rate_deficit', dynamic='Coalescence')
 
 
-class CollisionRatePerGridbox(_CollisionRateProduct):
+class CollisionRatePerGridbox(RateProduct):
     def __init__(self, name=None, unit='s^-1'):
-        super().__init__(name=name, unit=unit, counter='collision_rate')
+        super().__init__(name=name, unit=unit, counter='collision_rate', dynamic='Coalescence')
---
+++ PySDM/products/impl/rate_product.py
@@ -0,0 +1,26 @@
+from PySDM.products.impl.product import Product
+
+
+class RateProduct(Product):
+    def __init__(self, name, unit, counter, dynamic):
+        super().__init__(name=name, unit=unit)
+        self.timestep_count = 0
+        self.counter = counter
+        self.dynamic = dynamic
+
+    def register(self, builder):
+        super().register(builder)
+        self.counter = getattr(self.particulator.dynamics[self.dynamic], self.counter)
+        self.dynamic = None
+        self.particulator.observers.append(self)
+
+    def notify(self):
+        self.timestep_count += 1
+
+    def _impl(self, **kwargs):
+        self._download_to_buffer(self.counter)
+        if self.timestep_count != 0:
+            self.buffer[:] /= self.timestep_count * self.particulator.dt
+            self.timestep_count = 0
+        self.counter[:] = 0
+        return self.buffer
