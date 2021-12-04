1c37837834264365c7e12809f2338affdf8a7c87
claresinger <csinger@caltech.edu>
2021-12-04 15:02:51 -0800

    update compressed film models and add one more. update Ruehl models to take vectors, not just scalars in the root finding.

--- PySDM/physics/surface_tension/__init__.py
+++ PySDM/physics/surface_tension/__init__.py
@@ -4,3 +4,4 @@ Particle surface tension formulae
 from .constant import Constant
 from .compressed_film_ovadnevaite import CompressedFilmOvadnevaite
 from .compressed_film_ruehl import CompressedFilmRuehl
+from .szyszkowski_langmuir import SzyszkowskiLangmuir
--- PySDM/physics/surface_tension/szyszkowski_langmuir.py
+++ PySDM/physics/surface_tension/szyszkowski_langmuir.py
@@ -6,28 +6,25 @@ from PySDM.physics import constants as const
 nu_org = np.nan
 A0 = np.nan
 C0 = np.nan
-m_sigma = np.nan
 sgm_min = np.nan
 
 
-class CompressedFilmRuehl:
+class SzyszkowskiLangmuir:
     """
-    Compressed film model of surface-partitioning of organics from Ruehl et al. (2016).
-    Described in supplementary materials equations (13) and (15).
+    Szyszkowski-Langmuir surface-partitioning of organics described in Ruehl et al. (2016).
+    Described in supplementary materials equations (12) and (14).
 
     Allows for more realistic thermodynamic partitioning of some organic to the surface,
     while some remains dissolved in the bulk phase. The surface concentration is solved
     implicitly from the isotherm equation that relates the bulk organic concentration
     `C_bulk` to the surface average molecular area `A`. The equation of state relates
-    the surface concentration to the surface tension. For the compressed film model it
-    is linear, with slope `m_sigma`.
+    the surface concentration to the surface tension.
     """
     @staticmethod
     def _check():
         assert np.isfinite(nu_org)
         assert np.isfinite(A0)
         assert np.isfinite(C0)
-        assert np.isfinite(m_sigma)
         assert np.isfinite(sgm_min)
 
     @staticmethod
@@ -41,12 +38,11 @@ class CompressedFilmRuehl:
         A_iso = (4 * np.pi * r_wet**2) / (f_org * v_dry * sci.N_A / nu_org) # m^2 = A*f_surf
 
         # solve implicitly for fraction of organic at surface
-        c = m_sigma*sci.N_A/(2*sci.R*T)
-        f = lambda f_surf: np.log(Cb_iso*(1-f_surf)/C0) - c*(A0**2 - (A_iso/f_surf)**2)
+        f = lambda f_surf: A0/(A_iso/f_surf) - (Cb_iso*(1-f_surf)/C0)/(1 + (Cb_iso*(1-f_surf)/C0))
         sol = optimize.root(f, np.ones(len(v_wet)))
         f_surf = sol.x
 
         # calculate surface tension
-        sgm = const.sgm_w - (A0 - A_iso/f_surf)*m_sigma
+        sgm = const.sgm_w - ((sci.R*T)/(A0*sci.N_A))*np.log(1 + Cb_iso*(1-f_surf)/C0)
         sgm = np.minimum(np.maximum(sgm, sgm_min), const.sgm_w)
         return sgm
