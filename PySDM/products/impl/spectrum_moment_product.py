43be296c8a7eec0c74dfcbae4bbbf15d27235522
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-12-31 12:27:28 +0100

    fix attr_unit check

--- PySDM/products/impl/spectrum_moment_product.py
+++ PySDM/products/impl/spectrum_moment_product.py
@@ -9,7 +9,6 @@ from PySDM.products.impl.product import Product
 class SpectrumMomentProduct(ABC, Product):
     def __init__(self, name, unit, attr_unit):
         super().__init__(name=name, unit=unit)
-        _ = self._parse_unit(unit)
         self.attr_bins_edges = None
         self.attr_unit = attr_unit
         self.moment_0 = None
@@ -21,6 +20,7 @@ class SpectrumMomentProduct(ABC, Product):
             (len(self.attr_bins_edges) - 1, self.particulator.mesh.n_cell), dtype=float)
         self.moments = self.particulator.Storage.empty(
             (len(self.attr_bins_edges) - 1, self.particulator.mesh.n_cell), dtype=float)
+        _ = self._parse_unit(self.attr_unit)
 
     def _recalculate_spectrum_moment(
         self, attr,
