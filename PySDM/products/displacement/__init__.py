cd2228eda4cb3f2eb133cf34afdb880ae79caf90
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-01-04 23:00:38 +0100

    adding MaxCourantNumber product (+test)

--- PySDM/products/displacement/__init__.py
+++ PySDM/products/displacement/__init__.py
@@ -2,3 +2,4 @@
 products pertinent to the `PySDM.dynamics.displacement.Displacement` dynamic
 """
 from .surface_precipitation import SurfacePrecipitation
+from .max_courant_number import MaxCourantNumber
