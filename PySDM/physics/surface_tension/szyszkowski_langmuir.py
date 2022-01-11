4f236b660e3af8d1dec315bfd9d7d8c73cc4f464
claresinger <csinger@caltech.edu>
2022-01-11 15:33:23 -0800

    update szy lang with new const format

--- PySDM/physics/surface_tension/szyszkowski_langmuir.py
+++ PySDM/physics/surface_tension/szyszkowski_langmuir.py
@@ -1,13 +1,10 @@
+"""
+surface tension coefficient model featuring surface-partitioning
+ as in [Ruehl et al. (2016)](https://doi.org/10.1126/science.aad4889)
+"""
 import numpy as np
 from scipy import constants as sci
 from scipy import optimize
-from PySDM.physics import constants as const
-
-nu_org = np.nan
-A0 = np.nan
-C0 = np.nan
-sgm_min = np.nan
-
 
 class SzyszkowskiLangmuir:
     """
@@ -21,28 +18,30 @@ class SzyszkowskiLangmuir:
     the surface concentration to the surface tension.
     """
     @staticmethod
-    def _check():
-        assert np.isfinite(nu_org)
-        assert np.isfinite(A0)
-        assert np.isfinite(C0)
-        assert np.isfinite(sgm_min)
+    def __init__(self, const):
+        assert np.isfinite(const.RUEHL_nu_org)
+        assert np.isfinite(const.RUEHL_A0)
+        assert np.isfinite(const.RUEHL_C0)
+        assert np.isfinite(const.RUEHL_sgm_min)
 
     @staticmethod
-    def sigma(T, v_wet, v_dry, f_org):
+    def sigma(const, T, v_wet, v_dry, f_org):
         r_wet = ((3 * v_wet) / (4 * np.pi))**(1/3) # m - wet radius
 
         # C_bulk is the concentration of the organic in the bulk phase
-        Cb_iso = (f_org*v_dry/nu_org) / (v_wet/const.nu_w) # = C_bulk / (1-f_surf)
+        Cb_iso = (f_org*v_dry/const.RUEHL_nu_org) / (v_wet/const.nu_w) # = C_bulk / (1-f_surf)
 
         # A is the area that one molecule of organic occupies at the droplet surface
-        A_iso = (4 * np.pi * r_wet**2) / (f_org * v_dry * sci.N_A / nu_org) # m^2 = A*f_surf
+        A_iso = (4 * np.pi * r_wet**2) / (f_org * v_dry * sci.N_A / const.RUEHL_nu_org) # m^2 = A*f_surf
 
         # solve implicitly for fraction of organic at surface
-        f = lambda f_surf: A0/(A_iso/f_surf) - (Cb_iso*(1-f_surf)/C0)/(1 + (Cb_iso*(1-f_surf)/C0))
-        sol = optimize.root(f, 0.5*np.ones(len(v_wet)))
-        f_surf = np.minimum(np.maximum(sol.x, 0), 1)
+        a = const.RUEHL_A0 / A_iso
+        c = Cb_iso / const.RUEHL_C0
+        f = lambda f_surf: a*f_surf - (c*(1-f_surf)) / (1 + c*(1-f_surf))
+        sol = optimize.root_scalar(f, bracket=[0, 1])
+        f_surf = sol.root
 
         # calculate surface tension
-        sgm = const.sgm_w - ((sci.R*T)/(A0*sci.N_A))*np.log(1 + Cb_iso*(1-f_surf)/C0)
-        sgm = np.minimum(np.maximum(sgm, sgm_min), const.sgm_w)
+        sgm = const.sgm_w - ((sci.R*T)/(const.RUEHL_A0*sci.N_A)) * np.log(1 + Cb_iso*(1-f_surf)/const.RUEHL_C0)
+        sgm = np.minimum(np.maximum(sgm, const.RUEHL_sgm_min), const.sgm_w)
         return sgm
