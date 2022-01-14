569a809ee7e5f12f0bbf207619a0e1132e017f0b
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-01-14 14:19:57 -0600

    adding missing import

--- PySDM/dynamics/terminal_velocity/__init__.py
+++ PySDM/dynamics/terminal_velocity/__init__.py
@@ -2,3 +2,4 @@
 particle terminal velocity formulae
 """
 from PySDM.dynamics.terminal_velocity.gunn_and_kinzer import Interpolation
+from PySDM.dynamics.terminal_velocity.rogers_and_yau import RogersYau
