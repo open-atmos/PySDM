df2cfed7d7b0da194970aac1c1a296e3fc667248
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-01-14 17:15:32 -0600

    missing new line and docstring

--- PySDM/dynamics/collisions/__init__.py
+++ PySDM/dynamics/collisions/__init__.py
@@ -1,1 +1,5 @@
+"""
+collisions-related logic including the `PySDM.dynamics.collisions.Coalescence`
+ dynamic and coalescence ``PySDM.dynamics.collisions.kernels`
+"""
 from .coalescence import Coalescence
