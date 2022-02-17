d1759d2f233e207c3752f9c23120ed00a4acdbee
Emily de Jong <edejong@caltech.edu>
2022-02-17 10:48:55 -0800

    Rename collision_time_step to close #761

--- PySDM/products/collision/coalescence_timestep_min.py
+++ PySDM/products/collision/coalescence_timestep_min.py
@@ -1,22 +1,22 @@
 """
-Minimal coalescence timestep used when adaptive timestepping is enabled in the
+Minimal collision timestep used when adaptive timestepping is enabled in the
  `PySDM.dynamics.collisions.collision.Collision` dynamic (fetching a value resets the counter)
 """
 from PySDM.products.impl.product import Product
 
 # TODO #761
-class CoalescenceTimestepMin(Product):
+class CollisionTimestepMin(Product):
     def __init__(self, unit='s', name=None):
         super().__init__(unit=unit, name=name)
-        self.coalescence = None
+        self.collision = None
         self.range = None
 
     def register(self, builder):
         super().register(builder)
-        self.coalescence = self.particulator.dynamics['Collision']
-        self.range = self.coalescence.dt_coal_range
+        self.collision = self.particulator.dynamics['Collision']
+        self.range = self.collision.dt_coal_range
 
     def _impl(self, **kwargs):
-        self._download_to_buffer(self.coalescence.stats_dt_min)
-        self.coalescence.stats_dt_min[:] = self.coalescence.particulator.dt
+        self._download_to_buffer(self.collision.stats_dt_min)
+        self.collision.stats_dt_min[:] = self.collision.particulator.dt
         return self.buffer
