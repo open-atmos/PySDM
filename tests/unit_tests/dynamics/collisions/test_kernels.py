713139498063f85100c85a97e9eaf5846000ef0f
Emily de Jong <emilydejong@Emilys-MacBook-Pro.local>
2022-04-13 14:44:35 -0700

    Writing kernel tests

--- tests/unit_tests/dynamics/collisions/test_kernels.py
+++ tests/unit_tests/dynamics/collisions/test_kernels.py
@@ -4,7 +4,11 @@ import pytest
 
 from PySDM import Builder
 from PySDM.backends import CPU
-from PySDM.dynamics.collisions.collision_kernels import Geometric, Golovin
+from PySDM.dynamics.collisions.collision_kernels import (
+    Geometric,
+    Golovin,
+    SimpleGeometric,
+)
 from PySDM.environments import Box
 from PySDM.formulae import Formulae
 
@@ -51,4 +55,58 @@ class TestKernels:
         sut(output, is_first_in_pair=is_first_in_pair)
 
         # assert
-        np.testing.assert_allclose(output.to_ndarray(), [np.nan, np.nan])
+        np.testing.assert_array_less(
+            [0.0, 0.0],
+            output.to_ndarray(),
+        )
+
+    @staticmethod
+    @pytest.mark.parametrize("C", (0.0, 1.0))
+    def test_simple_geometric(C):
+        # arrange
+        volume = np.asarray([44.0, 666.0])
+
+        builder = Builder(backend=CPU(), n_sd=volume.size)
+        sut = SimpleGeometric(C=C)
+        sut.register(builder)
+        builder.set_environment(Box(dv=None, dt=None))
+        _ = builder.build(attributes={"volume": volume, "n": np.ones_like(volume)})
+
+        _PairwiseStorage = builder.particulator.PairwiseStorage
+        _Indicator = builder.particulator.PairIndicator
+        output = _PairwiseStorage.from_ndarray(np.zeros_like(volume))
+        is_first_in_pair = _Indicator(length=volume.size)
+
+        # act
+        sut(output, is_first_in_pair=is_first_in_pair)
+
+        # assert
+        if C > 0.0:
+            np.testing.assert_array_less([0.0, 0.0], output.to_ndarray())
+        else:
+            np.testing.assert_array_equal([0.0, 0.0], output.to_ndarray())
+
+    @staticmethod
+    @pytest.mark.parametrize("volume", (np.array([1.0, 2.0]), np.array([1.0, 1.0])))
+    def test_simple_geometric_same_size(volume):
+        # arrange
+
+        builder = Builder(backend=CPU(), n_sd=volume.size)
+        sut = SimpleGeometric(C=1.0)
+        sut.register(builder)
+        builder.set_environment(Box(dv=None, dt=None))
+        _ = builder.build(attributes={"volume": volume, "n": np.ones_like(volume)})
+
+        _PairwiseStorage = builder.particulator.PairwiseStorage
+        _Indicator = builder.particulator.PairIndicator
+        output = _PairwiseStorage.from_ndarray(np.zeros_like(volume))
+        is_first_in_pair = _Indicator(length=volume.size)
+
+        # act
+        sut(output, is_first_in_pair=is_first_in_pair)
+
+        # assert
+        if volume[0] == volume[1]:
+            np.testing.assert_array_equal([0.0, 0.0], output.to_ndarray())
+        else:
+            np.testing.assert_array_less([0.0, 0.0], output.to_ndarray())
