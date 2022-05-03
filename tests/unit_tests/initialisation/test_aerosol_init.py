fdcda9a53d30d0f550d8e9a1247d9ac6aacb12b8
claresinger <csinger@caltech.edu>
2022-05-03 12:49:58 -0700

    fix pylint and formatting

--- tests/unit_tests/initialisation/test_aerosol_init.py
+++ tests/unit_tests/initialisation/test_aerosol_init.py
@@ -1,36 +1,36 @@
+# pylint: disable=missing-module-docstring
 import numpy as np
 import pytest
-
 from chempy import Substance
+
 # from PySDM.initialisation import spectra
 from PySDM.initialisation.aerosol_composition import DryAerosolMixture
 from PySDM.physics import si
 
 # spectrum = spectra.Lognormal(norm_factor=100.0 / si.cm**3, m_mode=50.0 * si.nm, s_geom=2.0)
 
+
 @pytest.mark.parametrize(
     "mass_fractions",
     [
-        pytest.param({"(NH4)2SO4":1.0, "insoluble":0.0}),
-        pytest.param({"(NH4)2SO4":0.5, "insoluble":0.5}),
-        pytest.param({"(NH4)2SO4":0.0, "insoluble":1.0}),
+        pytest.param({"(NH4)2SO4": 1.0, "insoluble": 0.0}),
+        pytest.param({"(NH4)2SO4": 0.5, "insoluble": 0.5}),
+        pytest.param({"(NH4)2SO4": 0.0, "insoluble": 1.0}),
     ],
 )
-def test_aerosol_init(mass_fractions, plot=False):
+def test_aerosol_init(mass_fractions):
     # Arrange
-    compounds=("(NH4)2SO4", "insoluble")
-    molar_masses={
-        "(NH4)2SO4": Substance.from_formula("(NH4)2SO4").mass
-        * si.gram
-        / si.mole,
+    compounds = ("(NH4)2SO4", "insoluble")
+    molar_masses = {
+        "(NH4)2SO4": Substance.from_formula("(NH4)2SO4").mass * si.gram / si.mole,
         "insoluble": 44 * si.g / si.mole,
     }
-    densities={
+    densities = {
         "(NH4)2SO4": 1.77 * si.g / si.cm**3,
-        "insoluble": 1.77 * si.g / si.cm**3,
+        "insoluble": 1.2 * si.g / si.cm**3,
     }
-    is_soluble={"(NH4)2SO4": True, "insoluble": False}
-    ionic_dissociation_phi={"(NH4)2SO4": 3, "insoluble": 0}
+    is_soluble = {"(NH4)2SO4": True, "insoluble": False}
+    ionic_dissociation_phi = {"(NH4)2SO4": 3, "insoluble": 0}
 
     aer = DryAerosolMixture(
         compounds=compounds,
@@ -39,17 +39,15 @@ def test_aerosol_init(mass_fractions, plot=False):
         is_soluble=is_soluble,
         ionic_dissociation_phi=ionic_dissociation_phi,
     )
-    
+
     # Act
     kappa = aer.kappa(mass_fractions)
-    vol_fracs = aer.volume_fractions(mass_fractions)
+    volume_fractions = aer.volume_fractions(mass_fractions)
 
     # Assert
-    kappas = {"(NH4)2SO4":0.72, "insoluble":0.0}
-    print(sum(kappas[k]*mass_fractions[k] for k in kappas))
+    kappas = {"(NH4)2SO4": 0.72, "insoluble": 0.0}
     np.testing.assert_approx_equal(
         kappa["Constant"],
-        sum(kappas[k]*mass_fractions[k] for k in kappas),
-        significant=2
+        sum(kappas[k] * volume_fractions[k] for k in kappas.keys()),
+        significant=2,
     )
-
