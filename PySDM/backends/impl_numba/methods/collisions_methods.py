b9e457806a3bd257ed3a660d037f14dec70c0c8b
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-11 09:52:20 -0500

    add # pylint: disable=too-many-arguments for __fragmentation_limiters()

--- PySDM/backends/impl_numba/methods/collisions_methods.py
+++ PySDM/backends/impl_numba/methods/collisions_methods.py
@@ -355,6 +355,7 @@ class CollisionsMethods(BackendMethods):
 
     @staticmethod
     @numba.njit(**{**conf.JIT_FLAGS})
+    # pylint: disable=too-many-arguments
     def __fragmentation_limiters(n_fragment, frag_size, v_max, vmin, nfmax, x_plus_y):
         for i in numba.prange(len(n_fragment)):  # pylint: disable=not-an-iterable
             n_fragment[i] = x_plus_y[i] / frag_size[i]
