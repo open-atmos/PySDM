af51028bd99ba337e3a2a5c56a5c93e03cc00f8f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-09-27 23:19:31 +0200

    add test for c_inline (checking the new trailing come avoidance logic)

---
+++ tests/unit_tests/test_formulae.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+from PySDM import formulae
+
+
+def test_c_inline():
+    # arrange
+    def fun(_, xxx):
+        return min(
+            xxx,
+            2,
+        )
+
+    # act
+    c_code = formulae._c_inline(fun, constants={'pi': 3.14}, xxx=0)  # pylint: disable=protected-access
+
+    # assert
+    assert ", )" not in c_code
