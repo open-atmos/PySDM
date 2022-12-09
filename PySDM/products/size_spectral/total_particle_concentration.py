145a213140e0430902234c21171961c662de6e45
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-12-10 00:46:05 +0100

    use ConcentrationProduct in total particle conc. products

--- PySDM/products/size_spectral/total_particle_concentration.py
+++ PySDM/products/size_spectral/total_particle_concentration.py
@@ -1,14 +1,13 @@
 """
 particle concentration (per volume of air)
 """
-from PySDM.products.impl.moment_product import MomentProduct
+from PySDM.products.impl.concentration_product import ConcentrationProduct
 
 
-class TotalParticleConcentration(MomentProduct):
-    def __init__(self, name=None, unit="m^-3"):
-        super().__init__(name=name, unit=unit)
+class TotalParticleConcentration(ConcentrationProduct):
+    def __init__(self, name=None, unit="m^-3", stp=False):
+        super().__init__(name=name, unit=unit, specific=False, stp=stp)
 
     def _impl(self, **kwargs):
         self._download_moment_to_buffer(attr="volume", rank=0)
-        self.buffer[:] /= self.particulator.mesh.dv
-        return self.buffer
+        return super()._impl(**kwargs)
