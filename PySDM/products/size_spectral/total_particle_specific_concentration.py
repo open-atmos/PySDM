b9c013ea9526d93dce740b9e5471994841108f88
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-12-16 09:12:28 +0100

    common code for computing concentrations at STP (#974)

--- PySDM/products/size_spectral/total_particle_specific_concentration.py
+++ PySDM/products/size_spectral/total_particle_specific_concentration.py
@@ -1,17 +1,13 @@
 """
 particle specific concentration (per mass of dry air)
 """
-from PySDM.products.impl.moment_product import MomentProduct
+from PySDM.products.impl.concentration_product import ConcentrationProduct
 
 
-class TotalParticleSpecificConcentration(MomentProduct):
+class TotalParticleSpecificConcentration(ConcentrationProduct):
     def __init__(self, name=None, unit="kg^-1"):
-        super().__init__(name=name, unit=unit)
+        super().__init__(name=name, unit=unit, stp=False, specific=True)
 
     def _impl(self, **kwargs):
         self._download_moment_to_buffer(attr="volume", rank=0)
-        result = self.buffer.copy()  # TODO #217
-        self._download_to_buffer(self.particulator.environment["rhod"])
-        result[:] /= self.particulator.mesh.dv
-        result[:] /= self.buffer
-        return result
+        return super()._impl(**kwargs)
