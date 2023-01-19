0efdb3bf34a6c9be09677138eb34172df1167cf1
Hlib Hraif <hlib.hraif@protonmail.com>
2023-01-19 13:20:22 +0100

    Try reverting `shuffle_local`

--- PySDM/backends/impl_numba/methods/index_methods.py
+++ PySDM/backends/impl_numba/methods/index_methods.py
@@ -71,7 +71,9 @@ class IndexMethods(BackendMethods):
     @numba.njit(**conf.JIT_FLAGS)
     def shuffle_local(idx, u01, cell_start):
         for c in numba.prange(len(cell_start) - 1):  # pylint: disable=not-an-iterable
-            fisher_yates_shuffle(idx, u01, cell_start[c], cell_start[c + 1])
+            for i in range(cell_start[c + 1] - 1, cell_start[c], -1):
+                j = int(cell_start[c] + u01[i] * (cell_start[c + 1] - cell_start[c]))
+                idx[i], idx[j] = idx[j], idx[i]
 
     @staticmethod
     def sort_by_key(idx, attr):
