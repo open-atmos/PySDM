71a7d4dcd9344aa04a19a5c7dbb5a5210bb1625d
edejong-caltech <edejong@caltech.edu>
2023-02-10 12:02:03 -0700

    Switch from tanh to erf in gaussian CDF

--- PySDM/physics/fragmentation_function/gaussian.py
+++ PySDM/physics/fragmentation_function/gaussian.py
@@ -3,6 +3,8 @@ Gaussian PDF
 CDF = 1/2(1 + erf(x/sqrt(2)));
 approximate as erf(x) ~ tanh(ax) with a = sqrt(pi)log(2) as in Vedder 1987
 """
+import math
+
 import numpy as np
 
 
@@ -12,6 +14,7 @@ class Gaussian:  # pylint: disable=too-few-public-methods
 
     @staticmethod
     def frag_size(const, mu, sigma, rand):
-        return mu - sigma / const.sqrt_two / const.sqrt_pi / np.log(2) * np.log(
-            (0.5 + rand) / (1.5 - rand)
-        )
+        return mu + sigma / 2 * (1 + math.erf(rand / const.sqrt_two))
+        # return mu - sigma / const.sqrt_two / const.sqrt_pi / np.log(2) * np.log(
+        #     (0.5 + rand) / (1.5 - rand)
+        # )
