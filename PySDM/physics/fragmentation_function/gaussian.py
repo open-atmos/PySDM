9989efe95056e67664c1fd71127085c13f0626e5
edejong-caltech <edejong@caltech.edu>
2023-02-13 11:51:09 -0700

    Linting

--- PySDM/physics/fragmentation_function/gaussian.py
+++ PySDM/physics/fragmentation_function/gaussian.py
@@ -5,8 +5,6 @@ approximate as erf(x) ~ tanh(ax) with a = sqrt(pi)log(2) as in Vedder 1987
 """
 import math
 
-import numpy as np
-
 
 class Gaussian:  # pylint: disable=too-few-public-methods
     def __init__(self, _):
@@ -15,6 +13,3 @@ class Gaussian:  # pylint: disable=too-few-public-methods
     @staticmethod
     def frag_size(const, mu, sigma, rand):
         return mu + sigma / 2 * (1 + math.erf(rand / const.sqrt_two))
-        # return mu - sigma / const.sqrt_two / const.sqrt_pi / np.log(2) * np.log(
-        #     (0.5 + rand) / (1.5 - rand)
-        # )
