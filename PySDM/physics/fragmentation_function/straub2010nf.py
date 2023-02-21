2067f36bdf0e09f61ffc646f794fc945475d6f54
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-02-21 23:43:46 +0100

    make Straub formulae CUDA-compatible

--- PySDM/physics/fragmentation_function/straub2010nf.py
+++ PySDM/physics/fragmentation_function/straub2010nf.py
@@ -13,7 +13,12 @@ class Straub2010Nf:  # pylint: disable=too-few-public-methods
     @staticmethod
     def sigma1(const, CW):
         return np.sqrt(
-            np.log((0.0125 * CW**0.5) ** 2 / 12 / const.STRAUB_E_D1**2 + 1)
+            np.log(
+                np.power((np.sqrt(CW) / 8) / 10, 2)
+                / 12
+                / np.power(const.STRAUB_E_D1, const.TWO)
+                + 1
+            )
         )
 
     @staticmethod
@@ -21,16 +26,18 @@ class Straub2010Nf:  # pylint: disable=too-few-public-methods
         return (
             const.PI
             / 6
-            * np.exp(
-                np.log(const.STRAUB_E_D1)
-                - sigma1**2 / 2
-                - sigma1
-                / const.sqrt_two
-                / const.sqrt_pi
-                / const.LN_2
-                * np.log((0.5 + rand) / (1.5 - rand))
+            * np.power(
+                np.exp(
+                    np.log(const.STRAUB_E_D1)
+                    - np.power(sigma1, const.TWO) / 2
+                    - sigma1
+                    / const.sqrt_two
+                    / const.sqrt_pi
+                    / const.LN_2
+                    * np.log((1 / const.TWO + rand) / (const.THREE / const.TWO - rand))
+                ),
+                const.THREE,
             )
-            ** 3
         )
 
     @staticmethod
@@ -38,15 +45,15 @@ class Straub2010Nf:  # pylint: disable=too-few-public-methods
         return (
             const.PI
             / 6
-            * (
+            * np.power(
                 const.STRAUB_MU2
-                - ((0.007 * (CW - 21.0)) ** 2 / 12)
+                - (np.power(7 * (CW - 21) / 1000, const.TWO) / 12)
                 / const.sqrt_two
                 / const.sqrt_pi
                 / const.LN_2
-                * np.log((0.5 + rand) / (1.5 - rand))
+                * np.log((1 / const.TWO + rand) / (const.THREE / const.TWO - rand)),
+                const.THREE,
             )
-            ** 3
         )
 
     @staticmethod
@@ -54,15 +61,15 @@ class Straub2010Nf:  # pylint: disable=too-few-public-methods
         return (
             const.PI
             / 6
-            * (
-                (0.9 * ds)
-                - ((0.01 * (0.76 * CW**0.5 + 1.0)) ** 2 / 12)
+            * np.power(
+                (9 * ds / 10)
+                - (np.power((76 * np.sqrt(CW) / 100 + 1) / 100, const.TWO) / 12)
                 / const.sqrt_two
                 / const.sqrt_pi
                 / const.LN_2
-                * np.log((0.5 + rand) / (1.5 - rand))
+                * np.log((1 / const.TWO + rand) / (const.THREE / const.TWO - rand)),
+                const.THREE,
             )
-            ** 3
         )
 
     @staticmethod
@@ -72,25 +79,39 @@ class Straub2010Nf:  # pylint: disable=too-few-public-methods
             / 6
             * (
                 v_max / const.PI_4_3 * 8
-                + ds**3
+                + np.power(ds, const.THREE)
                 - Nr1
                 * np.exp(
                     3 * np.log(const.STRAUB_E_D1)
                     + 6
                     * np.log(
-                        (0.0125 * CW**0.5) ** 2 / 12 / const.STRAUB_E_D1**2 + 1
+                        np.power((np.sqrt(CW) / 8) / 10, const.TWO)
+                        / 12
+                        / np.power(const.STRAUB_E_D1, const.TWO)
+                        + 1
                     )
                     / 2
                 )
                 - Nr2
                 * (
-                    const.STRAUB_MU2**3
-                    + 3 * const.STRAUB_MU2 * ((0.007 * (CW - 21.0)) ** 2 / 12) ** 2
+                    np.power(const.STRAUB_MU2, const.THREE)
+                    + 3
+                    * const.STRAUB_MU2
+                    * np.power(
+                        np.power(7 * (CW - 21) / 1000, const.TWO) / 12, const.TWO
+                    )
                 )
                 - Nr3
                 * (
-                    (0.9 * ds) ** 3
-                    + 3 * 0.9 * ds * ((0.01 * (0.76 * CW**0.5 + 1.0)) ** 2 / 12) ** 2
+                    np.power(9 * ds / 10, const.THREE)
+                    + 3
+                    * 9
+                    * ds
+                    / 10
+                    * np.power(
+                        np.power((76 * np.sqrt(CW) / 100 + 1) / 100, const.TWO) / 12,
+                        const.TWO,
+                    )
                 )
             )
         )
