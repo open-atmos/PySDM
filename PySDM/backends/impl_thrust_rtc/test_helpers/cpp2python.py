9d1cc63fc6aa382c18f9490e46c6a1d255f0c0d3
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-03-31 00:32:51 +0200

    remove erf additions

--- PySDM/backends/impl_thrust_rtc/test_helpers/cpp2python.py
+++ PySDM/backends/impl_thrust_rtc/test_helpers/cpp2python.py
@@ -189,7 +189,6 @@ def to_numba(name, args, iter_var, body):
     result = (
         f"""
 def make(self):
-    from math import erf
     import numpy as np
     from numpy import floor, ceil, exp, log, power, sqrt, arctanh
     import numba
