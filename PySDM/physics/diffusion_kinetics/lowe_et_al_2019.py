ee83617d4d8b8773a0a905ff66fd32f8f853e2e8
claresinger <csinger@caltech.edu>
2023-05-18 14:10:25 -0700

    add doc, change dv=0 to const.l19_dv

--- PySDM/physics/diffusion_kinetics/lowe_et_al_2019.py
+++ PySDM/physics/diffusion_kinetics/lowe_et_al_2019.py
@@ -1,6 +1,7 @@
 """
-eq. 13-14 in Pruppacher & Klett 2005 for Delta v = 0
-no corrections for thermal conductivity
+as in [Lowe et al. 2019](https://doi.org/10.1038/s41467-019-12982-0)
+uses eq. 13-14 in Pruppacher & Klett 2005 with Delta v = 0
+and no corrections for thermal conductivity
 """
 from PySDM.physics.diffusion_kinetics.pruppacher_and_klett_2005 import PruppacherKlett
 
@@ -19,4 +20,4 @@ class LoweEtAl2019(PruppacherKlett):
 
     @staticmethod
     def D(const, D, r, lmbd):
-        return super().D(const, D, r, lmbd, 0.0)
+        return super().D(const, D, r, lmbd, const.l19_dv)
