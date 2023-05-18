3721e4344e3ad36e48fd64f3939d9c8915615b5c
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-18 13:11:46 +0200

    pylint-disable comment

--- PySDM/physics/diffusion_thermics/seinfeld_and_pandis_2010.py
+++ PySDM/physics/diffusion_thermics/seinfeld_and_pandis_2010.py
@@ -4,7 +4,7 @@ as in Seinfeld and Pandis 2010 (eq. 15.65)
 import numpy as np
 
 
-class SeinfeldAndPandis2010:
+class SeinfeldAndPandis2010:  # pylint: disable=too-few-public-methods
     def __init__(self, _):
         pass
 
