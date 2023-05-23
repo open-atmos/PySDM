244d29dc29767d9d04068d331bccdd738c521cca
claresinger <csinger@caltech.edu>
2023-05-23 10:33:38 -0700

    use units

--- PySDM/physics/latent_heat/lowe2019.py
+++ PySDM/physics/latent_heat/lowe2019.py
@@ -4,9 +4,11 @@ using equation form from Seinfeld and Pandis, with constant values from ICPM cod
 """
 from PySDM.physics.latent_heat.seinfeld_and_pandis_2010 import SeinfeldPandis
 
+from ..constants import si
+
 
 class Lowe2019(SeinfeldPandis):  # pylint: disable=too-few-public-methods
     def __init__(self, const):
-        assert const.l_l19_a == 0.167
-        assert const.l_l19_b == 3.65e-4
+        assert const.l_l19_a == 0.167 * si.dimensionless
+        assert const.l_l19_b == 3.65e-4 / si.kelvin
         SeinfeldPandis.__init__(self, const)
