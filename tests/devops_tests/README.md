498f47debc60dd5fcc2ff69f15376d353c08c8a4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-25 17:31:32 +0200

    devops_tests, pdoc and sit wflow fixes

---
+++ tests/devops_tests/README.md
@@ -0,0 +1,2 @@
+# devops_tests
+pytest test routines asserting for GitHub issue-linked TO-DO labelling in the code, README link consistency and some Jupyter notebook sanity checks
--- tests/devops_tests/test_todos_annotated.py
+++ tests/devops_tests/test_todos_annotated.py
@@ -1,85 +1,74 @@
 """ utilities to ensure all TO-DO comments in the code are annotated
     with an id of an open GitHub issue """
+
 import os
-import pathlib
 import re
-import sys
 
 import pytest
-from fastcore.net import ExceptionsHTTP
+from binaryornot.check import is_binary
 from ghapi.all import GhApi, paged
+from git.cmd import Git
 
 
-# https://stackoverflow.com/questions/7012921/recursive-grep-using-python
-def findfiles(path, regex):
+def _grep(filepath, regex):
     reg_obj = re.compile(regex)
     res = []
-    for root, _, fnames in os.walk(path):
-        for fname in fnames:
-            if reg_obj.match(fname):
-                res.append(os.path.join(root, fname))
-    return res
-
-
-def grep(filepath, regex):
-    reg_obj = re.compile(regex)
-    res = []
-    with open(filepath, encoding="utf8") as f:
-        for line in f:
+    with open(filepath, encoding="utf8") as file_lines:
+        for line in file_lines:
             if reg_obj.match(line):
                 res.append(line)
     return res
 
 
 @pytest.fixture(
-    params=findfiles(
-        pathlib.Path(__file__).parent.parent.parent.absolute(),
-        r".*\.(ipynb|py|txt|yml|m|jl|md)$",
-    )
+    params=(
+        path
+        for path in Git(Git(".").rev_parse("--show-toplevel")).ls_files().split("\n")
+        if os.path.isfile(path)
+    ),
+    name="git_tracked_file",
 )
-def file(request):
+def _git_tracked_file(request):
     return request.param
 
 
-@pytest.fixture(scope="session")
-def gh_issues():
+@pytest.fixture(scope="session", name="gh_issues")
+def _gh_issues():
     res = {}
-    if "CI" not in os.environ or (
-        "GITHUB_ACTIONS" in os.environ and sys.version_info.minor >= 8
-    ):
-        try:
-            api = GhApi(owner="open-atmos", repo="PySDM")
-            pages = paged(
-                api.issues.list_for_repo,
-                owner="open-atmos",
-                repo="PySDM",
-                state="all",
-                per_page=100,
-            )
-            for page in pages:
-                for item in page.items:
-                    res[item.number] = item.state
-        except ExceptionsHTTP[403]:
-            pass
+    repo = os.path.basename(Git(".").rev_parse("--show-toplevel"))
+    api = GhApi(owner="open-atmos", repo=repo)
+    pages = paged(
+        api.issues.list_for_repo,
+        owner="open-atmos",
+        repo=repo,
+        state="all",
+        per_page=100,
+    )
+    for page in pages:
+        for item in page.items:
+            res[item.number] = item.state
     return res
 
 
-# pylint: disable=redefined-outer-name
-def test_todos_annotated(file, gh_issues):
-    if (
-        os.path.basename(file) == "test_todos_annotated.py"
-        or file.endswith("-checkpoint.ipynb")
-        or ".eggs" in file
-    ):
-        return
-    for line in grep(file, r".*TODO.*"):
-        match = re.search(r"TODO #(\d+)", line)
+def test_todos_annotated(git_tracked_file, gh_issues):
+    """raises assertion errors if a (TODO|FIXME) is not annotated or if the annotation
+    does not point to an open issue"""
+    if is_binary(git_tracked_file):
+        pytest.skip("binary file")
+    for line in _grep(git_tracked_file, r".*(TODO|FIXME).*"):
+        if "(TODO|FIXME)" in line:
+            continue
+        match = re.search(r"(TODO|FIXME) #(\d+)", line)
         if match is None:
-            raise AssertionError(f"TODO not annotated with issue id ({line})")
+            raise AssertionError(f"(TODO|FIXME) not annotated with issue id ({line})")
         giving_up_with_hope_other_builds_did_it = len(gh_issues) == 0
         if not giving_up_with_hope_other_builds_did_it:
-            number = int(match.group(1))
+            number = int(match.group(2))
             if number not in gh_issues.keys():
-                raise AssertionError(f"TODO annotated with non-existent id ({line})")
+                raise AssertionError(
+                    f"(TODO|FIXME) annotated with non-existent id ({line})"
+                )
             if gh_issues[number] != "open":
-                raise AssertionError(f"TODO remains for a non-open issue ({line})")
+                raise AssertionError(
+                    f"(TODO|FIXME) remains for a non-open issue ({line})"
+                )
