2e56cc38325164b129a18bdd19976e65b2dddb38
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-26 23:36:27 +0200

    make Lowe2019 ctor DimensionalAnalysis-compatible

--- PySDM/physics/latent_heat/lowe2019.py
+++ PySDM/physics/latent_heat/lowe2019.py
@@ -2,13 +2,12 @@
 temperature-dependent latent heat of vaporization used in Lowe et al. 2019
 using equation form from Seinfeld and Pandis, with constant values from ICPM code
 """
+from PySDM.physics import constants_defaults
 from PySDM.physics.latent_heat.seinfeld_and_pandis_2010 import SeinfeldPandis
-
-from ..constants import si
 
 
 class Lowe2019(SeinfeldPandis):  # pylint: disable=too-few-public-methods
     def __init__(self, const):
-        assert const.l_l19_a == 0.167 * si.dimensionless
-        assert const.l_l19_b == 3.65e-4 / si.kelvin
+        assert const.l_l19_a == constants_defaults.l_l19_a
+        assert const.l_l19_b == constants_defaults.l_l19_b
         SeinfeldPandis.__init__(self, const)
