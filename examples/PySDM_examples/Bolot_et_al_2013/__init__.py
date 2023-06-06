083d05f13200153bba0ee9ba8b42b62b9e096f8c
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-06-06 15:28:47 +0200

    migrate examples PR into PySDM PR

---
+++ examples/PySDM_examples/Bolot_et_al_2013/__init__.py
@@ -0,0 +1,5 @@
+"""
+figures from Bolot et al. 2013 (Atmos. Chem. Phys. 13)
+https://doi.org/10.5194/acp-13-7903-2013
+"""
+# pylint: disable=invalid-name
---
+++ examples/PySDM_examples/Merlivat_and_Nief_1967/__init__.py
@@ -0,0 +1,5 @@
+"""
+figure from Merlivat and Nief 1967 (Tellus)
+https://doi.org/10.3402/tellusa.v19i1.9756
+"""
+# pylint: disable=invalid-name
