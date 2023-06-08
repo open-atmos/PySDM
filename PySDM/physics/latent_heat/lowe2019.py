9d955a903ce0d6a9414511be338a3e23e5504b89
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2023-06-08 11:05:21 +0200

    clarify where the Lowe et al. 2019 formulae come from (#1072)
    Co-authored-by: claresinger <csinger@caltech.edu>

--- PySDM/physics/latent_heat/lowe2019.py
+++ PySDM/physics/latent_heat/lowe2019.py
@@ -1,12 +1,13 @@
 """
 temperature-dependent latent heat of vaporization used in Lowe et al. 2019
+using equation form from Seinfeld and Pandis, with constant values from ICPM code
 """
+from PySDM.physics import constants_defaults
+from PySDM.physics.latent_heat.seinfeld_and_pandis_2010 import SeinfeldPandis
 
 
-class Lowe2019:  # pylint: disable=too-few-public-methods
-    def __init__(self, _):
-        pass
-
-    @staticmethod
-    def lv(const, T):
-        return const.l_tri * (const.T_tri / T) ** (const.l_l19_a + const.l_l19_b * T)
+class Lowe2019(SeinfeldPandis):  # pylint: disable=too-few-public-methods
+    def __init__(self, const):
+        assert const.l_l19_a == constants_defaults.l_l19_a
+        assert const.l_l19_b == constants_defaults.l_l19_b
+        SeinfeldPandis.__init__(self, const)
