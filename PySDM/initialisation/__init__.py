066f59f9028a98f84d4beb9971e865e05dff1773
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-06-29 17:24:30 -0700

    added function to initialize fall momentum

--- PySDM/initialisation/__init__.py
+++ PySDM/initialisation/__init__.py
@@ -4,3 +4,4 @@ wet radii equilibration
 """
 from .discretise_multiplicities import discretise_multiplicities
 from .equilibrate_wet_radii import equilibrate_wet_radii
+from .init_fall_momenta import init_fall_momenta
