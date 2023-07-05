c30d3a4a2be9b813158f3753b82a7367a919a632
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-07-05 15:47:14 -0700

    update momentum using a relaxation timescale in `RelaxedVelocity` dynamic

--- PySDM/dynamics/relaxed_velocity.py
+++ PySDM/dynamics/relaxed_velocity.py
@@ -8,28 +8,33 @@ from PySDM.attributes.impl.attribute import Attribute
 from PySDM.particulator import Particulator
 from PySDM.physics import si
 
+import numpy as np
+
 DEFAULTS = namedtuple("_", ("rtol", "adaptive"))(rtol=1e-2, adaptive=True)
 
 
 class RelaxedVelocity:  # pylint: disable=too-many-instance-attributes
-    def __init__(self, tau=1*si.second):
-        self.particulator: Particulator = None
-        # self.delta_v = None
-        # self.tau = tau
+    def __init__(self, tau=100*si.second):
+        self.tau: float = tau
 
     def register(self, builder):
-        self.particulator = builder.particulator
+        self.particulator: Particulator = builder.particulator
         
-        self.fall_momentum_attr = builder.get_attribute("fall momentum")
-        self.terminal_vel_attr = builder.get_attribute("terminal velocity")
-        self.volume_attr = builder.get_attribute("volume")
+        self.fall_momentum_attr: Attribute = builder.get_attribute("fall momentum")
+        self.terminal_vel_attr: Attribute = builder.get_attribute("terminal velocity")
+        self.volume_attr: Attribute = builder.get_attribute("volume")
         
-        self.rho_w = builder.formulae.constants.rho_w
+        self.rho_w: float = builder.formulae.constants.rho_w
 
-        # self.delta_v_data = self.particulator.Storage.empty((self.particulator.n_sd,))
+        self.tmp_data = self.particulator.Storage.empty((self.particulator.n_sd,), dtype=float)
 
     def __call__(self):
-        self.fall_momentum_attr.data.product(self.terminal_vel_attr.get(), self.volume_attr.get())
-        self.fall_momentum_attr.data[:] *= self.rho_w
+        self.tmp_data.product(self.terminal_vel_attr.get(), self.volume_attr.get())
+        self.tmp_data *= self.rho_w
+        self.tmp_data -= self.fall_momentum_attr.get()
+        self.tmp_data *= 1 - np.exp(-self.particulator.dt/self.tau)
+
+        self.fall_momentum_attr.data += self.tmp_data
 
         self.particulator.attributes.mark_updated("fall momentum")
+
