2fd4881d4859b546fd330a62712f7c450c66c075
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-07-06 15:16:12 -0700

    completed animated plots for 0d example

--- examples/PySDM_examples/Bhalla_2023/settings_0D.py
+++ examples/PySDM_examples/Bhalla_2023/settings_0D.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 from PySDM_examples.Shima_et_al_2009.settings import Settings as Settings_Shima
 
 from PySDM.dynamics.collisions.collision_kernels import Golovin, Geometric
@@ -8,18 +8,20 @@ import numpy as np
 
 
 class Settings(Settings_Shima):
-    def __init__(self, n_sd: Union[int, None] = None, steps: Union[list[int], None] = None, n_part: Union[float, None] = None, evaluate_relaxed_velocity=False):
+    def __init__(self, n_sd: Union[int, None] = None, max_t: Optional[int] = None, n_part: Optional[float] = None, evaluate_relaxed_velocity=False):
         super().__init__()
 
         self.n_sd = n_sd or 2**13
-        self.steps = steps or [0, 1200, 2400, 3600]
+        self.max_t = max_t or 3600
+        self.steps = np.arange(0, self.max_t+1, 50)
 
         self.X0 = self.formulae.trivia.volume(radius=30.531 * si.micrometres)
 
-        self.kernel: Union[Golovin, Geometric] = Golovin(b=1.5e3 / si.second)
+        # self.kernel: Union[Golovin, Geometric] = Golovin(b=1.5e3 / si.second)
+        self.kernel: Union[Golovin, Geometric] = Geometric()
 
         self.radius_bins_edges = np.logspace(
-            np.log10(10 * si.um), np.log10(5e3 * si.um), num=128, endpoint=True
+            np.log10(10 * si.um), np.log10(5e4 * si.um), num=128, endpoint=True
         )
 
         self.n_part = n_part or 2**23 / si.metre**3
