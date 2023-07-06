508dee7e9892a6956f3633eea74e44dd038b935f
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-07-06 16:55:04 -0700

    initialize to predicted values from approximation (or to 0), currently uses RogersYau approx by default

--- PySDM/initialisation/init_fall_momenta.py
+++ PySDM/initialisation/init_fall_momenta.py
@@ -1,11 +1,11 @@
 import numpy as np
 from PySDM.backends import CPU
-from PySDM.dynamics.terminal_velocity import Interpolation
+from PySDM.dynamics.terminal_velocity import Interpolation, RogersYau
 from PySDM.formulae import Formulae
 from PySDM.particulator import Particulator
 
 
-def init_fall_momenta(volume: np.ndarray, rho_w: float):
+def init_fall_momenta(volume: np.ndarray, rho_w: float, zero: bool = False, terminal_velocity_approx = RogersYau):
     """
     Calculate default values of the FallMomentum attribute
     (needed when using FallVelocity attribute)
@@ -17,6 +17,18 @@ def init_fall_momenta(volume: np.ndarray, rho_w: float):
     Returns:
         - a numpy array of initial momentum values
     """
-    # TODO: good luck
-    # for now it just makes velocity 4
-    return volume*rho_w*4
+    if zero:
+        return np.zeros_like(volume)
+
+    particulator = Particulator(0, CPU(Formulae()))
+
+    approximation = terminal_velocity_approx(particulator=particulator)
+
+    radii_arr = particulator.formulae.trivia.radius(volume)
+    radii = particulator.Storage.from_ndarray(radii_arr)
+
+    output = particulator.Storage.empty((len(volume),), dtype=float)
+
+    approximation(output=output, radius=radii)
+
+    return output.to_ndarray() * volume * rho_w 
