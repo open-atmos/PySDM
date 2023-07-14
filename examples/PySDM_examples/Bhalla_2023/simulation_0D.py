231f1d3edc8f88f137d750539cbb032e94af27fc
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-07-13 19:54:31 -0700

    simplify 0D simulation

--- examples/PySDM_examples/Bhalla_2023/simulation_0D.py
+++ examples/PySDM_examples/Bhalla_2023/simulation_0D.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional, Type, Union
+from PySDM_examples.Bhalla_2023.plot_terminal_velocity_approx import get_approx
 import numpy as np
 from PySDM_examples.Shima_et_al_2009.spectrum_plotter import SpectrumColors
 import matplotlib.pyplot as plt
@@ -31,25 +32,37 @@ class Simulation:
         self.builder.set_environment(
             Box(dv=self.settings.dv, dt=self.settings.dt))
 
+        ##############
+        # ATTRIBUTES #
+        ##############
         attributes = {}
 
         sampling = ConstantMultiplicity(self.settings.spectrum)
         attributes["volume"], attributes["n"] = sampling.sample(
             self.settings.n_sd)
 
+        self.builder.request_attribute("fall velocity")
+        attributes["fall momentum"] = init_fall_momenta(
+            attributes["volume"], self.builder.formulae.constants.rho_w)
+
+        self.builder.request_attribute("radius")
+
+        ############
+        # DYNAMICS #
+        ############
+
+        relaxed_velocity = RelaxedVelocity(tau=self.settings.tau)
+        self.builder.add_dynamic(relaxed_velocity)
+
         coalescence = Coalescence(
             collision_kernel=self.settings.kernel, adaptive=self.settings.adaptive
         )
         self.builder.add_dynamic(coalescence)
 
-        if self.settings.evaluate_relaxed_velocity:
-            relaxed_velocity = RelaxedVelocity(tau=self.settings.tau)
-            self.builder.add_dynamic(relaxed_velocity)
 
-            attributes["fall momentum"] = init_fall_momenta(
-                attributes["volume"], self.builder.formulae.constants.rho_w)
-
-            self.builder.request_attribute("fall velocity")
+        ############
+        # PRODUCTS #
+        ############
 
         products = [
             ParticleVolumeVersusRadiusLogarithmSpectrum(
@@ -58,44 +71,63 @@ class Simulation:
             RadiusBinnedNumberAveragedTerminalVelocity(
                 self.settings.radius_bins_edges, name="terminal_vel"
             ),
+            RadiusBinnedNumberAveragedFallVelocity(
+                self.settings.radius_bins_edges, name="fall_vel"
+            ),
             WallTime(),
         ]
 
-        if self.settings.evaluate_relaxed_velocity:
-            products.append(RadiusBinnedNumberAveragedFallVelocity(
-                self.settings.radius_bins_edges, name="fall_vel"
-            ))
-
         self.particulator = self.builder.build(attributes, tuple(products))
+
+
+        #############
+        # OBSERVERS #
+        #############
 
         self.particulator.observers.append(
             Progress(self.settings.output_steps[-1]))
 
-        # self.particulator.observers.append(WarnVelocityDiff(self.particulator))
 
+        ############
+        # OUTPUT   #
+        ############
+
+        self.dt = self.settings.dt
+        self.num_steps = int(self.settings.max_t / self.dt)
         self.done: bool = False
-        self._output: list[dict[str, Any]] = []
+        self._output_products: dict[str, list[Any]] = {
+            "dm/dlnr": [None for _ in range(self.num_steps+1)],
+            "terminal_vel": [None for _ in range(self.num_steps+1)],
+            "fall_vel": [None for _ in range(self.num_steps+1)]
+        }
+        self._output_attributes: dict[str, list[Any]] = {a: [None for _ in range(self.num_steps+1)] for a in self.particulator.attributes.keys()}
         self._exec_time: float = -1
+
+    def save(self, index):
+        self._output_products["dm/dlnr"][index] = self.particulator.products["dv/dlnr"].get()[
+            0]
+        self._output_products["dm/dlnr"][index][:] *= self.settings.rho
+
+        self._output_products["terminal_vel"][index] = self.particulator.products["terminal_vel"].get(
+        )
+
+        self._output_products["fall_vel"][index] = self.particulator.products["fall_vel"].get(
+        )
+
+        for a in self._output_attributes:
+            self._output_attributes[a][index] = self.particulator.attributes[a].to_ndarray(
+            )
+
 
     def run(self):
         assert not self.done
 
-        self._output = []
         self.particulator.products["wall time"].reset()
-        for step in self.settings.output_steps:
-            self.particulator.run(step - self.particulator.n_steps)
 
-            output_vals: dict[str, Any] = {}
-
-            output_vals["dm/dlnr"] = self.particulator.products["dv/dlnr"].get()[0]
-            output_vals["dm/dlnr"][:] *= self.settings.rho
-
-            output_vals["terminal_vel"] = self.particulator.products["terminal_vel"].get()
-
-            if self.settings.evaluate_relaxed_velocity:
-                output_vals["fall_vel"] = self.particulator.products["fall_vel"].get()
-
-            self._output.append(output_vals)
+        self.save(0)
+        for step in range(1, self.num_steps+1):
+            self.particulator.run(1)
+            self.save(step)
 
         self._exec_time = self.particulator.products["wall time"].get()
         self.done = True
@@ -108,56 +140,25 @@ class Simulation:
         return res
 
     def _plot_vs_lnr_single(self, index: int, product_name: str, y_scale: float, colors: SpectrumColors, set_to=None):
-        X = np.linspace(1, 100, 50)
-        Y = X*0.05
         if set_to is not None:
             set_to.set_data(self.settings.radius_bins_edges[:-
                                                             1] * si.metres / si.micrometres,
-                            self._output[index][product_name] * y_scale)
-            set_to.set_label(f"t = {self.settings.steps[index]}s")
-            set_to.set_color(colors(
-                self.settings.steps[index] /
-                (self.settings.output_steps[-1] * self.settings.dt)
-            ))
+                            self._output_products[product_name][index] * y_scale)
+            set_to.set_label(f"t = {index*self.dt}s")
+            set_to.set_color(colors(index / self.num_steps))
             return set_to
 
         else:
             return plt.step(
                 self.settings.radius_bins_edges[:-
                                                 1] * si.metres / si.micrometres,
-                self._output[index][product_name] * y_scale,
+                self._output_products[product_name][index] * y_scale,
                 where="post",
-                label=f"t = {self.settings.steps[index]}s",
-                color=colors(
-                    self.settings.steps[index] /
-                    (self.settings.output_steps[-1] * self.settings.dt)
-                ),
+                label=f"t = {index*self.dt}s",
+                color=colors(index / self.num_steps),
             )
 
-    def plot_vs_lnr(self, product_name: str, y_scale: float = 1, y_label: Optional[str] = None, num_plots: int = 4):
-        assert self.done
-
-        y_label = y_label or product_name
-
-        plt_colors = SpectrumColors()
-
-        steps = np.linspace(0, len(self.settings.steps)-1, num_plots)
-        for i in steps:
-            self._plot_vs_lnr_single(
-                int(i), product_name, y_scale, plt_colors)
-
-        plt_name = self.get_plt_name(product_name.replace("/", "_"))
-
-        plt.xscale("log", base=10)
-
-        plt.title(plt_name)
-        plt.xlabel("particle radius [µm]")
-        plt.ylabel(y_label)
-        plt.legend()
-
-        show_plot(filename=f"{plt_name}.pdf")
-
-    def plot_vs_lnr_animation(self, product_name: str, y_scale: float = 1, y_label: Optional[str] = None, num_fixed: int = 4, show=True):
+    def plot_vs_lnr_animation(self, product_name: str, y_scale: float = 1, y_label: Optional[str] = None, num_fixed: int = 4, show=True, speedup=40):
         assert self.done
 
         plt_colors = SpectrumColors()
@@ -165,7 +166,7 @@ class Simulation:
         fig = plt.figure()
         graph, = self._plot_vs_lnr_single(0, product_name, y_scale, plt_colors)
 
-        stamp_steps = np.linspace(0, len(self.settings.steps)-1, num_fixed)
+        stamp_steps = np.linspace(0, self.num_steps, num_fixed)
         for i in stamp_steps:
             new_graph, = self._plot_vs_lnr_single(
                 int(i), product_name, y_scale, plt_colors)
@@ -173,12 +174,12 @@ class Simulation:
 
         def animate(i):
             update_list = [self._plot_vs_lnr_single(
-                i, product_name, y_scale, plt_colors, set_to=graph), plt.legend()]
+                i*speedup, product_name, y_scale, plt_colors, set_to=graph), plt.legend()]
 
             return update_list
 
         anim = animation.FuncAnimation(fig, animate,
-                                       frames=len(self.settings.steps), interval=50)
+                                       frames=self.num_steps//speedup, interval=50)
 
         plt_name = self.get_plt_name(product_name.replace("/", "_"))
 
@@ -188,7 +189,7 @@ class Simulation:
         plt.xlabel("particle radius [µm]")
         plt.ylabel(y_label)
 
-        output_nums = [i[product_name] for i in self._output]
+        output_nums = self._output_products[product_name]
         plt.xlim((np.min(self.settings.radius_bins_edges) * si.metres / si.micrometres,
                   np.max(self.settings.radius_bins_edges) * si.metres / si.micrometres))
         plt.ylim((0, np.max(output_nums) * y_scale))
@@ -203,22 +204,59 @@ class Simulation:
             except:
                 plt.show()
 
+    def plot_sd_velocity_animation(self, show=True, speedup=40):
+        """
+        Create an animation similar to the plot ln vs r animation
+        except it plots the velocity attributes at each index
+        """
+        assert self.done
 
-def generate_dm_dlnr_animation(evaluate_relaxed_velocity=True, tau=100*si.seconds):
-    settings = Settings(
-        n_sd=2**18, max_t=7200, evaluate_relaxed_velocity=evaluate_relaxed_velocity, tau=tau)
-    simulation = Simulation(settings)
+        def plot_at_index(i, set_to=None):
+            if set_to is not None:
+                set_to.set_data(self._output_attributes["radius"][i]*si.metres/si.micrometres, self._output_attributes["fall velocity"][i])
+                set_to.set_label(f"t = {i*self.dt}s")
+                return set_to
 
-    simulation.run()
-
-    simulation.plot_vs_lnr_animation(
-        "dm/dlnr", si.kilograms/si.grams, "dm/dlnr [g/m^3/(unit dr/r)]", show=False, num_fixed=8)
+            else:
+                return plt.plot(self._output_attributes["radius"][i]*si.metres/si.micrometres, self._output_attributes["fall velocity"][i], "ro")
 
 
-if __name__ == "__main__":
-    generate_dm_dlnr_animation(evaluate_relaxed_velocity=False)
-    for tau in [1, 10, 100]:
-        generate_dm_dlnr_animation(
-            evaluate_relaxed_velocity=True, tau=tau)
+        LOG_LOWER_BOUND = -6
+        LOG_UPPER_BOUND = -2
 
-# simulation.plot_vs_lnr_animation("fall_vel", 1, "fall velocity [m/s]")
+        radii_arr = np.logspace(LOG_LOWER_BOUND, LOG_UPPER_BOUND, 1000)
+        terminal_vel_arr = get_approx(radii_arr, particulator=self.particulator)
+
+
+        fig = plt.figure()
+        graph, = plot_at_index(0)
+
+        plt.plot(radii_arr*si.metres/si.micrometres, terminal_vel_arr,
+        "b-", label="Rogers Yau", alpha=0.5, linewidth=3)
+
+        def animate(i):
+            return [plot_at_index(i*speedup, set_to=graph), plt.legend()]
+
+        anim = animation.FuncAnimation(fig, animate,
+                                       frames=self.num_steps//speedup, interval=50)
+
+        plt_name = self.get_plt_name("sd_velocity")
+
+        plt.xscale("log", base=10)
+
+        plt.title(plt_name)
+        plt.xlabel("particle radius [µm]")
+        plt.ylabel("velocity [m/s]")
+
+        anim.save(f"{plt_name}.gif", fps=30)
+
+        if show:
+            try:
+                assert get_ipython().__class__.__name__ == "ZMQInteractiveShell"
+                display(HTML(anim.to_html5_video()))
+                plt.close()
+            except:
+                plt.show()
+
+
+
