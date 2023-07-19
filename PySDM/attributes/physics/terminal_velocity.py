3c292b38227f8651ecdd55124051a0aad08d4792
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2023-07-19 16:18:19 -0700

    remove new product (for now)

--- PySDM/attributes/physics/terminal_velocity.py
+++ PySDM/attributes/physics/terminal_velocity.py
@@ -2,7 +2,7 @@
 particle terminal velocity (used for collision probability and particle displacement)
 """
 from PySDM.attributes.impl.derived_attribute import DerivedAttribute
-from PySDM.dynamics.terminal_velocity import Interpolation, RogersYau
+from PySDM.dynamics.terminal_velocity import Interpolation
 
 
 class TerminalVelocity(DerivedAttribute):
@@ -11,8 +11,7 @@ class TerminalVelocity(DerivedAttribute):
         dependencies = [self.radius]
         super().__init__(builder, name="terminal velocity", dependencies=dependencies)
 
-        # self.approximation = Interpolation(builder.particulator)
-        self.approximation = RogersYau(particulator=builder.particulator)
+        self.approximation = Interpolation(builder.particulator)
 
     def recalculate(self):
         self.approximation(self.data, self.radius.get())
