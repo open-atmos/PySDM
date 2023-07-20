0e634e9401c8fb0322e0ba5bdb263473ebf2a14e
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-07-20 14:07:43 +0200

    add placeholder for smoke test (first one without any env featured)

---
+++ tests/smoke_tests/no_env/pierchala_et_al_2022/test_fig_3.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot
+
+class TestFig3:
+    @staticmethod
+    def test_top_left_panel(plot=False):
+        # arrange
+
+
+        # act
+
+        # plot
+        if plot:
+            pyplot.show()
+
+        # assert
+        assert False
