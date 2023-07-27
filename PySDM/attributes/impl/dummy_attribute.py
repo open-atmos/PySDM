f6a5d6be14be6d5a992e95d1571d1e7fd43fe1c4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-07-27 06:26:19 +0200

    remove get_relative_fall_momentum, add optional warning logic to make_dummy_attribute_factory

--- PySDM/attributes/impl/dummy_attribute.py
+++ PySDM/attributes/impl/dummy_attribute.py
@@ -1,5 +1,7 @@
 """ logic around `PySDM.attributes.impl.dummy_attribute.DummyAttribute` - parent class
 for do-nothing attributes """
+import warnings
+
 import numpy as np
 
 from .attribute import Attribute
@@ -17,8 +19,13 @@ class DummyAttribute(Attribute):
         return self.data
 
 
-def make_dummy_attribute_factory(name):
+def make_dummy_attribute_factory(name, warn=False):
     def _factory(builder):
         return DummyAttribute(builder, name=name)
+
+    if warn:
+        warnings.warn(
+            f"dummy implementation used for requested attribute named '{name}'"
+        )
 
     return _factory
