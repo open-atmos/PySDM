7ffa50b1f9d0f94bd4710a6278e80746ce3ddbf0
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-08-26 21:24:34 +0200

    one more line shortening

--- PySDM/backends/impl_thrust_rtc/methods/moments_methods.py
+++ PySDM/backends/impl_thrust_rtc/methods/moments_methods.py
@@ -33,7 +33,10 @@ class MomentsMethods(ThrustRTCBackendMethods):
             if (min_x <= x_attr[i] && x_attr[i] < max_x) {
                 atomicAdd((real_type*)&moment_0[cell_id[i]], (real_type)(multiplicity[i]));
                 for (auto k = 0; k < n_ranks; k+=1) {
-                    auto value = multiplicity[i] * pow((real_type)(attr_data[i]), (real_type)(ranks[k]));
+                    auto value = multiplicity[i] * pow(
+                        (real_type)(attr_data[i]),
+                        (real_type)(ranks[k])
+                    );
                     atomicAdd((real_type*) &moments[n_cell * k + cell_id[i]], value);
                }
             }
