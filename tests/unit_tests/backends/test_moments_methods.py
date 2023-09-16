df03eb451cc8f88b82b783d56c39a48a4cf73fdc
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-09-16 15:11:10 +0200

    add one pylint disable comment

--- tests/unit_tests/backends/test_moments_methods.py
+++ tests/unit_tests/backends/test_moments_methods.py
@@ -15,6 +15,7 @@ from PySDM import Formulae
         (0, 1, 1.5, 0),
     ],
 )
+# pylint: disable=too-many-arguments
 def test_moments_range(backend_class, min_x, max_x, value, expected, double_precision):
     if backend_class.__name__ == "Numba" and not double_precision:
         pytest.skip()  # TODO #1144
