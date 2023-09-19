bce29c06f8e548554b3dac53b50cdcd7b1013613
Oleksii Bulenok <a.bulenok@student.uj.edu.pl>
2023-09-19 17:03:32 +0200

    Add water mass BaseAttribute and make volume ExtensiveAttribute

--- PySDM/attributes/physics/volume.py
+++ PySDM/attributes/physics/volume.py
@@ -1,11 +1,15 @@
 """
-particle (wet) volume, key attribute for coalescence
+particle (wet) volume, derived attribute for coalescence
 in simulation involving mixed-phase clouds, positive values correspond to
 liquid water and negative values to ice
 """
-from PySDM.attributes.impl.extensive_attribute import ExtensiveAttribute
+from PySDM.attributes.impl import DerivedAttribute
 
 
-class Volume(ExtensiveAttribute):
+class Volume(DerivedAttribute):
     def __init__(self, builder):
+        self.water_mass = builder.get_attribute("water mass")
         super().__init__(builder, name="volume")
+
+    def recalculate(self):
+        self.data.product(self.water_mass.get(), 1 / self.formulae.constants.rho_w)
