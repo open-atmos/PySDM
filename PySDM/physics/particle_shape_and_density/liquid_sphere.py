73ca65cd1b9cfbccd592867d125d07c68685cfa0
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-09-24 18:39:13 +0200

    add radius_to_mass func

--- PySDM/physics/particle_shape_and_density/liquid_sphere.py
+++ PySDM/physics/particle_shape_and_density/liquid_sphere.py
@@ -1,6 +1,7 @@
 """
 spherical particles with constant density of water
 """
+import numpy as np
 
 
 class LiquidSphere:
@@ -18,3 +19,7 @@ class LiquidSphere:
     @staticmethod
     def volume_to_mass(const, volume):
         return const.rho_w * volume
+
+    @staticmethod
+    def radius_to_mass(const, radius):
+        return const.rho_w * const.PI_4_3 * np.power(radius, const.THREE)
