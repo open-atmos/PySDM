2746d38958efe73903fa75b84d41865da19e5d0a
Oleksii Bulenok <a.bulenok@student.uj.edu.pl>
2023-10-05 14:37:05 +0200

    Add ExponFrag to __init__.py

--- PySDM/dynamics/collisions/breakup_fragmentations/__init__.py
+++ PySDM/dynamics/collisions/breakup_fragmentations/__init__.py
@@ -3,6 +3,7 @@ TODO #744
 """
 from .always_n import AlwaysN
 from .constant_mass import ConstantMass
+from .expon_frag import ExponFrag
 from .exponential import Exponential
 from .feingold1988 import Feingold1988
 from .gaussian import Gaussian
