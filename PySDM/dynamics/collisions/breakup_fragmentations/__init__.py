0758800c3b10710d5d0dd6b3b6a90d0c5c8d2e78
Oleksii Bulenok <a.bulenok@student.uj.edu.pl>
2023-10-05 14:32:50 +0200

    fix JOSS paper reproducibility

--- PySDM/dynamics/collisions/breakup_fragmentations/__init__.py
+++ PySDM/dynamics/collisions/breakup_fragmentations/__init__.py
@@ -1,8 +1,6 @@
 """
 TODO #744
 """
-import warnings
-
 from .always_n import AlwaysN
 from .constant_mass import ConstantMass
 from .exponential import Exponential
@@ -11,8 +9,3 @@ from .gaussian import Gaussian
 from .lowlist82 import LowList1982Nf
 from .slams import SLAMS
 from .straub2010 import Straub2010Nf
-
-
-class ExponFrag(Exponential):  # pylint: disable=too-few-public-methods
-    def __init_subclass__(cls):
-        warnings.warn("Class has been renamed", DeprecationWarning)
