16e35fb9a4aa34809fa23c61a5f8b02a9fec47a0
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-10-08 23:21:05 +0200

    less tight tolerance for condensation mass conservation test

--- tests/smoke_tests/parcel/arabas_and_shima_2017/test_conservation.py
+++ tests/smoke_tests/parcel/arabas_and_shima_2017/test_conservation.py
@@ -51,9 +51,8 @@ def test_water_mass_conservation(settings_idx, mass_of_dry_air, scheme, coord):
     total_water_mixing_ratio = simulation.particulator.environment[
         "water_vapour_mixing_ratio"
     ].to_ndarray() + liquid_water_mixing_ratio(simulation)
-    significant = 6 if scheme == "GPU" else 14  # TODO #540
     np.testing.assert_approx_equal(
-        total_water_mixing_ratio, initial_total_water_mixing_ratio, significant
+        total_water_mixing_ratio, initial_total_water_mixing_ratio, significant=6
     )
     if scheme != "SciPy":
         assert simulation.particulator.products["S_max"].get() >= output["S"][-1]
