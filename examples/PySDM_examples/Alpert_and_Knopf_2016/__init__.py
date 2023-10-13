3509c6823846b234637664bf76e9baef55e6c049
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-10-13 19:23:20 +0200

    precommit (white spaces)

--- examples/PySDM_examples/Alpert_and_Knopf_2016/__init__.py
+++ examples/PySDM_examples/Alpert_and_Knopf_2016/__init__.py
@@ -1,5 +1,5 @@
 """
-box-environment example based on 
+box-environment example based on
 [Alpert & Knopf 2016 (Atmos. Chem. Phys. 16)](https://doi.org/10.5194/acp-16-2083-2016)
 """
 # pylint: disable=invalid-name
