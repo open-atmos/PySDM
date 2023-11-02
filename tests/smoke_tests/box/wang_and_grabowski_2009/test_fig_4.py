7678e662b07f085f29f31feb278d9938fcc3c9ef
Sanket Bhiogade <sanket.bhiogade95@gmail.com>
2023-11-02 14:29:10 +0530

    modify th init file

--- tests/smoke_tests/box/wang_and_grabowski_2009/test_fig_4.py
+++ tests/smoke_tests/box/wang_and_grabowski_2009/test_fig_4.py
@@ -1,4 +1,6 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
 from PySDM_examples import Wang_and_Grabowski_2009
+
 
 def test_hello_world():
     assert False
