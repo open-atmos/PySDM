5dce6347926f3865eec68889b18de07e55cde795
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-11-03 16:34:24 +0100

    fixing DummyParticulator + one less local var for pylint

--- tests/unit_tests/dummy_particulator.py
+++ tests/unit_tests/dummy_particulator.py
@@ -9,8 +9,10 @@ from .dummy_environment import DummyEnvironment
 class DummyParticulator(Builder, Particulator):
     def __init__(self, backend_class, n_sd=0, formulae=None, grid=None):
         backend = backend_class(formulae, double_precision=True)
-        Builder.__init__(self, n_sd, backend, DummyEnvironment(grid=grid))
+        env = DummyEnvironment(grid=grid)
+        Builder.__init__(self, n_sd, backend, env)
         Particulator.__init__(self, n_sd, backend)
+        self.environment = env
         self.particulator = self
         self.req_attr = {"multiplicity": Multiplicities(self), "cell id": CellID(self)}
         self.attributes = None
