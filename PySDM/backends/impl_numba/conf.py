b8cd9f22c3ecaf6d4cb085ba92469dded4c275bf
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2023-11-07 09:43:58 +0100

    add warning, use platform.machine instead of os.uname

--- PySDM/backends/impl_numba/conf.py
+++ PySDM/backends/impl_numba/conf.py
@@ -2,16 +2,23 @@
 default settings for Numba just-in-time compilation
 """
 import os
+import platform
 import warnings
 
 import numba
 
 JIT_FLAGS = {
-    "parallel": os.uname().machine != "arm64",  # TODO #1183 - atomics don't work!
+    "parallel": True,
     "fastmath": True,
     "error_model": "numpy",
     "cache": False,  # https://github.com/numba/numba/issues/2956
 }
+
+if platform.machine() == "arm64":
+    warnings.warn(
+        "Disabling Numba threading due to ARM64 CPU (atomics do not work yet)"
+    )
+    JIT_FLAGS["parallel"] = False  # TODO #1183 - atomics don't work on ARM64!
 
 try:
     numba.parfors.parfor.ensure_parallel_support()
