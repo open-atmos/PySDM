505ac1ad503bd7efcf0a74fb8eed997296b85fbf
edejong-caltech <edejong@caltech.edu>
2024-01-04 12:47:32 -0600

    Update defaults in power series

--- PySDM/dynamics/terminal_velocity/power_series.py
+++ PySDM/dynamics/terminal_velocity/power_series.py
@@ -10,10 +10,11 @@ class PowerSeries:  # pylint: disable=too-few-public-methods
     def __init__(self, particulator, *, prefactors=None, powers=None):
         si = const.si
         self.particulator = particulator
-        self.prefactors = prefactors or [2e2 * si.m / si.s / np.sqrt(si.m)]
+        self.prefactors = prefactors or [2.0e-1 * si.m / si.s / np.sqrt(si.m)]
         self.powers = powers or [1 / 6]
         for i in range(len(self.prefactors)):
             self.prefactors[i] *= (4 / 3 * const.PI) ** (self.powers[i])
+            self.prefactors[i] /= (1 * si.um**3) ** (self.powers[i])
         assert len(self.prefactors) == len(self.powers)
 
     def __call__(self, output, radius):
