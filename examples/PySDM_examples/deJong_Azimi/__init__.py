ffa40d36c770a0aec99dc4b7eb1403dfc2f258f2
edejong-caltech <edejong@caltech.edu>
2024-01-04 15:22:34 -0600

    Moving case-specific rainshaft simulation to within example folder

--- examples/PySDM_examples/deJong_Azimi/__init__.py
+++ examples/PySDM_examples/deJong_Azimi/__init__.py
@@ -2,6 +2,9 @@
 box- and single-column breakup-focused examples from
 [de Jong et al. 2023](https://doi.org/10.5194/gmd-16-4193-2023)
 """
+from .settings1D import Settings1D
+
 # pylint: disable=invalid-name
 from .settings_0D import Settings0D
+from .simulation1D import Simulation
 from .simulation_0D import run_box
