b676075016ae78b0ebf1a42f8e07884f7a88046d
Sanket Bhiogade <sbhiogade@agh.edu.pl>
2024-01-08 23:20:40 +0100

    New Example: Graf 2019 Fig. 4 (Parcel env, Condensation, Isotope)

--- tests/examples_tests/conftest.py
+++ tests/examples_tests/conftest.py
@@ -49,10 +49,10 @@ TEST_SUITES = {
     ],
     "isotopes": [
         "Bolot_et_al_2013",
+        "Graf_et_al_2019",
         "Merlivat_and_Nief_1967",
         "Van_Hook_1968",
         "Pierchala_et_al_2022",
-        "Graf_et_al_2019",
         # "Rozanski_and_Sonntag_1982",  # TODO #1207
     ],
 }
