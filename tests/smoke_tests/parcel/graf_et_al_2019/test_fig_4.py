fc9dd2f4b78795ff3c3d3dc0db0335cd028da9b3
Sanket Bhiogade <sbhiogade@agh.edu.pl>
2024-01-11 09:52:10 +0100

    precommit changes

--- tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
+++ tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+
 import pytest
-from PySDM.physics import si
 from PySDM_examples import Graf_2019
 from PySDM_examples.utils import notebook_vars
+
+from PySDM.physics import si
 
 PLOT = False
 
@@ -10,15 +12,13 @@ PLOT = False
 @pytest.fixture(scope="session", name="vars")
 def vars_fixture():
     return notebook_vars(
-        file=Path(Graf_2019.__file__).parent / "figure_4.ipynb",
-        plot=PLOT
+        file=Path(Graf_2019.__file__).parent / "figure_4.ipynb", plot=PLOT
     )
 
 
-@pytest.mark.parametrize("level_name, expected_height", (
-        ("CB", 1 * si.km),
-        ("0C", 2.24 * si.km)
-))
+@pytest.mark.parametrize(
+    "level_name, expected_height", (("CB", 1 * si.km), ("0C", 2.24 * si.km))
+)
 def test_fig_4(vars, level_name, expected_height):
     # arrange
     tolerance = 50 * si.m
