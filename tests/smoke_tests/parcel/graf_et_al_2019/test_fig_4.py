cb384a5864114b12e475ef0ad2845235d8796825
Sanket Bhiogade <sbhiogade@agh.edu.pl>
2024-01-11 09:57:05 +0100

    add docstring & fix built-in shadow

--- tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
+++ tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
@@ -1,3 +1,4 @@
+""" tests ensuring values on plots match those in the paper """
 from pathlib import Path
 
 import pytest
@@ -9,8 +10,8 @@ from PySDM.physics import si
 PLOT = False
 
 
-@pytest.fixture(scope="session", name="vars")
-def vars_fixture():
+@pytest.fixture(scope="session", name="variables")
+def variables_fixture():
     return notebook_vars(
         file=Path(Graf_2019.__file__).parent / "figure_4.ipynb", plot=PLOT
     )
@@ -19,7 +20,7 @@ def vars_fixture():
 @pytest.mark.parametrize(
     "level_name, expected_height", (("CB", 1 * si.km), ("0C", 2.24 * si.km))
 )
-def test_fig_4(vars, level_name, expected_height):
+def test_fig_4(variables, level_name, expected_height):
     # arrange
     tolerance = 50 * si.m
 
