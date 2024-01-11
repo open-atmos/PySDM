562b942d05530c4cab455ab3ee48cf3e02e3a681
Sanket Bhiogade <sbhiogade@agh.edu.pl>
2024-01-11 09:35:35 +0100

    New fixture for Reusing of Levels in Smoke test of Graf_et_al_2019

--- tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
+++ tests/smoke_tests/parcel/graf_et_al_2019/test_fig_4.py
@@ -1,19 +1,30 @@
 from pathlib import Path
+import pytest
 from PySDM.physics import si
 from PySDM_examples import Graf_2019
 from PySDM_examples.utils import notebook_vars
 
+PLOT = False
 
-def test_fig_4(plot=False):
-    # arrange
-    vars = notebook_vars(
+
+@pytest.fixture(scope="session", name="vars")
+def vars_fixture():
+    return notebook_vars(
         file=Path(Graf_2019.__file__).parent / "figure_4.ipynb",
-        plot=plot
+        plot=PLOT
     )
 
+
+@pytest.mark.parametrize("level_name, expected_height", (
+        ("CB", 1 * si.km),
+        ("0C", 2.24 * si.km)
+))
+def test_fig_4(vars, level_name, expected_height):
+    # arrange
+    tolerance = 50 * si.m
+
     # act
-    actual = vars["levels"]["CB"] + vars["alt_initial"]
-    expected = 1 * si.km
+    actual = vars["levels"][level_name] + vars["alt_initial"]
 
     # assert
-    assert abs(expected - actual) < 50 * si.m
+    assert abs(expected_height - actual) < tolerance
