fa18686466e589d698dc22aa6b5fa43aebb8a7c3
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-01-20 23:23:48 +0100

    avoid scipy deprecation warning

--- tests/smoke_tests/kinematic_2d/arabas_et_al_2015/test_export.py
+++ tests/smoke_tests/kinematic_2d/arabas_et_al_2015/test_export.py
@@ -10,7 +10,7 @@ from PySDM_examples.Szumowski_et_al_1998.simulation import Simulation
 from PySDM_examples.Szumowski_et_al_1998.storage import Storage
 from PySDM_examples.utils import DummyController
 from PySDM_examples.utils.widgets import IntSlider
-from scipy.io import netcdf
+from scipy.io import netcdf_file
 
 from PySDM import Formulae
 from PySDM.backends import CPU
@@ -78,7 +78,7 @@ def test_export_with_gui_settings():
         vtk_exporter.write_pvd()
 
         # Assert
-        versions = netcdf.netcdf_file(  # pylint: disable=no-member
+        versions = netcdf_file(  # pylint: disable=no-member
             file.absolute_path
         ).versions
         assert "PyMPDATA" in str(versions)
