7c37aceeacb98039c777134b61db99b2f302896a
claresinger <clareees@gmail.com>
2024-01-22 17:07:05 -0500

    fix issue references

--- tests/smoke_tests/parcel/lowe_et_al_2019/test_fig_2.py
+++ tests/smoke_tests/parcel/lowe_et_al_2019/test_fig_2.py
@@ -29,28 +29,28 @@ class TestFig2:  # pylint: disable=too-few-public-methods
                 0.075,
                 169,
             ),
-            (  # SS_max & SS_100m & Nc_100m doesn't match for this case
+            (  # TODO #1247 SS_max & SS_100m & Nc_100m doesn't match for this case
                 paper_aerosol.AerosolBoreal(water_molar_volume=WATER_MOLAR_VOLUME),
                 "Constant",
                 0.182,
                 0.055,
                 422,
             ),
-            (  # SS_100m & Nc_100m doesn't match for this case
+            (  # TODO #1247 SS_100m & Nc_100m doesn't match for this case
                 paper_aerosol.AerosolBoreal(water_molar_volume=WATER_MOLAR_VOLUME),
                 "CompressedFilmOvadnevaite",
                 0.137,
                 0.055,
                 525,
             ),
-            (  # SS_100m & Nc_100m doesn't match for this case
+            (  # TODO #1247 SS_100m & Nc_100m doesn't match for this case
                 paper_aerosol.AerosolNascent(water_molar_volume=WATER_MOLAR_VOLUME),
                 "Constant",
                 0.407,
                 0.122,
                 68,
             ),
-            (  # SS_100m & Nc_100m doesn't match for this case
+            (  # TODO #1247 SS_100m & Nc_100m doesn't match for this case
                 paper_aerosol.AerosolNascent(water_molar_volume=WATER_MOLAR_VOLUME),
                 "CompressedFilmOvadnevaite",
                 0.314,
@@ -60,7 +60,7 @@ class TestFig2:  # pylint: disable=too-few-public-methods
         ),
     )
     # TODO #1247 AerosolMarine passes, but others fail
-    # TODO #1264 general mismatches in parcel profiles
+    # TODO #1246 general mismatches in parcel profiles
     @pytest.mark.xfail()
     def test_peak_supersaturation_and_final_concentration(
         *, aerosol, surface_tension, s_max, s_100m, n_100m
