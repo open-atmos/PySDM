d48f5e7d6613f788aca65549c78c070c8a0ebb0d
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-02-05 21:25:46 +0100

    more code reuse across EffectiveRadius and ActivatedEffectiveRadius classes

--- PySDM/products/size_spectral/effective_radius_activated.py
+++ PySDM/products/size_spectral/effective_radius_activated.py
@@ -2,49 +2,28 @@
 effective radius of particles within a grid cell, for activated, unactivated or both
 """
 
-import numba
 import numpy as np
 
-from PySDM.backends.impl_numba.conf import JIT_FLAGS
 from PySDM.products.impl.activation_filtered_product import _ActivationFilteredProduct
 from PySDM.physics import constants as const
 from PySDM.products.impl.moment_product import MomentProduct
+from PySDM.products.size_spectral import EffectiveRadius
 
 GEOM_FACTOR = const.PI_4_3 ** (-1 / 3)
 
 
 class ActivatedEffectiveRadius(MomentProduct, _ActivationFilteredProduct):
     def __init__(
-        self,
-        *,
-        radius_range=None,
-        count_unactivated: bool,
-        count_activated: bool,
-        name=None,
-        unit="m"
+        self, *, count_unactivated: bool, count_activated: bool, name=None, unit="m"
     ):
         MomentProduct.__init__(self, name=name, unit=unit)
         _ActivationFilteredProduct.__init__(
             self, count_activated=count_activated, count_unactivated=count_unactivated
         )
-        self.volume_range = None
-        self.radius_range = radius_range or (0, np.inf)
 
     def register(self, builder):
         _ActivationFilteredProduct.register(self, builder)
         MomentProduct.register(self, builder)
-        self.volume_range = self.formulae.trivia.volume(np.asarray(self.radius_range))
-
-    @staticmethod
-    @numba.njit(**JIT_FLAGS)
-    def __get_impl(buffer, tmp):
-        buffer[:] = np.where(
-            tmp[:] > 0,
-            buffer[:]
-            * GEOM_FACTOR
-            / (tmp[:] + (tmp[:] == 0)),  # (+ x==0) to avoid div-by-zero warnings
-            np.nan,
-        )
 
     def _impl(self, **kwargs):
         _ActivationFilteredProduct.impl(
@@ -59,5 +38,7 @@ class ActivatedEffectiveRadius(MomentProduct, _ActivationFilteredProduct):
             attr="volume",
             rank=1,
         )
-        ActivatedEffectiveRadius.__get_impl(self.buffer, tmp)
+        EffectiveRadius.nan_aware_reff_impl(
+            input_volume_output_reff=self.buffer, volume_2_3=tmp
+        )
         return self.buffer
