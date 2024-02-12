061bb1879185acaeb64555b19ee210f37f0b171c
Sanket Bhiogade <sbhiogade@agh.edu.pl>
2024-02-12 16:18:56 +0100

    Kinetic and Thermal diffusion for Jensen and Nugent 2016 Example

---
+++ PySDM/physics/diffusion_thermics/jensen_and_nugent_2016.py
@@ -0,0 +1,30 @@
+"""
+as in Jensen and Nugent 2016 (eq. 12-13)
+with reference to [Grabowski et al. (2011)](https://doi:10.1016/j.atmosres.2010.10.020)
+"""
+
+import numpy as np
+
+
+class JensenAndNugent:
+    def __init__(self, _):
+        pass
+
+    @staticmethod
+    def lambdaK(const, T, p):
+        return (
+            ((1.5e-11) * np.power(T, 3))
+            - (4.8e-8 * np.power(T, 2))
+            + (10e-4 * T)
+            - (3.9 * 10e-4)
+        )
+
+    @staticmethod
+    def K(_, const, K, T, r, lmbd):
+        return lmbd / (
+            (r / ((0.216 * const.si.um) + r))
+            + (
+                (lmbd / 0.7 * r * (0.001293 * const.si.g / const.si.m**3))
+                * (np.sqrt((2 * const.pi) / (const.Rd * T)))
+            )
+        )
