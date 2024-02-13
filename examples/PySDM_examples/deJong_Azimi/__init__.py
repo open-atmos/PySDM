733a9e12850d28da531186e29b473206c74f693f
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-02-13 15:02:26 +0100

    simplifying box notebook

--- examples/PySDM_examples/deJong_Azimi/__init__.py
+++ examples/PySDM_examples/deJong_Azimi/__init__.py
@@ -5,5 +5,4 @@ moment-based microphysics in (Cloudy.jl)[https://github.com/CliMA/Cloudy.jl]
 
 # pylint: disable=invalid-name
 from .settings1D import Settings1D
-from .settings_0D import Settings0D
 from .simulation_0D import run_box
--- examples/PySDM_examples/deJong_Azimi/simulation_0D.py
+++ examples/PySDM_examples/deJong_Azimi/simulation_0D.py
@@ -7,12 +7,6 @@ from PySDM.builder import Builder
 from PySDM.dynamics import Coalescence
 from PySDM.environments import Box
 from PySDM.initialisation.sampling.spectral_sampling import ConstantMultiplicity
-from PySDM.physics import si
-from PySDM.products.collision.collision_rates import (
-    CoalescenceRatePerGridbox,
-    CollisionRateDeficitPerGridbox,
-    CollisionRatePerGridbox,
-)
 from PySDM.products.size_spectral import (
     ParticleVolumeVersusRadiusLogarithmSpectrum,
     VolumeFirstMoment,
@@ -21,21 +15,23 @@ from PySDM.products.size_spectral import (
 )
 
 
-def run_box(settings, steps=None, backend_class=CPU):
-    builder = Builder(n_sd=settings.n_sd, backend=backend_class(settings.formulae))
+def run_box(settings, backend_class=CPU):
     env = Box(dv=settings.dv, dt=settings.dt)
-    builder.set_environment(env)
-    env["rhod"] = 1.0
+    builder = Builder(
+        n_sd=settings.n_sd, backend=backend_class(settings.formulae), environment=env
+    )
+    env["rhod"] = settings.rhod
     attributes = {}
     attributes["volume"], attributes["multiplicity"] = ConstantMultiplicity(
         settings.spectrum
     ).sample(settings.n_sd)
-    coal = Coalescence(
-        collision_kernel=settings.kernel,
-        coalescence_efficiency=settings.coal_eff,
-        adaptive=settings.adaptive,
+    builder.add_dynamic(
+        Coalescence(
+            collision_kernel=settings.kernel,
+            coalescence_efficiency=settings.coal_eff,
+            adaptive=settings.adaptive,
+        )
     )
-    builder.add_dynamic(coal)
     products = (
         ParticleVolumeVersusRadiusLogarithmSpectrum(
             radius_bins_edges=settings.radius_bins_edges, name="dv/dlnr"
@@ -43,30 +39,18 @@ def run_box(settings, steps=None, backend_class=CPU):
         ZerothMoment(name="M0"),
         VolumeFirstMoment(name="M1"),
         VolumeSecondMoment(name="M2"),
-        CollisionRatePerGridbox(name="cr"),
-        CollisionRateDeficitPerGridbox(name="crd"),
-        CoalescenceRatePerGridbox(name="cor"),
     )
-    core = builder.build(attributes, products)
+    particulator = builder.build(attributes, products)
 
-    if steps is None:
-        steps = settings.output_steps
-    y = np.ndarray((len(steps), len(settings.radius_bins_edges) - 1))
-    mom = np.ndarray((len(steps), 3))
-    rates = np.zeros((len(steps), 4))
-    # run
-    for i, step in enumerate(steps):
-        core.run(step - core.n_steps)
-        y[i] = core.products["dv/dlnr"].get()[0]
-        rates[i, 0] = core.products["cr"].get()
-        rates[i, 1] = core.products["crd"].get()
-        rates[i, 2] = core.products["cor"].get()
-        mom[i, 0] = core.products["M0"].get()
-        mom[i, 1] = core.products["M1"].get()
-        mom[i, 2] = core.products["M2"].get()
+    y = np.ndarray((len(settings.steps), len(settings.radius_bins_edges) - 1))
+    mom = np.ndarray((len(settings.steps), 3))
+    for i, step in enumerate(settings.steps):
+        particulator.run(step - particulator.n_steps)
+        y[i] = particulator.products["dv/dlnr"].get()[0]
+        mom[i, 0] = particulator.products["M0"].get()
+        mom[i, 1] = particulator.products["M1"].get()
+        mom[i, 2] = particulator.products["M2"].get()
 
-    x = (settings.radius_bins_edges[:-1] / si.micrometres,)[0]
-
-    return namedtuple("_", ("x", "y", "rates", "moments"))(
-        x=x, y=y, rates=rates, moments=mom
+    return namedtuple("_", ("radius_bins_left_edges", "dv_dlnr", "moments"))(
+        radius_bins_left_edges=settings.radius_bins_edges[:-1], dv_dlnr=y, moments=mom
     )
