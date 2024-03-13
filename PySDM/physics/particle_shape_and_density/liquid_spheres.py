fixes in tests, making Re formula a one-liner

PySDM/physics/particle_shape_and_density/liquid_spheres.py
@@ -27,9 +27,5 @@ class LiquidSpheres:
         return const.rho_w * const.PI_4_3 * np.power(radius, const.THREE)
 
     @staticmethod
-    def reynolds_number(
-        const, radius, velocity_wrt_air, air_dynamic_viscosity, air_density
-    ):
-        return (
-            const.TWO * radius * velocity_wrt_air * air_density / air_dynamic_viscosity
-        )
+    def reynolds_number(_, radius, velocity_wrt_air, dynamic_viscosity, density):
+        return 2 * radius * velocity_wrt_air * density / dynamic_viscosity

tests/unit_tests/dynamics/condensation/test_diagnostics.py
@@ -12,7 +12,9 @@ from PySDM.impl.mesh import Mesh
 
 
 class _TestEnv:
-    def __init__(self, *, dt, dv, rhod, thd, water_vapour_mixing_ratio, T, p, RH):
+    def __init__(
+        self, *, dt, dv, rhod, thd, water_vapour_mixing_ratio, T, p, RH, rho, eta
+    ):
         self.mesh = Mesh.mesh_0d()
         self.full = None
         self.particulator = None
@@ -25,6 +27,8 @@ class _TestEnv:
             "T": T,
             "p": p,
             "RH": RH,
+            "air density": rho,
+            "air dynamic viscosity": eta,
         }
 
     def register(self, builder):
@@ -58,6 +62,8 @@ class _TestParticulator:  # pylint: disable=too-few-public-methods
         RH=np.nan,
         dry_volume=np.nan,
         wet_radius=np.nan,
+        rho=np.nan,
+        eta=np.nan,
     ):
         env = _TestEnv(
             dt=dt,
@@ -68,6 +74,8 @@ class _TestParticulator:  # pylint: disable=too-few-public-methods
             T=T,
             p=p,
             RH=RH,
+            rho=rho,
+            eta=eta,
         )
         builder = Builder(n_sd=n_sd, backend=backend(), environment=env)
 