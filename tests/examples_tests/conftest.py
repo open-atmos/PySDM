98cdf006264be80cb82914953bc84310fcc8a711
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-04-13 19:17:02 +0200

    new CI split in examples

--- tests/examples_tests/conftest.py
+++ tests/examples_tests/conftest.py
@@ -16,16 +16,26 @@ def findfiles(path, regex):
 
 
 TEST_SUITES = {
-    "aqueous_chemistry": ["Jaruga_and_Pawlowska_2018", "Kreidenweis_et_al_2003"],
-    "freezing": [
+    "chemistry_freezing_isotopes": [
+        "Jaruga_and_Pawlowska_2018",
+        "Kreidenweis_et_al_2003",
         "Alpert_and_Knopf_2016",
         "Ervens_and_Feingold_2012",
         "Niedermeier_et_al_2014",
+        "Bolot_et_al_2013",
+        "Merlivat_and_Nief_1967",
+        "Van_Hook_1968",
+        "Pierchala_et_al_2022",
+        "Graf_et_al_2019",
+        "Lamb_et_al_2017",
+        # "Rozanski_and_Sonntag_1982",  # TODO #1207
     ],
-    "condensation": [
-        "Abdul_Razzak_Ghan_2000",
-        "Arabas_and_Shima_2017",
+    "condensation_a": [
         "Lowe_et_al_2019",
+        "Abdul_Razzak_Ghan_2000",
+    ],
+    "condensation_b": [
+        "Arabas_and_Shima_2017",
         "Pyrcel",
         "Yang_et_al_2018",
         "Singer_Ward",
@@ -34,26 +44,19 @@ TEST_SUITES = {
     ],
     "coagulation": ["Berry_1967", "Shima_et_al_2009"],
     "breakup": ["Bieli_et_al_2022", "deJong_Mackay_et_al_2023", "Srivastava_1982"],
-    "multi-process": [
-        "Bartman_et_al_2021",
-        "Morrison_and_Grabowski_2007",
-        "Arabas_et_al_2015",
+    "multi-process_a": [
         "Bartman_2020_MasterThesis",
         "Bulenok_2023_MasterThesis",
         "deJong_Azimi",
+    ],
+    "multi-process_b": [
+        "Bartman_et_al_2021",
+        "Morrison_and_Grabowski_2007",
+        "Arabas_et_al_2015",
         "Arabas_et_al_2023",
         "Szumowski_et_al_1998",
         "Shipway_and_Hill_2012",
         "utils",
-    ],
-    "isotopes": [
-        "Bolot_et_al_2013",
-        "Merlivat_and_Nief_1967",
-        "Van_Hook_1968",
-        "Pierchala_et_al_2022",
-        "Graf_et_al_2019",
-        "Lamb_et_al_2017",
-        # "Rozanski_and_Sonntag_1982",  # TODO #1207
     ],
 }
 
