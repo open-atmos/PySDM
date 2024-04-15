0a6ed91245ba24529ee9a78762c97913f6627482
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-04-16 00:27:40 +0200

    saner null imports?

--- PySDM/physics/isotope_diffusivity_ratios/__init__.py
+++ PySDM/physics/isotope_diffusivity_ratios/__init__.py
@@ -1,6 +1,6 @@
 """ heavy-to-light diffusivity ratios for water molecules """
 
-from ..impl.null import Null
+from PySDM.physics.impl.null import Null
 from .grahams_law import GrahamsLaw
 from .stewart_1975 import Stewart1975
 from .hellmann_and_harvey_2020 import HellmannAndHarvey2020
--- PySDM/physics/isotope_meteoric_water_line_excess/__init__.py
+++ PySDM/physics/isotope_meteoric_water_line_excess/__init__.py
@@ -1,5 +1,5 @@
 """ definitions of meteoric water line excess parameters """
 
+from PySDM.physics.impl.null import Null
 from .barkan_and_luz_2007 import BarkanAndLuz2007
 from .dansgaard_1964 import Dansgaard1964
-from ..impl.null import Null
