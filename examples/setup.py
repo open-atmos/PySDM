5f0991461a32ae26547e09d46758fdc72469b46f
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-04-16 10:32:32 +0200

    try workarounding pip install issues

--- examples/setup.py
+++ examples/setup.py
@@ -37,7 +37,6 @@ setup(
         "tests": [
             "pytest",
             "nbconvert",
-            "jupyter-server" + "<2.14.0" if CI else "",
             "jupyter-core" + "<5.0.0" if CI else "",
             # https://github.com/jupyter/nbformat/issues/232
             "jsonschema" + "==3.2.0" if CI else "",
