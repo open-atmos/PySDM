c877cdd1a8ef214543fec719a662f59822750144
jtbuch <jb4625@columbia.edu>
2024-05-20 10:44:49 -0400

    spatial sampling for 2D flows

--- PySDM/initialisation/sampling/spatial_sampling.py
+++ PySDM/initialisation/sampling/spatial_sampling.py
@@ -8,11 +8,13 @@ spatial sampling logic (i.e., physical x-y-z coordinates)
 
 class Pseudorandom:  # pylint: disable=too-few-public-methods
     @staticmethod
-    def sample(*, backend, grid, n_sd, z_part= None, x_part= None):
+    def sample(*, backend, grid, n_sd, z_part=None, x_part=None):
         dimension = len(grid)
         n_elements = dimension * n_sd
-        scale_fac= [] # scale factor for each dimension; y = scale_factor * eps + affine_factor
-        affine_fac= [] # affine factor for each dimension
+        scale_fac = (
+            []
+        )  # scale factor for each dimension; y = scale_factor * eps + affine_factor
+        affine_fac = []  # affine factor for each dimension
 
         storage = backend.Storage.empty(n_elements, dtype=float)
         backend.Random(seed=backend.formulae.seed, size=n_elements)(storage)
@@ -21,7 +23,7 @@ class Pseudorandom:  # pylint: disable=too-few-public-methods
         if z_part is None:
             scale_fac.append(grid[0])
             affine_fac.append(0)
-            
+
             if x_part is not None:
                 ix_min = int(grid[1] * x_part[0])
                 ix_max = int(grid[1] * x_part[1])
@@ -37,7 +39,7 @@ class Pseudorandom:  # pylint: disable=too-few-public-methods
             iz_max = int(grid[0] * z_part[1])
             scale_fac.append(iz_max - iz_min)
             affine_fac.append(iz_min)
-            
+
             if x_part is not None:
                 ix_min = int(grid[1] * x_part[0])
                 ix_max = int(grid[1] * x_part[1])
@@ -48,7 +50,7 @@ class Pseudorandom:  # pylint: disable=too-few-public-methods
                 if dimension == 2:
                     scale_fac.append(grid[1])
                     affine_fac.append(0)
-        
+
         for dim in range(dimension):
             positions[dim, :] *= scale_fac[dim]
             positions[dim, :] += affine_fac[dim]
