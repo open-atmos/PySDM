675f8b1623d824577ed2fc534cb4d7ac8253ee53
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-05-31 14:31:50 +0200

    first asserts in R&S smoke test

--- tests/smoke_tests/parcel_a/rozanski_and_sonntag_1982/test_figs_4_5_6.py
+++ tests/smoke_tests/parcel_a/rozanski_and_sonntag_1982/test_figs_4_5_6.py
@@ -2,10 +2,12 @@
 
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 from PySDM_examples.utils import notebook_vars
 from PySDM_examples import Rozanski_and_Sonntag_1982
+from PySDM.physics import in_unit
 
 PLOT = False
 
@@ -18,5 +20,45 @@ def variables_fixture():
     )
 
 
-def test_fig_5_asymptotes(variables):
-    pass
+class TestFigs456:
+    @staticmethod
+    def test_fig_5_vapour_asymptote(variables):
+        delta_vapour_at_the_cloud_top_per_mille = in_unit(
+            variables["formulae"].trivia.isotopic_ratio_2_delta(
+                ratio=variables["data"][-1]["Rv_2H"][1:],
+                reference_ratio=variables["const"].VSMOW_R_2H,
+            ),
+            variables["const"].PER_MILLE,
+        )
+
+        d_delta = np.diff(delta_vapour_at_the_cloud_top_per_mille)
+        dd_delta = np.diff(d_delta)
+
+        mean = np.mean(delta_vapour_at_the_cloud_top_per_mille)
+        assert (d_delta < 0).all()
+        assert (dd_delta > 0).all()
+        assert (np.abs(d_delta / mean)[-10:] < 0.02).all()
+        np.testing.assert_approx_equal(
+            delta_vapour_at_the_cloud_top_per_mille[-1], -460, significant=3
+        )
+
+    @staticmethod
+    def test_fig_5_rain_at_the_cloud_base(variables):
+        delta_rain_at_the_cloud_base_per_mille = in_unit(
+            variables["formulae"].trivia.isotopic_ratio_2_delta(
+                ratio=variables["data"][variables["level_indices"]["CB"]]["Rr_2H"][1:],
+                reference_ratio=variables["const"].VSMOW_R_2H,
+            ),
+            variables["const"].PER_MILLE,
+        )
+
+        d_delta = np.diff(delta_rain_at_the_cloud_base_per_mille)
+        dd_delta = np.diff(d_delta)
+
+        mean = np.mean(delta_rain_at_the_cloud_base_per_mille)
+        assert (d_delta[len(d_delta) // 4 :] < 0).all()
+        assert (dd_delta[len(dd_delta) // 4 :] > 0).all()
+        assert (np.abs(d_delta / mean)[-10:] < 0.02).all()
+        np.testing.assert_approx_equal(
+            delta_rain_at_the_cloud_base_per_mille[-1], -2.92, significant=3
+        )
