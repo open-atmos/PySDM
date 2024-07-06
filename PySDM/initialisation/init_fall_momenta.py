8e464ec932a255026c77b1c56cdea86fcd82635e
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-07-07 00:29:10 +0200

    complete volume-to-mass transition for base attribute. closes #798

--- PySDM/initialisation/init_fall_momenta.py
+++ PySDM/initialisation/init_fall_momenta.py
@@ -12,8 +12,7 @@ from PySDM.particulator import Particulator
 
 
 def init_fall_momenta(
-    volume: np.ndarray,
-    rho_w: float,  # TODO #798 - we plan to use masses instead of volumes soon
+    water_mass: np.ndarray,
     zero: bool = False,
     terminal_velocity_approx=GunnKinzer1949,  # TODO #1155
 ):
@@ -24,24 +23,26 @@ def init_fall_momenta(
     `PySDM.attributes.physics.relative_fall_velocity.RelativeFallVelocity` attribute)
 
     Parameters:
-        - volume: a numpy array of superdroplet volumes
-        - rho_w: the density of water (generally found in formulae.constants)
+        - water_mass: a numpy array of superdroplet water masses
 
     Returns:
         - a numpy array of initial momentum values
     """
     if zero:
-        return np.zeros_like(volume)
+        return np.zeros_like(water_mass)
 
     particulator = Particulator(0, CPU(Formulae()))  # TODO #1155
 
     approximation = terminal_velocity_approx(particulator=particulator)
 
-    radii_arr = particulator.formulae.trivia.radius(volume)
+    volume_arr = particulator.formulae.particle_shape_and_density.mass_to_volume(
+        water_mass
+    )
+    radii_arr = particulator.formulae.trivia.radius(volume=volume_arr)
     radii = particulator.Storage.from_ndarray(radii_arr)
 
-    output = particulator.Storage.empty((len(volume),), dtype=float)
+    output = particulator.Storage.empty((len(water_mass),), dtype=float)
 
     approximation(output=output, radius=radii)
 
-    return output.to_ndarray() * volume * rho_w  # TODO #798 this assumes no ice
+    return output.to_ndarray() * water_mass
