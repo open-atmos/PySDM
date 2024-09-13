a1b988d630c1584639aa2f32b737cd898381906e
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-09-13 14:54:42 +0200

    replace pytest.mark.xfail with pytest.raises to check for exception type and message

--- tests/unit_tests/backends/test_seeding.py
+++ tests/unit_tests/backends/test_seeding.py
@@ -1,5 +1,7 @@
 """ Seeding backend tests of injection logic """
 
+from contextlib import nullcontext
+
 import numpy as np
 import pytest
 
@@ -14,22 +16,19 @@ class TestSeeding:
 
     @staticmethod
     @pytest.mark.parametrize(
-        "n_sd, number_of_super_particles_to_inject",
+        "n_sd, number_of_super_particles_to_inject, context",
         (
-            (1, 1),
-            pytest.param(1, 2, marks=pytest.mark.xfail(strict=True)),
-            (max_number_to_inject, max_number_to_inject - 1),
-            (max_number_to_inject, max_number_to_inject),
-            pytest.param(
-                max_number_to_inject + 2,
-                max_number_to_inject + 1,
-                marks=pytest.mark.xfail(strict=True),
-            ),
+            (1, 1, nullcontext()),
+            (1, 2, pytest.raises(ValueError, match="inject more super particles than space available")),
+            (max_number_to_inject, max_number_to_inject - 1, nullcontext()),
+            (max_number_to_inject, max_number_to_inject, nullcontext()),
+            (max_number_to_inject + 2, max_number_to_inject + 1, pytest.raises(ValueError, match="inject multiple super particles with the same attributes")),
         ),
     )
     def test_number_of_super_particles_to_inject(
         n_sd,
         number_of_super_particles_to_inject,
+        context,
         dt=1,
         dv=1,
     ):
@@ -61,31 +60,33 @@ class TestSeeding:
         )
 
         # act
-        particulator.seeding(
-            seeded_particle_index=seeded_particle_index,
-            seeded_particle_multiplicity=seeded_particle_multiplicity,
-            seeded_particle_extensive_attributes=seeded_particle_extensive_attributes,
-            number_of_super_particles_to_inject=number_of_super_particles_to_inject,
-        )
+        with context:
+            particulator.seeding(
+                seeded_particle_index=seeded_particle_index,
+                seeded_particle_multiplicity=seeded_particle_multiplicity,
+                seeded_particle_extensive_attributes=seeded_particle_extensive_attributes,
+                number_of_super_particles_to_inject=number_of_super_particles_to_inject,
+            )
 
-        # assert
-        assert (
-            number_of_super_particles_to_inject
-            == particulator.attributes.super_droplet_count
-        )
+            # assert
+            assert (
+                number_of_super_particles_to_inject
+                == particulator.attributes.super_droplet_count
+            )
 
     @staticmethod
     @pytest.mark.parametrize(
-        "seeded_particle_index",
+        "seeded_particle_index, context",
         (
-            [0, 0, 0],
-            [0, 1, 2],
-            [2, 1, 0],
-            pytest.param([0], marks=pytest.mark.xfail(strict=True)),
+            ([0, 0, 0], nullcontext()),
+            ([0, 1, 2], nullcontext()),
+            ([2, 1, 0], nullcontext()),
+            ([0], pytest.raises(ValueError, match=" multiple super particles with the same attributes")),
         ),
     )
     def test_seeded_particle_index_multiplicity_extensive_attributes(
         seeded_particle_index,
+        context,
         n_sd=3,
         number_of_super_particles_to_inject=3,
         dt=1,
@@ -123,21 +124,22 @@ class TestSeeding:
         )
 
         # act
-        particulator.seeding(
-            seeded_particle_index=seeded_particle_index_impl,
-            seeded_particle_multiplicity=seeded_particle_multiplicity_impl,
-            seeded_particle_extensive_attributes=seeded_particle_extensive_attributes_impl,
-            number_of_super_particles_to_inject=number_of_super_particles_to_inject,
-        )
+        with context:
+            particulator.seeding(
+                seeded_particle_index=seeded_particle_index_impl,
+                seeded_particle_multiplicity=seeded_particle_multiplicity_impl,
+                seeded_particle_extensive_attributes=seeded_particle_extensive_attributes_impl,
+                number_of_super_particles_to_inject=number_of_super_particles_to_inject,
+            )
 
-        # assert
-        np.testing.assert_array_equal(
-            particulator.attributes["multiplicity"].to_ndarray(),
-            np.asarray(seeded_particle_multiplicity)[seeded_particle_index],
-        )
-        np.testing.assert_array_equal(
-            particulator.attributes["water mass"].to_ndarray(),
-            np.asarray(seeded_particle_extensive_attributes["water mass"])[
-                seeded_particle_index
-            ],
-        )
+            # assert
+            np.testing.assert_array_equal(
+                particulator.attributes["multiplicity"].to_ndarray(),
+                np.asarray(seeded_particle_multiplicity)[seeded_particle_index],
+            )
+            np.testing.assert_array_equal(
+                particulator.attributes["water mass"].to_ndarray(),
+                np.asarray(seeded_particle_extensive_attributes["water mass"])[
+                    seeded_particle_index
+                ],
+            )
