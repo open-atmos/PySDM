1d5c4ae8cbf78981a53ca670a487baa6a790fde4
pawel <pawel.magnu@gmail.com>
2024-10-30 13:26:38 +0100

    metadata for pypi

--- examples/setup.py
+++ examples/setup.py
@@ -1,13 +1,19 @@
 import os
+import re
 import platform
 
 from setuptools import find_packages, setup
 
 
 def get_long_description():
-    with open("README.md", "r", encoding="utf8") as file:
-        long_description = file.read()
-    return long_description
+    """returns contents of the pdoc landing site with pdoc links converted into URLs"""
+    with open(
+            "docs/pysdm_examples_landing.md", "r", encoding="utf8"
+    ) as file:
+        pdoc_links = re.compile(
+            r"(`)([\w\d_-]*).([\w\d_-]*)(`)", re.MULTILINE | re.UNICODE
+        )
+        return pdoc_links.sub(r'<a href="\2/\3.html">\3</a>', file.read())
 
 
 CI = "CI" in os.environ
