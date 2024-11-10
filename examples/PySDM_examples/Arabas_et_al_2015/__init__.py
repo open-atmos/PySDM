5a542ca640b590656dfc23cc8e85e7ff6f882672
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-11-10 02:37:40 +0100

    fix merge conflict

--- examples/PySDM_examples/Arabas_et_al_2015/__init__.py
+++ examples/PySDM_examples/Arabas_et_al_2015/__init__.py
@@ -2,6 +2,12 @@
 """
 2D prescribed-flow case extended with Paraview visualisation with spin-up logic from
 [Arabas et al. 2015](http://doi.org/10.5194/gmd-8-1677-2015)
+
+gui.ipynb:
+.. include:: ./gui.ipynb.badges.md
+
+paraview_hello_world.ipynb:
+.. include:: ./paraview_hello_world.ipynb.badges.md
 """
 from .settings import Settings
 from .spin_up import SpinUp
