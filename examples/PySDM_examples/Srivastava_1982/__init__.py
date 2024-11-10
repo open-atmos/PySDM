4315ea3199798a2530f72514ba9169a47209a30a
Paweł Magnuszewski <47724273+pawelmagnu@users.noreply.github.com>
2024-11-10 02:23:57 +0100

    documentation beef up (incl. example gallery and badge generation) (#1407)
    Co-authored-by: Sylwester Arabas <sylwester.arabas@agh.edu.pl>

--- examples/PySDM_examples/Srivastava_1982/__init__.py
+++ examples/PySDM_examples/Srivastava_1982/__init__.py
@@ -1,6 +1,9 @@
 """
 box-model examples feat. analytic solution for breakup from
 [Srivastava 1982 (JAS)](https://doi.org/10.1175/1520-0469(1982)039%3C1317:ASMOPC%3E2.0.CO;2)
+
+figures.ipynb:
+.. include:: ./figures.ipynb.badges.md
 """
 
 from .equations import Equations, EquationsHelpers
--- examples/PySDM_examples/deJong_Azimi/__init__.py
+++ examples/PySDM_examples/deJong_Azimi/__init__.py
@@ -1,6 +1,12 @@
 """
 box- and single-column coalescence-focused examples used to test new
 moment-based microphysics in (Cloudy.jl)[https://github.com/CliMA/Cloudy.jl]
+
+box.ipynb:
+.. include:: ./box.ipynb.badges.md
+
+rainshaft.ipynb:
+.. include:: ./rainshaft.ipynb.badges.md
 """
 
 # pylint: disable=invalid-name
