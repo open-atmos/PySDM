42b2799256bc06d73a950e4d07fe22deeeb03e2a
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-11-29 01:03:53 +0100

    yet another try at sorting out circular import

--- PySDM/physics/dimensional_analysis.py
+++ PySDM/physics/dimensional_analysis.py
@@ -6,20 +6,24 @@ for use in unit tests which disables Numba and enables Pint
 from importlib import reload
 
 class DimensionalAnalysis:
-    # pylint: disable=import-outside-toplevel
-    from PySDM import formulae
-    from PySDM import physics
-    from . import constants, constants_defaults
-    from .impl import flag
-
-    def __enter__(*_):  # pylint: disable=no-method-argument,no-self-argument
+    def __enter__(*_):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
+        from PySDM import formulae
+        from PySDM import physics
+        from . import constants, constants_defaults
+        from .impl import flag
+        
         flag.DIMENSIONAL_ANALYSIS = True
         reload(constants)
         reload(constants_defaults)
         reload(formulae)
         reload(physics)
 
-    def __exit__(*_):  # pylint: disable=no-method-argument,no-self-argument
+    def __exit__(*_):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
+        from PySDM import formulae
+        from PySDM import physics
+        from . import constants, constants_defaults
+        from .impl import flag
+
         flag.DIMENSIONAL_ANALYSIS = False
         reload(constants)
         reload(constants_defaults)
