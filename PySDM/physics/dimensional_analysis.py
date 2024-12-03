79ad79b6e031ab4d23a9cc07981cbeacd2aa9a09
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-12-03 01:47:09 +0100

    skip __init__ refactor

--- PySDM/physics/dimensional_analysis.py
+++ PySDM/physics/dimensional_analysis.py
@@ -5,20 +5,25 @@ for use in unit tests which disables Numba and enables Pint
 
 from importlib import reload
 
+
 class DimensionalAnalysis:
-    def __enter__(*_):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
+    def __enter__(
+        *_,
+    ):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
         from PySDM import formulae
         from PySDM import physics
         from . import constants, constants_defaults
         from .impl import flag
-        
+
         flag.DIMENSIONAL_ANALYSIS = True
         reload(constants)
         reload(constants_defaults)
         reload(formulae)
         reload(physics)
 
-    def __exit__(*_):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
+    def __exit__(
+        *_,
+    ):  # pylint: disable=no-method-argument,no-self-argument,import-outside-toplevel
         from PySDM import formulae
         from PySDM import physics
         from . import constants, constants_defaults
