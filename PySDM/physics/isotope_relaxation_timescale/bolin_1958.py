9a28c7811997df62251aa2c070b5bd8774cce8eb
Agnieszka Żaba <azaba@agh.edu.pl>
2024-12-05 00:23:53 +0100

    Revert "cleanup"
    This reverts commit c2521fff1a1a2ee78af267ed5be89c049139fe34.

--- PySDM/physics/isotope_relaxation_timescale/bolin_1958.py
+++ PySDM/physics/isotope_relaxation_timescale/bolin_1958.py
@@ -3,10 +3,8 @@
 
 import numpy as np
 
-
 class Bolin1958:  # pylint: disable=too-few-public-methods
     """Implementation of timescale used by Bolin 1958"""
-
     def __init__(self, const):
         assert np.isfinite(const.BOLIN_C1)
 
@@ -14,4 +12,4 @@ class Bolin1958:  # pylint: disable=too-few-public-methods
     # pylint: disable=too-many-arguments
     def tau(const, radius, r_dr_dt):
         """timescale for evaporation of a falling drop with tritium"""
-        return (-3 / radius**2 * r_dr_dt * const.BOLIN_C1) ** -1
+        return (-3 / radius**2 * r_dr_dt * const.BOLIN_C1)**-1
