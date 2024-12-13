b19a5f24a268e09257eb4c884d197770ca4b0746
sfonxu <wronski.michal4@wp.pl>
2024-12-13 09:07:56 +0100

    fix path in docs script

--- .github/workflows/docs.py
+++ .github/workflows/docs.py
@@ -9,10 +9,10 @@ out_path = sys.argv[2]
 os.chdir(code_path)
 os.environ["PDOC_ALLOW_EXEC"] = "1"
 
-for notebook_path in glob.glob("PySDM_examples/*/*.ipynb"):
+for notebook_path in glob.glob("examples/PySDM_examples/*/*.ipynb"):
     with open(notebook_path, encoding="utf8") as fin:
         with open(notebook_path + ".badges.md", 'w') as fout:
             fout.write(nbformat.read(fin, nbformat.NO_CONVERT).cells[0].source)
-subprocess.run(["python", "-We", "-m", "pdoc", "-o", "html", "PySDM","examples/PySDM_examples", "-t", "docs/templates", "--math", "--mermaid"],env=os.environ)
+subprocess.run(["python", "-We", "-m", "pdoc", "-o", "html", "PySDM","examples/PySDM_examples", "-t", "docs/templates", "--math", "--mermaid"],env=os.environ,check=True)
 
 
