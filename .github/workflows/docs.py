329c29251926dfbcb6e696381eeb117c3d3d4595
sfonxu <wronski.michal4@wp.pl>
2024-12-13 09:19:59 +0100

    fix formatting

--- .github/workflows/docs.py
+++ .github/workflows/docs.py
@@ -11,8 +11,23 @@ os.environ["PDOC_ALLOW_EXEC"] = "1"
 
 for notebook_path in glob.glob("examples/PySDM_examples/*/*.ipynb"):
     with open(notebook_path, encoding="utf8") as fin:
-        with open(notebook_path + ".badges.md", 'w') as fout:
+        with open(notebook_path + ".badges.md", "w") as fout:
             fout.write(nbformat.read(fin, nbformat.NO_CONVERT).cells[0].source)
-subprocess.run(["python", "-We", "-m", "pdoc", "-o", "html", "PySDM","examples/PySDM_examples", "-t", "docs/templates", "--math", "--mermaid"],env=os.environ,check=True)
-
-
+subprocess.run(
+    [
+        "python",
+        "-We",
+        "-m",
+        "pdoc",
+        "-o",
+        "html",
+        "PySDM",
+        "examples/PySDM_examples",
+        "-t",
+        "docs/templates",
+        "--math",
+        "--mermaid",
+    ],
+    env=os.environ,
+    check=True,
+)
