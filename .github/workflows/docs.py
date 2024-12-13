1a11ac4522652c5591e6c2803b227ef806c2e288
sfonxu <wronski.michal4@wp.pl>
2024-12-13 08:51:16 +0100

    added docs-generating script

---
+++ .github/workflows/docs.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import sys
+import glob, nbformat
+
+code_path = sys.argv[1]
+out_path = sys.argv[2]
+
+os.chdir(code_path)
+os.environ["PDOC_ALLOW_EXEC"] = "1"
+
+for notebook_path in glob.glob("PySDM_examples/*/*.ipynb"):
+    with open(notebook_path, encoding="utf8") as fin:
+        with open(notebook_path + ".badges.md", 'w') as fout:
+            fout.write(nbformat.read(fin, nbformat.NO_CONVERT).cells[0].source)
+subprocess.run(["python", "-We", "-m", "pdoc", "-o", "html", "PySDM","examples/PySDM_examples", "-t", "docs/templates", "--math", "--mermaid"],env=os.environ)
+
+
