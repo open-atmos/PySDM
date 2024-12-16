32a3fa67c702e520b288645e0f8c46b204420910
sfonxu <wronski.michal4@wp.pl>
2024-12-16 17:55:07 +0100

    changes regarding first three points of review

--- .github/workflows/docs.py
+++ .github/workflows/docs.py
@@ -1,6 +1,5 @@
 """Script to generate docs based on given input directory (first argument)
 and output directory (second argument)"""
-import os
 import subprocess
 import sys
 import glob
@@ -9,21 +8,18 @@ import nbformat
 code_path = sys.argv[1]
 out_path = sys.argv[2]
 
-os.chdir(code_path)
-os.environ["PDOC_ALLOW_EXEC"] = "1"
-
-for notebook_path in glob.glob("examples/PySDM_examples/*/*.ipynb"):
+for notebook_path in glob.glob(f"{code_path}/examples/PySDM_examples/*/*.ipynb"):
     with open(notebook_path, encoding="utf8") as fin:
         with open(notebook_path + ".badges.md", "w", encoding="utf8") as fout:
             fout.write(nbformat.read(fin, nbformat.NO_CONVERT).cells[0].source)
 subprocess.run(
     [
-        "python",
+        sys.executable,
         "-We",
         "-m",
         "pdoc",
         "-o",
-        "html",
+        f"{out_path}/html",
         "PySDM",
         "examples/PySDM_examples",
         "-t",
@@ -31,6 +27,6 @@ subprocess.run(
         "--math",
         "--mermaid",
     ],
-    env=os.environ,
+    env={"PDOC_ALLOW_EXEC": "1"},
     check=True,
 )
