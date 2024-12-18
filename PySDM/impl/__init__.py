94f38f99eaf7861444c16b1ed33d126e7d5473ca
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-12-18 11:16:32 +0100

    use proper "no-local-version" instead of the hack (thanks @RonnyPfannschmidt!)

--- PySDM/impl/__init__.py
+++ PySDM/impl/__init__.py
@@ -1,3 +1,1 @@
 """ stuff not intended to be used from user code """
-
-from .scm_version import scheme
--- PySDM/impl/scm_version.py
+++
@@ -1,6 +0,0 @@
-""" overriding defaults for setuptools_scm (see pyproject.toml) """
-
-
-def scheme(_):
-    """removes git hash from version string to avoid clash with PyPI rules"""
-    return ""
