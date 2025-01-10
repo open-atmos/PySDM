f6f26bbafd2f9645b22484200712f775fe34901b
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2025-01-10 23:41:23 +0100

    change import order

--- PySDM/physics/homogeneous_liquid_nucleation_rate/__init__.py
+++ PySDM/physics/homogeneous_liquid_nucleation_rate/__init__.py
@@ -2,6 +2,6 @@
 homogeneous liquid droplet nucleation rate formulations
 """
 
+from PySDM.impl.null_physics_class import Null
 from .cnt import CNT
 from .constant import Constant
-from PySDM.impl.null_physics_class import Null
