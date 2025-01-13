f32763b6a205083f61b755a6b201fff4250cc4eb
olastrz <strzabalaaleksandra15@gmail.com>
2025-01-13 14:09:00 +0100

    one global var less

--- examples/PySDM_examples/utils/finalanimation.py
+++ examples/PySDM_examples/utils/finalanimation.py
@@ -25,68 +25,66 @@ sd_attributespvd = OpenDataFile(args.attributes_path)
 
 
 #startup_settings
-renderView1 = GetActiveViewOrCreate('RenderView')
+from collections import namedtuple
+x = {
+    'renderView1': GetActiveViewOrCreate('RenderView')
+}
+x = namedtuple("X", x.keys())(**x)
+
 multiplicityLUT = GetColorTransferFunction('multiplicity')
-sd_attributespvdDisplay = Show(sd_attributespvd, renderView1, 'UnstructuredGridRepresentation')
-sd_attributespvdDisplay = GetDisplayProperties(sd_attributespvd, view=renderView1)
+sd_attributespvdDisplay = Show(sd_attributespvd, x.renderView1, 'UnstructuredGridRepresentation')
+sd_attributespvdDisplay = GetDisplayProperties(sd_attributespvd, view=x.renderView1)
 effectiveradiusLUT = GetColorTransferFunction('effectiveradius')
 multiplicityLUT.RescaleTransferFunction(19951.0, 50461190157.0)
 effectiveradiusLUT.RescaleTransferFunction(0.1380997175392798, 207.7063518856934)
-sd_productspvdDisplay = GetDisplayProperties(sd_productspvd, view=renderView1)
-sd_productspvdDisplay.SetRepresentationType('Surface With Edges')
-renderView1.Update()
+sd_productspvdDisplay = GetDisplayProperties(sd_productspvd, view=x.renderView1)
+x.renderView1.Update()
 
 
-def create_new_calculator(registrationame, input, representation, function, color_by1, color_by2, color_by3, scalar_coloring = False, hide = False):
+def create_new_calculator(input, representation, function, color_by1, color_by2, color_by3, scalar_coloring = False, hide = False, *, y, registrationame):
     calculator = Calculator(registrationName=registrationame, Input=input)
-    display = Show(calculator, renderView1, representation)
+    display = Show(calculator, y.renderView1, representation)
     calculator.Function = function
-    renderView1.Update()
+    y.renderView1.Update()
     if scalar_coloring == True:
         ColorBy(display, (color_by1, color_by2, color_by3))
     else:
         None
     if hide == True:
-        Hide(calculator, renderView1)
+        Hide(calculator, y.renderView1)
     else:
         None
-    renderView1.Update()
-
-calculator1 = create_new_calculator('Calculator1', sd_attributespvd, 'UnstructuredGridRepresentation', '"relative fall velocity"*(-iHat)', 'None', 'None', 'None')
+    y.renderView1.Update()
 
 
-def scalar_bar():
-    calculator1Display = Show(calculator1, renderView1, 'UnstructuredGridRepresentation')
-    calculator1Display.SetScalarBarVisibility(renderView1, True)
-    scalarBar = GetScalarBar(effectiveradiusLUT, renderView1)
+def scalar_bar(*, y):
+    calculator1Display = Show(calculator1, y.renderView1, 'UnstructuredGridRepresentation')
+    calculator1Display.SetScalarBarVisibility(y.renderView1, True)
+    scalarBar = GetScalarBar(effectiveradiusLUT, y.renderView1)
     scalarBar.ComponentTitle = ""
     scalarBar.Title = "effective radius [um]"
-    renderView1.Update()
-
-scalar_bar()
+    y.renderView1.Update()
 
 
-def create_glyph(registration_name, input, scale_array1, scale_array2, color_by = False):
+
+def create_glyph(registration_name, input, scale_array1, scale_array2, color_by = False, *, y):
     glyph = Glyph(registrationName=registration_name, Input=input,
         GlyphType='Arrow')
-    glyphDisplay = Show(glyph, renderView1, 'GeometryRepresentation')
+    glyphDisplay = Show(glyph, y.renderView1, 'GeometryRepresentation')
     glyphDisplay.Representation = 'Surface'
     glyph.ScaleArray = [scale_array1, scale_array2]
     glyph.ScaleFactor = 100
-    glyphDisplay.SetScalarBarVisibility(renderView1, True)
+    glyphDisplay.SetScalarBarVisibility(y.renderView1, True)
     if color_by == True:
         ColorBy(glyphDisplay, None)
     else:
         None
-    renderView1.Update()
-
-glyph1 = create_glyph('Glyph1', calculator1, 'POINTS', 'relative fall velocity')
-
-calculator2 = create_new_calculator('Calculator2', sd_productspvd, 'StructuredGridRepresentation', 'cx*jHat+cy*iHat', 'CELLS', 'Result', 'Magnitude', True, True)
+    y.renderView1.Update()
 
 
-def apply_presets_logscale_opacity_and_update():
-    calculator1Display = Show(calculator1, renderView1, 'UnstructuredGridRepresentation')
+
+def apply_presets_logscale_opacity_and_update(*, y):
+    calculator1Display = Show(calculator1, y.renderView1, 'UnstructuredGridRepresentation')
     multiplicityLUT.ApplyPreset(args.multiplicity_preset, True)
     if args.multiplicity_logscale:
         multiplicityLUT.MapControlPointsToLogSpace()
@@ -105,38 +103,32 @@ def apply_presets_logscale_opacity_and_update():
 
     effectiveradiusLUT.NanColor = args.effectiveradius_nan_color
 
+    sd_productspvdDisplay.SetRepresentationType('Surface With Edges')
     sd_productspvdDisplay.Opacity = args.sd_products_opacity
     calculator1Display.Opacity = args.calculator1_opacity
     sd_attributespvdDisplay.Opacity = args.sd_attributes_opacity
 
-    renderView1.Update()
-
-apply_presets_logscale_opacity_and_update()
-
-glyph2 = create_glyph('Glyph2', calculator2, 'CELLS', 'Result', True)
+    y.renderView1.Update()
 
 
-def get_layout():
+
+
+def get_layout(*, y):
     layout1 = GetLayout()
     layout1.SetSize(1205, 739)
-    renderView1.Update()
+    y.renderView1.Update()
 
-get_layout()
-
-
-def set_current_camera_placement():
-    renderView1.InteractionMode = '2D'
-    renderView1.CameraPosition = [836.5045867211775, 677.8909274570431, -4098.0762113533165]
-    renderView1.CameraFocalPoint = [836.5045867211775, 677.8909274570431, 0.0]
-    renderView1.CameraViewUp = [1.0, 0.0, 0.0]
-    renderView1.CameraParallelScale = 1060.6601717798205
-    renderView1.Update()
-
-set_current_camera_placement()
+def set_current_camera_placement(*, y):
+    y.renderView1.InteractionMode = '2D'
+    y.renderView1.CameraPosition = [836.5045867211775, 677.8909274570431, -4098.0762113533165]
+    y.renderView1.CameraFocalPoint = [836.5045867211775, 677.8909274570431, 0.0]
+    y.renderView1.CameraViewUp = [1.0, 0.0, 0.0]
+    y.renderView1.CameraParallelScale = 1060.6601717798205
+    y.renderView1.Update()
 
 
-def axes_settings():
-    view = renderView1
+
+def axes_settings(*, view):
     view.ViewSize = [2000, 800]
     view.Background = [1, 1, 1]
     view.CenterAxesVisibility = True
@@ -161,42 +153,51 @@ def axes_settings():
     axesGrid.YTitleColor = [0, 0, 0]
     axesGrid.YLabelColor = [0, 0, 0]
     axesGrid.GridColor = [0.1, 0.1, 0.1]
-    renderView1.CenterAxesVisibility = False
-    renderView1.Update()
-
-axes_settings()
+    view.CenterAxesVisibility = False
+    view.Update()
 
 
-def time_annotation():
+
+
+def time_annotation(*, y):
     time = AnnotateTimeFilter(guiName = "AnnotateTimeFilter1", Format = 'Time:{time:f}s')
-    repr = Show(time, renderView1)
-    renderView1.Update()
-
-time_annotation()
+    repr = Show(time, y.renderView1)
+    y.renderView1.Update()
 
 
-def text(text_in, position_y): 
+
+def text(text_in, position_y, *, view): 
     text = Text()
     text.Text = text_in
-    textDisplay = Show(text, renderView1)
+    textDisplay = Show(text, view)
     textDisplay.Color = [1.0, 1.0, 1.0]  
     textDisplay.WindowLocation = 'Any Location'
     textDisplay.Position = [0.01, position_y]
 
-text("Arrows scale with Courant number C=u*Δt/Δx,", 0.7)
-text("reflecting the grid spacing Δx and Δy.", 0.65)
 
+calculator1 = create_new_calculator(sd_attributespvd, 'UnstructuredGridRepresentation', '"relative fall velocity"*(-iHat)', 'None', 'None', 'None', y=x, registrationame='Calculator1')
+scalar_bar(y=x)
+glyph1 = create_glyph('Glyph1', calculator1, 'POINTS', 'relative fall velocity', y=x)
+calculator2 = create_new_calculator(sd_productspvd, 'StructuredGridRepresentation', 'cx*jHat+cy*iHat', 'CELLS', 'Result', 'Magnitude', True, True, y=x, registrationame='Calculator2')
+apply_presets_logscale_opacity_and_update(y=x)
+glyph2 = create_glyph('Glyph2', calculator2, 'CELLS', 'Result', True, y=x)
+get_layout(y=x)
+set_current_camera_placement(y=x)
+axes_settings(view=x.renderView1)
+time_annotation(y=x)
+text("Arrows scale with Courant number C=u*Δt/Δx,", 0.7, view=x.renderView1)
+text("reflecting the grid spacing Δx and Δy.", 0.65, view=x.renderView1)
 
 # save animation to an Ogg Vorbis file
-SaveAnimation('output/anim2.ogv', renderView1, FrameRate=5)
+SaveAnimation('output/anim2.ogv', x.renderView1, FrameRate=5)
 # save animation frame as pdfs
 for t in sd_productspvd.TimestepValues:
-    renderView1.ViewTime = t
+    x.renderView1.ViewTime = t
     for reader in (sd_productspvd, sd_attributespvd):
         reader.UpdatePipeline(t)    
         ExportView(
             filename=f'output/anim_frame_{t}.pdf',
-            view=renderView1,
+            view=x.renderView1,
             Rasterize3Dgeometry= False,
             GL2PSdepthsortmethod= 'BSP sorting (slow, best)',
     )
