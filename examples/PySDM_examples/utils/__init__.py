65df8894982d0fbc56e75e0f2da5f7e3fb911fbf
Agnieszka Żaba <azaba@agh.edu.pl>
2025-01-21 19:23:20 +0100

    delete notebook_vars.py from PySDM_examples.utils in favor of jupyter-utils import

--- examples/PySDM_examples/utils/__init__.py
+++ examples/PySDM_examples/utils/__init__.py
@@ -4,6 +4,6 @@ reusable commons for examples
 
 from .basic_simulation import BasicSimulation
 from .dummy_controller import DummyController
-from .notebook_vars import notebook_vars
 from .progbar_controller import ProgBarController
 from .read_vtk_1d import readVTK_1d
+from open_atmos_jupyter_utils import notebook_vars
