31364d71cedb326c742176e3fcd6707edee12e60
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-02-23 23:50:55 +0100

    temporarily move all planet code into the notebook

--- PySDM/physics/planet_data.py
+++
@@ -1,9 +0,0 @@
-class PlanetData():
-    def __init__(self, g0, Rd, R_str):
-        self.g0 = g0
-        self.Rd = Rd
-        self.R_str = R_str
-
-    def g(self, z):
-        acc = self.g0 * (self.Rd / (self.Rd + z)) ** 2
-        return acc
