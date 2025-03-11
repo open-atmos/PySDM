1803f07d947b2df1ed541fd4d4c39f35f009034b
Agnieszka Żaba <azaba@agh.edu.pl>
2025-03-11 22:08:17 +0100

    add imports to init

--- PySDM/physics/isotope_relaxation_timescale/__init__.py
+++ PySDM/physics/isotope_relaxation_timescale/__init__.py
@@ -2,6 +2,8 @@
 isotope relaxation timescale formulae
 """
 
-from PySDM.impl.null_physics_class import Null
 from .miyake_et_al_1968 import MiyakeEtAl1968
 from .bolin_1958 import Bolin1958
+from .zaba_and_arabas_2025 import ZabaAndArabas2025
+from .jouzel_et_al_1975 import JouzelEtAl1975
+from .stewart_1975 import Stewart1975
