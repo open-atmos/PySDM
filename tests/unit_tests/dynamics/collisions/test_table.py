d81238c76c3b0f0361050486a7341407b999d3c3
Jason <jbarr4@uw.edu>
2025-03-11 14:29:00 -0700

    Test for hall effeciencies

---
+++ tests/unit_tests/dynamics/collisions/test_table.py
@@ -0,0 +1,10 @@
+from hall import Hall
+
+
+def test_table():
+    hall = Hall()
+    collector_radius_m = 2e-5
+    collected_radius_m = 1e-6
+    result1 = hall.table(collected_radius_m, collector_radius_m)
+    result2 = hall.table(collector_radius_m, collected_radius_m)
+    assert result1 == result2 == 1e-4, "Test failed"
