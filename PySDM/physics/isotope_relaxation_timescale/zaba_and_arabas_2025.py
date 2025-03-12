49c5e8baaeb77ccf0ccb40450fcc7078e20980cb
Agnieszka Żaba <azaba@agh.edu.pl>
2025-03-12 15:58:05 +0100

    fix ZabaAndArabas formula for tau; remove unused import

--- PySDM/physics/isotope_relaxation_timescale/zaba_and_arabas_2025.py
+++ PySDM/physics/isotope_relaxation_timescale/zaba_and_arabas_2025.py
@@ -7,29 +7,34 @@ class ZabaAndArabas2025:
 
     @staticmethod
     def tau(  # pylint: disable=too-many-arguments
+        const,
         radius,
         alpha_env,
         D_iso,
         vent_coeff_iso,
         k_coeff_iso,
-        e_env,
-        e_iso_env,
-        rho_liq,
+        e_s,
         saturation,
         R_vap_env,
+        temperature,
+        M_iso,
     ):
         """e-fold timescale with alpha and water vapour pressures heavy and light water
         calculated in the temperature of environment"""
         return (
-            radius**2
+            -(radius**2)
             * alpha_env
-            * rho_liq
+            * const.rho_w
+            / e_s
+            * const.R_str
+            * temperature
             / 3
             / vent_coeff_iso
             / k_coeff_iso
             / D_iso
             * R_vap_env
-            / (e_iso_env / e_env * saturation / R_vap_env - 1)
+            / (saturation - 1)
+            / M_iso
         )
 
     @staticmethod
