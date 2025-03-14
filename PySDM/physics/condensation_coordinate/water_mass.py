6c8e1fdca64892f5f8f550d253737a77c4928e13
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-03-14 11:54:57 +0100

    addressing pylint hints

--- PySDM/physics/condensation_coordinate/water_mass.py
+++ PySDM/physics/condensation_coordinate/water_mass.py
@@ -8,13 +8,13 @@ class WaterMass:
         pass
 
     @staticmethod
-    def dx_dt(const, x, dm_dt):
+    def dx_dt(x, dm_dt):  # pylint: disable=unused-argument
         return dm_dt
 
     @staticmethod
-    def mass(_, x):
+    def mass(x):
         return x
 
     @staticmethod
-    def x(_, mass):
+    def x(mass):
         return mass
