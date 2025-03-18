ff58f7e74f0e19b0b73f5cb9f3c47d5fefa3a7f7
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-03-19 00:25:10 +0100

    debug prints + pytest filter

--- PySDM/physics/isotope_ratio_evolution/merlivat_and_jouzel_1979.py
+++ PySDM/physics/isotope_ratio_evolution/merlivat_and_jouzel_1979.py
@@ -11,6 +11,15 @@ class MerlivatAndJouzel1979:  # pylint: disable=too-few-public-methods
 
     @staticmethod
     def d_Rv_over_Rv(_, alpha, d_alpha, n_vapour, d_n_vapour, n_liquid):
+        print(
+            alpha,
+            d_alpha,
+            n_vapour,
+            d_n_vapour,
+            n_liquid,
+            ((alpha - 1) * d_n_vapour - n_liquid * d_alpha),
+            n_vapour + alpha * n_liquid,
+        )
         return ((alpha - 1) * d_n_vapour - n_liquid * d_alpha) / (
             n_vapour + alpha * n_liquid
         )
