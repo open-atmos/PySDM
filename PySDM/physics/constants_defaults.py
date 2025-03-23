b2b888800e2b3571803e3472b1e3e6c83a07dab7
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-03-23 12:51:25 +0100

    bib fix, removing code that is part of homogeneous freezing PR #1488

--- PySDM/physics/constants_defaults.py
+++ PySDM/physics/constants_defaults.py
@@ -100,7 +100,7 @@ HAC = 1.0
 
 # mass and heat accommodation coefficients for vapour deposition on ice
 MAC_ice = 0.5
-""" mass accommodation coefficient for vapour deposition  as recommended in
+""" mass accommodation coefficient for vapour deposition as recommended in
 [Kaercher & Lohmann 2002](https://doi.org/10.1029/2001JD000470) """
 HAC_ice = 0.7
 """ thermal accommodation coefficient for vapour deposition as recommended in
