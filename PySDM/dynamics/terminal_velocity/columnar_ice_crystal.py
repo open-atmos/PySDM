3745b928ab5ea5250a817f9e01e227c4cb6173fe
Tim Luettmer <tluettm@uni-mainz.de>
2025-03-24 17:27:06 +0100

    added empirical parametrisation for columnar ice crystals

---
+++ PySDM/dynamics/terminal_velocity/columnar_ice_crystal.py
@@ -0,0 +1,16 @@
+"""
+[Spichtinger & Gierens 2009](https://doi.org/10.5194/acp-9-685-2009)
+Eq. (18) .Assumed shape is columnar based on empirical parameterizations of
+[Heymsfield & Iaquinta (2000)](https://doi.org/10.1175/1520-0469(2000)057<0916:CCTV>2.0.CO;2)
+[Barthazy & Schefold (2006)](https://doi.org/10.1016/j.atmosres.2005.12.009)
+"""
+
+class columnar_ice_crystal:  # pylint: disable=too-few-public-methods
+    def __init__(self, particulator):
+        self.particulator = particulator
+
+    def __call__(self, output, mass):
+        self.particulator.backend.terminal_velocity(
+            values=output.data,
+            mass=mass.data,
+        )
--- PySDM/physics/terminal_velocity/__init__.py
+++ PySDM/physics/terminal_velocity/__init__.py
@@ -3,3 +3,4 @@
 from .rogers_yau import RogersYau
 from .gunn_kinzer_1949 import GunnKinzer1949
 from .power_series import PowerSeries
+from .columnar_ice_crystal import columnar_ice_crystal
