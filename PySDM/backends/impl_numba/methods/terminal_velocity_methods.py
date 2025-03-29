bf4033f443ea09db87c25d0608fcc238849fe44a
Tim Luettmer <tluettm@uni-mainz.de>
2025-03-29 16:08:43 +0100

    added check for negative radius (indicating ice mass) to RogersYau and GunnKinzer terminal velocity

--- PySDM/backends/impl_numba/methods/terminal_velocity_methods.py
+++ PySDM/backends/impl_numba/methods/terminal_velocity_methods.py
@@ -10,25 +10,28 @@ from PySDM.backends.impl_common.backend_methods import BackendMethods
 
 
 class TerminalVelocityMethods(BackendMethods):
+    # TODO: give terminal velocity functions name of the parametrisation
     @cached_property
     def _interpolation_body(self):
         @numba.njit(**self.default_jit_flags)
         def body(output, radius, factor, b, c):
             for i in numba.prange(len(radius)):  # pylint: disable=not-an-iterable
-                if radius[i] < 0:
-                    output[i] = 0
-                else:
+                if radius[i] > 0:
                     r_id = int(factor * radius[i])
                     r_rest = ((factor * radius[i]) % 1) / factor
                     output[i] = b[r_id] + r_rest * c[r_id]
+                # TODO: check if output 0 for radius 0 is necessary
+                elif radius == 0:
+                    output[i] = 0
 
         return body
 
+    # TODO: give terminal velocity functions name of the parametrisation
     def interpolation(self, *, output, radius, factor, b, c):
         return self._interpolation_body(
             output.data, radius.data, factor, b.data, c.data
         )
-
+    # TODO: give terminal velocity functions name of the parametrisation
     @cached_property
     def _terminal_velocity_body(self):
         v_term = self.formulae.terminal_velocity.v_term
@@ -36,10 +39,12 @@ class TerminalVelocityMethods(BackendMethods):
         @numba.njit(**self.default_jit_flags)
         def body(*, values, radius):
             for i in numba.prange(len(values)):  # pylint: disable=not-an-iterable
-                values[i] = v_term(radius[i])
+                if radius[i] >= 0.:
+                    values[i] = v_term(radius[i])
 
         return body
 
+    # TODO: give terminal velocity functions name of the parametrisation
     def terminal_velocity(self, *, values, radius):
         self._terminal_velocity_body(values=values, radius=radius)
 
