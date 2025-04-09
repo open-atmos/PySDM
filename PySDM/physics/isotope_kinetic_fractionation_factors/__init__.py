cd913bf0baee8affb11fad952c11b0806c815ce1
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-09 11:14:21 -0600

    add docstring to kinetic fractionation

--- PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
+++ PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
@@ -1,3 +1,7 @@
+"""
+kinetic fractionation factors for water isotopologues
+"""
+
 from PySDM.impl.null_physics_class import Null
 from .jouzel_and_merlivat_1984 import JouzelAndMerlivat1984
 from .pierchala_et_al_2022 import PierchalaEtAl2022
