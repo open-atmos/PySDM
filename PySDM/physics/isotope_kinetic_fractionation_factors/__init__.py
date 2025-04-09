a3ee3588e71719d3be75a5bdea01144e738029ff
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-04-10 00:38:04 +0200

    renaming pierchala_et_al_2022 kinetic formula to craig_gordon + new references & support for web.archive urls in docs

--- PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
+++ PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
@@ -4,4 +4,4 @@ kinetic fractionation factors for water isotopologues
 
 from PySDM.impl.null_physics_class import Null
 from .jouzel_and_merlivat_1984 import JouzelAndMerlivat1984
-from .pierchala_et_al_2022 import PierchalaEtAl2022
+from .craig_gordon import CraigGordon
