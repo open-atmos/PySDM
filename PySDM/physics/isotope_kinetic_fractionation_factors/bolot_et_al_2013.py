88b3208f49a9413d53511cee4675b9b2af7100c6
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-11 19:36:21 -0600

    Revert "take out formula for Ai"
    This reverts commit 7d4b331117282d2dea7d7de3fdb725a7fe416e0c.

--- PySDM/physics/isotope_kinetic_fractionation_factors/bolot_et_al_2013.py
+++ PySDM/physics/isotope_kinetic_fractionation_factors/bolot_et_al_2013.py
@@ -17,26 +17,21 @@ class BolotEtAl2013:  # pylint: disable=too-few-public-methods
         relative_humidity,
         heavy_to_light_diffusivity_ratio,
         heavy_to_light_ventilation_ratio,
-        Ai,
+        lv,
+        cp,
+        fv,
+        fh,
+        Le,
+        R_v,
+        T_inf,
+        water_vapor_density,
+        condensed_water_density,
     ):  # pylint: disable=too-many-positional-arguments too-many-arguments
         """
         water_vapor_density - density of water vapor at ambient temperature
         condensed_water_density - density of liquid or ice at ambient temperature
         """
-
-        effective_supersaturation = 1 / (1 - Ai * (1 - 1 / relative_humidity))
-        return effective_supersaturation / (
-            alpha_equilibrium
-            / heavy_to_light_diffusivity_ratio
-            / heavy_to_light_ventilation_ratio
-            * (effective_supersaturation - 1)
-            + 1
-        )
-
-    def Ai(
-        lv, cp, fv, fh, Le, R_v, T_inf, water_vapor_density, condensed_water_density
-    ):  # pylint: disable=too-many-positional-arguments too-many-arguments
-        return 1 / (
+        Ai = 1 / (
             1
             + lv
             / cp
@@ -48,4 +43,12 @@ class BolotEtAl2013:  # pylint: disable=too-few-public-methods
             / relative_humidity
             * water_vapor_density
             / condensed_water_density
+        )
+        effective_supersaturation = 1 / (1 - Ai * (1 - 1 / relative_humidity))
+        return effective_supersaturation / (
+            alpha_equilibrium
+            / heavy_to_light_diffusivity_ratio
+            / heavy_to_light_ventilation_ratio
+            * (effective_supersaturation - 1)
+            + 1
         )
