23cc77de3224623655d37f1b029d875628bcd9ba
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-15 16:46:26 +0200

    fix line too long in init

--- examples/PySDM_examples/Kinzer_And_Gunn_1951/__init__.py
+++ examples/PySDM_examples/Kinzer_And_Gunn_1951/__init__.py
@@ -1,5 +1,6 @@
 """
-based on [Kinzer & Gunn 1951 (J. Meteor.)](https://doi.org/10.1175/1520-0469(1951)008<0071:TETATR>2.0.CO;2)
+based on [Kinzer & Gunn 1951 (J. Meteor.)]
+(https://doi.org/10.1175/1520-0469(1951)008<0071:TETATR>2.0.CO;2)
 
 fig_7.ipynb:
 .. include:: ./fig_7.ipynb.badges.md
