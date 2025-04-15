a6d98bf95b188d45924d71d5e50a76d57ce6b95b
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-15 17:00:24 +0200

    update url for K&G to not contain "<", ">"

--- examples/PySDM_examples/Kinzer_And_Gunn_1951/table_1_and_2.py
+++ examples/PySDM_examples/Kinzer_And_Gunn_1951/table_1_and_2.py
@@ -1,4 +1,4 @@
-"""from [Kinzer & Gunn 1951](https://doi.org/10.1175/1520-0469(1951)008<0071:TETATR>2.0.CO;2)"""
+"""from [Kinzer & Gunn 1951](https://doi.org/10.1175/1520-0469(1951)008%3C0071:TETATR%3E2.0.CO;2)"""
 
 table1 = {
     "Diameter [cm]": (
