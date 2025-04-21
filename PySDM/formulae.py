1cc1b44fe7b64a0799ee22d0dec24acfc35d29e3
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-21 23:40:55 +0200

    fix url

--- PySDM/formulae.py
+++ PySDM/formulae.py
@@ -23,7 +23,7 @@ from PySDM.dynamics.terminal_velocity import GunnKinzer1949, PowerSeries, Rogers
 from PySDM.dynamics.terminal_velocity.gunn_and_kinzer import TpDependent
 
 
-class Formulae:  # pylint: disable=too-few-public-methods,too-many-instance-attributes
+class Formulae:  # pylint: disable=too-few-public-methods,too-many-instance-attributes,too-many-statements
     def __init__(  # pylint: disable=too-many-locals
         self,
         *,
@@ -64,7 +64,6 @@ class Formulae:  # pylint: disable=too-few-public-methods,too-many-instance-attr
         bulk_phase_partitioning: str = "Null",
         handle_all_breakups: bool = False,
     ):
-        # pylint disable=too-many-statements
         # initialisation of the fields below is just to silence pylint and to enable code hints
         # in PyCharm and alike, all these fields are later overwritten within this ctor
         self.optical_albedo = optical_albedo
