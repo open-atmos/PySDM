c552853c5a84a47e9617ffd3c36c71b2b7a6bf14
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-21 23:45:09 +0200

    address line too long

--- PySDM/physics/ventilation/pruppacher_rasmussen_1979.py
+++ PySDM/physics/ventilation/pruppacher_rasmussen_1979.py
@@ -1,12 +1,12 @@
 """
 ventilation coefficient as a function of dimensionless Reynolds (Re) and Schmidt (Sc)
 numbers for liquid drops following
-[Pruppacher & Rasmussen (1979)](https://doi.org/10.1175/1520-0469%281979%29036%3C1255:AWTIOT%3E2.0.CO;2)
+[Pruppacher & Rasmussen 1979](https://doi.org/10.1175/1520-0469%281979%29036%3C1255:AWTIOT%3E2.0.CO;2)
 NB: this parameterization is only experimentally validated for Re < 2600
 but is hypothesized to be valid for spheres with Re < 8 × 10⁴
 based on theory (Pruppacher & Rasmussen, 1979).
 the parameterization also does not account for effects of air turbulence.
-"""
+"""  # pylint disable=line-too-long
 
 import numpy as np
 
