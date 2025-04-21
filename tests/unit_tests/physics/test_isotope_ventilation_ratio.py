9f803b67273ffe6209a1e5ddc7ee491683233abb
Agnieszka Żaba <azaba@agh.edu.pl>
2025-04-22 00:09:51 +0200

    fix pytest

--- tests/unit_tests/physics/test_isotope_ventilation_ratio.py
+++ tests/unit_tests/physics/test_isotope_ventilation_ratio.py
@@ -2,24 +2,22 @@
 
 import pytest
 from PySDM import Formulae
-from PySDM.formulae import _choices
 from PySDM.physics import isotope_ventilation_ratio, constants_defaults
 from PySDM.physics.dimensional_analysis import DimensionalAnalysis
 
 
 class TestIsotopeVentilationRatio:  # pylint: disable=too-few-public-methods
     @staticmethod
-    @pytest.mark.parametrize("variant", _choices(isotope_ventilation_ratio))
-    def test_dimensionality(variant):
+    def test_dimensionality():
         with DimensionalAnalysis():
             # Arrange
-            formulae = Formulae(isotope_ventilation_ratio=variant)
+            formulae = Formulae(isotope_ventilation_ratio="Brutsaert1982")
             si = constants_defaults.si
-            sut = formulae.isotope_ventilation_ratio.isotope_ventilation_coefficient
+            sut = formulae.isotope_ventilation_ratio.isotope_ventilation_ratio
 
             # Act
             re = sut(
-                sqrt_re_times_cbrt_sc=1 * si.dimensionless,
+                ventilation_coefficient=1 * si.dimensionless,
                 diffusivity_ratio=1 * si.dimensionless,
             )
 
