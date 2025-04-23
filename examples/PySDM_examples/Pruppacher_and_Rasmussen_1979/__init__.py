2a9a418325e14dd5c55e254a8332e72c0267c6f7
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-04-24 00:12:20 +0200

    add notebook-based smoke test

---
+++ examples/PySDM_examples/Pruppacher_and_Rasmussen_1979/__init__.py
@@ -0,0 +1,10 @@
+"""
+based on Pruppacher and Rasmussen 1979 (J. Atmos. Sci.)
+https://doi.org/10.1175/1520-0469(1979)036%3C1255:AWTIOT%3E2.0.CO;2
+
+fig_1.ipynb:
+.. include:: ./fig_1.ipynb.badges.md
+
+"""
+
+# pylint: disable=invalid-name
