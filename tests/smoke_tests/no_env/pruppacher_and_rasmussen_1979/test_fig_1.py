e59fb6405d138a139ebee72975d64f48b064b716
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-04-24 00:17:45 +0200

    fix bib

--- tests/smoke_tests/no_env/pruppacher_and_rasmussen_1979/test_fig_1.py
+++ tests/smoke_tests/no_env/pruppacher_and_rasmussen_1979/test_fig_1.py
@@ -1,5 +1,6 @@
 """
-regression tests checking values against paper Fig 1
+regression tests checking values against paper Fig 1 from [Pruppacher and Rasmussen
+1979](https://doi.org/10.1175/1520-0469%281979%29036%3C1255:AWTIOT%3E2.0.CO;2)
 """
 
 from pathlib import Path
