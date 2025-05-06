53380c7c627f3249b7642837a6aaa53da5ddd47a
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-06 12:19:11 +0200

    add Jouzel and Merlivat example and fig 8

---
+++ examples/PySDM_examples/Jouzel_and_Merlivat_1975/thermodynamic_profiles.py
@@ -0,0 +1,29 @@
+import numpy as np
+from mpmath.matrices.eigen_symmetric import svd_c
+
+from PySDM.physics.constants_defaults import T0
+from scipy.interpolate import make_interp_spline
+from PySDM.physics import si
+
+
+pressure = make_interp_spline(
+    x=np.asarray([-10, -20, -30, -40, -50])[::-1] + T0,
+    y=np.asarray([925, 780, 690, 630, 600])[::-1] * si.mbar,
+)
+""" Table 1 """
+# TODO: np.nans on the edges
+
+
+def ice_saturation_curve_4(T):
+    return 0.99 - 0.006 * (T - T0)
+
+
+def vapour_mixing_ratio(formulae, T):
+    const = formulae.constants
+    svp = formulae.saturation_vapour_pressure
+
+    p_v = ice_saturation_curve_4(T) * svp.pvs_ice(T)
+    rho_v = p_v / const.R_v / T
+    p_d = pressure(T) - p_v
+    rho_d = p_d / const.R_d / T
+    return rho_v / rho_d
---
+++ tests/smoke_tests/no_env/jouzel_and_merlivat_1975/test_thermodynamic_profiles.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot
+from matplotlib.pyplot import ylabel
+
+from PySDM_examples.Jouzel_and_Merlivat_1975 import thermodynamic_profiles
+from PySDM import Formulae
+
+
+class TestThermodynamicProfiles:
+    @staticmethod
+    def test_pressure(plot=True):
+        # arrange
+        formulae = Formulae()
+        T = formulae.trivia.C2K(np.linspace(0, -60))
+        p = thermodynamic_profiles.pressure(T)
+
+        # act
+
+        # plot
+        pyplot.gca().set(
+            ylabel="Pressure [Pa]",
+            xlabel="Temperature [K]",
+        )
+        pyplot.plot(T, p)
+
+        if plot:
+            pyplot.show()
+        else:
+            pyplot.clf()
+
+        # assert
+        # TODO
