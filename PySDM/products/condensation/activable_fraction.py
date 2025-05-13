d915870bdc7af88ecfd63cd152bd8f8c6f957b20
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-05-13 11:42:28 +0200

    introduce multiple choices for activation criteria in `activable_fraction` product

--- PySDM/products/condensation/activable_fraction.py
+++ PySDM/products/condensation/activable_fraction.py
@@ -8,20 +8,25 @@ from PySDM.products.impl import MomentProduct, register_product
 
 @register_product()
 class ActivableFraction(MomentProduct):
-    def __init__(self, unit="dimensionless", name=None):
+    def __init__(self, unit="dimensionless", name=None, filter_attr="critical supersaturation"):
         super().__init__(name=name, unit=unit)
+        self.filter_attr = filter_attr
 
     def register(self, builder):
         super().register(builder)
-        builder.request_attribute("critical supersaturation")
+        builder.request_attribute(self.filter_attr)
 
     def _impl(self, **kwargs):
-        s_max = kwargs["S_max"]
+        if self.filter_attr == "critical supersaturation":
+            s_max = kwargs["S_max"]
+            filter_range = (0, 1 + s_max / 100) 
+        else if self.filter_attr == "wet to critical volume ratio":
+            filter_range = (1, np.inf)
         self._download_moment_to_buffer(
             attr="volume",
             rank=0,
-            filter_range=(0, 1 + s_max / 100),
-            filter_attr="critical supersaturation",
+            filter_range=filter_range,
+            filter_attr=self.filter_attr,
         )
         frac = self.buffer.copy()
         self._download_moment_to_buffer(attr="volume", rank=0)
