e5925e87e488d25fb50bbb5ade58a95429860773
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-15 22:07:12 +0200

    Revert "add Howell to drop growth options; refactor vent_factor to mass_vent and heat_vent"
    This reverts commit a6821bac

--- PySDM/physics/drop_growth/mason_1971.py
+++ PySDM/physics/drop_growth/mason_1971.py
@@ -1,6 +1,7 @@
 """
 single-equation approximation of the vapour and heat diffusion problem
 as given in eq. 3.11 in [Mason 1971](https://archive.org/details/physicsofclouds0000maso)
+(see also discussion of the ventilation effect on page 125)
 """
 
 
@@ -10,9 +11,10 @@ class Mason1971:  # pylint: disable=too-few-public-methods
 
     # pylint: disable=too-many-arguments
     @staticmethod
-    def r_dr_dt(const, RH_eq, T, RH, lv, pvs, D, K):
+    def r_dr_dt(const, RH_eq, T, RH, lv, pvs, D, K, ventilation_factor):
         return (
-            (RH - RH_eq)
+            ventilation_factor
+            * (RH - RH_eq)
             / const.rho_w
             / (const.Rv * T / D / pvs + lv / K / T * (lv / T / const.Rv - 1))
         )
