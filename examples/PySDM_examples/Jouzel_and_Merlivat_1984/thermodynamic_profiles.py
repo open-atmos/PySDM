630adc29d122ebaff75658ce2296552d6ab0e60c
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-15 19:56:00 +0200

    clean up and refactoring; remove J&M constants from physics; update bibliography

--- examples/PySDM_examples/Jouzel_and_Merlivat_1984/thermodynamic_profiles.py
+++ examples/PySDM_examples/Jouzel_and_Merlivat_1984/thermodynamic_profiles.py
@@ -11,29 +11,22 @@ from PySDM.physics import si
 
 pressure = make_interp_spline(
     x=np.asarray([-10, -20, -30, -40, -50])[::-1] + T0,
-    y=np.asarray([785, 670, 620, 570, 500])[::-1] * si.mbar,
+    y=np.asarray([925, 780, 690, 630, 600])[::-1] * si.mbar,
 )
 """ Table 1, first two columns: temperature and pressure"""
 pressure.extrapolate = False
 
-A_coefficient = make_interp_spline(
-    x=np.asarray([-10, -20, -30, -40, -50])[::-1] + T0,
-    y=np.asarray([1.5, 1.25, 1.11, 1.05, 1.02])[::-1],
-)
-""" Table 1, first two columns: temperature and the Thermodynamic Coefficient A"""
-
 
 def ice_saturation_curve_4(const, T):
     """eq. (15)"""
-    return (
-        const.JOUZEL_MERLIVAT_CURVE_4_INTERCEPT_COEFF
-        - const.JOUZEL_MERLIVAT_CURVE_4_SLOPE_COEFF * (T - const.T0)
-    )
+    return 0.99 - 0.006 * (T - const.T0)
 
 
-def vapour_mixing_ratio(const, T, svp):
+def vapour_mixing_ratio(formulae, T):
     """mixing ratio with saturation wrt ice calculated with curve 4 equation"""
+    const = formulae.constants
+    svp = formulae.saturation_vapour_pressure
     p_v = ice_saturation_curve_4(const, T) * svp.pvs_ice(T)
     rho_v = p_v / const.Rv / T
-    rho_d = const.rho_STP
+    rho_d = (pressure(T) - p_v) / const.Rd / T
     return rho_v / rho_d
