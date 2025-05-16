f4d0096625cded0f7ef37dc4d8183f8910e8cd2b
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-16 15:11:01 +0200

    remove equation formatting

--- examples/PySDM_examples/Fisher_1991/__init__.py
+++ examples/PySDM_examples/Fisher_1991/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=invalid-name
 """
 based on the [Jouzel and Merlivat 1984](https://doi.org/10.1029/JD089iD07p11749) calculations
-of $\delta^{18}$O and $\delta^{2}$H used in d-excess formula and solved
+of delta_{18O} and delta_D used in d-excess formula and solved
 by integration of the eq. (1) in
 [Fisher 1991](https://doi.org/10.3402/tellusb.v43i5.15414) and eq. (3) in Jouzel and Merlivat 1984
 
--- examples/PySDM_examples/Jouzel_and_Merlivat_1984/__init__.py
+++ examples/PySDM_examples/Jouzel_and_Merlivat_1984/__init__.py
@@ -2,7 +2,7 @@
 """
 kinetic fractionation factors used in modelling isotopic effect
 in snow formation, replacing saturation fractionation factors with
-$\alpha_{saturation} \alpha_{kinetic}$
+alpha_{saturation} alpha_{kinetic}
 (see [Jouzel and Merlivat 1984](https://doi.org/10.1029/JD089iD07p11749))
 
 
