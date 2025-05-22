52faef676c38cca15ac599bc0e4a056b5aeceec1
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-22 18:16:13 +0200

    return nan in empty function

--- PySDM/physics/drop_growth/fick.py
+++ PySDM/physics/drop_growth/fick.py
@@ -5,6 +5,8 @@ The notation for terms associated with heat conduction and diffusion are from
 [Rogers & Yau 1989](https://archive.org/details/shortcourseinclo0000roge_m3k2).
 """
 
+import numpy as np
+
 
 class Fick:
     def __init__(self, _):
@@ -13,6 +15,7 @@ class Fick:
     @staticmethod
     def Fk(const, T, K, lv):  # pylint: disable=unused-argument
         """heat conduction not taken into account"""
+        return np.nan
 
     @staticmethod
     def Fd(const, T, D, pvs):
