cf2e1bb4f358da136aa7c9b45eee3e26e77064fd
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-05-25 13:30:51 +0200

    add fig_1 notebook for M&G

---
+++ examples/PySDM_examples/Morrison_and_Grabowski_2007/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=invalid-name
+"""
+prescribed-flow simulation settings data from
+[Morrison & Grabowski 2007 (JAS)](https://doi.org/10.1175/JAS3980)
+
+fig_1.ipynb:
+.. include:: ./fig_1.ipynb.badges.md
+"""
