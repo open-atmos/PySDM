6272e87795845f32b7cc5c19cfb42d1edb8d2956
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-27 17:37:50 +0200

    add missing heavy_to_light to ratios

--- tests/unit_tests/physics/test_isotope_diffusivity_ratios.py
+++ tests/unit_tests/physics/test_isotope_diffusivity_ratios.py
@@ -28,7 +28,9 @@ class TestIsotopeDiffusivityRatios:
             constants={"Md": constants_defaults.Md * 0.9986},
             isotope_diffusivity_ratios="Stewart1975",
         )
-        sut = getattr(formulae.isotope_diffusivity_ratios, f"ratio_{isotope}")
+        sut = getattr(
+            formulae.isotope_diffusivity_ratios, f"ratio_{isotope}_heavy_to_light"
+        )
 
         # act
         actual_value = sut(temperature=np.nan)
@@ -73,7 +75,9 @@ class TestIsotopeDiffusivityRatios:
     ):
         # arrange
         formulae = Formulae(isotope_diffusivity_ratios="HellmannAndHarvey2020")
-        sut = getattr(formulae.isotope_diffusivity_ratios, f"ratio_{isotope_label}")
+        sut = getattr(
+            formulae.isotope_diffusivity_ratios, f"ratio_{isotope_label}_heavy_to_light"
+        )
 
         # act
         actual_value = sut(temperature=temperature)
@@ -107,7 +111,7 @@ class TestIsotopeDiffusivityRatios:
                     Formulae(
                         isotope_diffusivity_ratios=paper
                     ).isotope_diffusivity_ratios,
-                    f"ratio_{isotope_label}",
+                    f"ratio_{isotope_label}_heavy_to_light",
                 )
             except AttributeError:
                 pytest.skip()
@@ -127,7 +131,8 @@ class TestIsotopeDiffusivityRatios:
             for isotope_label in ("2H", "17O", "18O"):
                 try:
                     sut = getattr(
-                        formulae.isotope_diffusivity_ratios, f"ratio_{isotope_label}"
+                        formulae.isotope_diffusivity_ratios,
+                        f"ratio_{isotope_label}_heavy_to_light",
                     )
                 except AttributeError:
                     pass
