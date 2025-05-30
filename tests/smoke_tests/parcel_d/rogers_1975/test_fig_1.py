4182c7a52bd2b491e5aab792e83b9fe4ef68ae9f
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-30 17:04:35 +0200

    add radius slope test

--- tests/smoke_tests/parcel_d/rogers_1975/test_fig_1.py
+++ tests/smoke_tests/parcel_d/rogers_1975/test_fig_1.py
@@ -39,3 +39,24 @@ class TestFig1:
         np.testing.assert_allclose(
             actual=peak_time, desired=expected_peak_time, atol=0.5
         )
+
+
+@staticmethod
+def test_fig1_radius_scope(variables):
+    """
+    Check if for first 2.5 seconds slope is smaller than after this time.
+    It represents slower droplets growth with small supersaturation.
+    """
+    # arrange
+    radius = variables["solution"].r
+    time_less_than = np.sum(variables["tsteps"].magnitude <= 2.5)
+    dr_before = np.diff(radius[:time_less_than]).magnitude
+    dr_after = np.diff(radius[time_less_than:]).magnitude
+
+    # act
+    sut = np.mean(dr_before) / np.mean(dr_after)
+
+    # assert
+    assert sut < 1
+    assert sut > 0
+    assert (dr_before > 0).all()
