f6d7bec9acf8e0fc953ade3c78746250645c43b6
Agnieszka Żaba <azaba@agh.edu.pl>
2025-05-30 16:24:36 +0200

    add smoke test for peak supersaturation; fix formula D over K

--- tests/smoke_tests/parcel_d/rogers_1975/test_fig_1.py
+++ tests/smoke_tests/parcel_d/rogers_1975/test_fig_1.py
@@ -4,6 +4,8 @@ import numpy as np
 import pytest
 from pathlib import Path
 
+from numpy.ma.core import argmax
+
 from PySDM.physics import si
 from PySDM.physics.constants import PER_CENT
 from PySDM_examples import Rogers_1975
@@ -19,21 +21,21 @@ def variables_fixture():
 
 class TestFig1:
     @staticmethod
-    def test_fig1_saturation_peak(variables):
+    def test_fig1_saturation_peak_against_paper(variables):
         # arrange
         SS = variables["solution"].S - 1
         time = variables["tsteps"]
         expected_peak_time = 7
-        expected_peak_value = 0.97
+        expected_peak_value = 0.97 * PER_CENT
 
         # act
         peak_value = max(SS)
-        peak_time = time.where(SS == peak_value)
+        peak_time = time[argmax(SS)]
 
         # assert
         np.testing.assert_allclose(
-            actual=peak_value, desired=expected_peak_value, rtol=0.01
+            actual=peak_value, desired=expected_peak_value, rtol=1e-3
         )
         np.testing.assert_allclose(
-            actual=peak_time, desired=expected_peak_time, rtol=0.01
+            actual=peak_time, desired=expected_peak_time, atol=0.5
         )
