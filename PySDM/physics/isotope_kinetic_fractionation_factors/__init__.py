f81c961b8648cc290a0e45b1a7bfdfeaff02ad11
Agnieszka Żaba <azaba@agh.edu.pl>
2025-06-04 16:38:41 +0200

    remove Bolot formula as it is defined in Jouzel & Merlivat

--- PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
+++ PySDM/physics/isotope_kinetic_fractionation_factors/__init__.py
@@ -4,5 +4,4 @@ kinetic fractionation factors for water isotopologues
 
 from PySDM.impl.null_physics_class import Null
 from .craig_gordon import CraigGordon
-from .bolot_et_al_2013 import BolotEtAl2013
 from .jouzel_and_merlivat_1984 import JouzelAndMerlivat1984
