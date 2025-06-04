b4855cd071d6e53003c243a78b1a3c8b4e801716
Tim Luettmer <tluettm@uni-mainz.de>
2025-06-04 11:16:54 +0200

    Added capacity fit for columnar ice crystals

--- PySDM/physics/constants_defaults.py
+++ PySDM/physics/constants_defaults.py
@@ -498,6 +498,16 @@ CRAIG_1961_SLOPE_COEFF = 8
 CRAIG_1961_INTERCEPT_COEFF = 10 * PER_MILLE
 """ 〃 """
 
+capacity_columnar_ice_B1 = 0.3
+""" [Spichtinger & Gierens 2009](https://doi.org/10.5194/acp-9-685-2009) """
+capacity_columnar_ice_B2 = 0.43
+""" 〃 """
+capacity_columnar_ice_A1 = 0.015755 * si.m / si.kg**(-capacity_columnar_ice_B1)
+""" 〃 """
+capacity_columnar_ice_A2 = 0.33565 * si.m / si.kg**(-capacity_columnar_ice_B2)
+""" 〃 """
+
+
 asymmetry_g = 0.85  # forward scattering from cloud droplets
 """ [Bohren 1987](https://doi.org/10.1119/1.15109) """
 
