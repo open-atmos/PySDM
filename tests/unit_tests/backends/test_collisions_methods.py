63074d70aea7db11cb76822100c67b8448bd89ef
emmacware <ecware@ucdavis.edu>
2025-06-05 18:24:18 +0200

    multicell figure test case

--- tests/unit_tests/backends/test_collisions_methods.py
+++ tests/unit_tests/backends/test_collisions_methods.py
@@ -8,7 +8,7 @@ from PySDM.backends import CPU, GPU
 from PySDM.backends.impl_common.index import make_Index
 from PySDM.backends.impl_common.indexed_storage import make_IndexedStorage
 from PySDM.backends.impl_common.pair_indicator import make_PairIndicator
-from PySDM.backends.impl_numba.methods.collisions_methods import pair_indices
+from PySDM.backends.impl_numba.methods.collisions_methods import pair_indices, coalesce
 
 NONZERO = 44
 
@@ -148,7 +148,7 @@ class TestCollisionMethods:
         ),
     )
     # pylint: disable=too-many-locals
-    def test_scale_prob_for_adaptive_sdm_gamma(
+    def test_scale_prob_for_adaptive_sdm_gamma_including_multi_cell_cases(
         *,
         backend_instance,
         gamma,
@@ -235,3 +235,90 @@ class TestCollisionMethods:
 
         # Assert
         assert all(cell_start.to_ndarray()[:] == np.array([0, 3]))
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "gamma, permutation, multiplicity, cell_id, dt_left, dt, dt_max, is_first_in_pair, ",
+        (
+            (
+                [2, 2, 2] + [3, 3] + [1, 1, 1],
+                tuple(range(n_part := 16)),
+                [1]*2 + [100]*(n_part-2),
+                [0]*(n_in_cell_0 := 6) + [1]*(n_in_cell_1 := 4) + [2]*(n_in_cell_2 := 6),
+                [dt := 12.]*(n_cell := 3),
+                dt,
+                dt,
+                [True, False]*(n_part // 2),
+            ),
+        ),
+    )
+    # pylint: disable=too-many-locals
+    def test_adaptivity_paper_diagram_scenario(
+        *,
+        gamma,
+        permutation,
+        multiplicity,
+        cell_id,
+        dt_left,
+        dt,
+        dt_max,
+        is_first_in_pair
+    ):
+        # Arrange
+        backend = CPU() 
+        _permutation = make_Index(backend).from_ndarray(np.asarray(permutation))
+        _multiplicity = make_IndexedStorage(backend).from_ndarray(_permutation, np.asarray(multiplicity))
+        _cell_id = backend.Storage.from_ndarray(np.asarray(cell_id))
+        _dt_left = backend.Storage.from_ndarray(np.asarray(dt_left))
+        _is_first_in_pair = make_PairIndicator(backend)(len(multiplicity))
+        _is_first_in_pair.indicator[:] = np.asarray(is_first_in_pair)
+        _n_substep = backend.Storage.from_ndarray(np.zeros_like(dt_left, dtype=int))
+        _dt_min = backend.Storage.from_ndarray(np.asarray(dt_left))
+        dt_range = (np.nan, dt_max)
+
+        # Act
+        while any(_dt_left.data > 0):
+            _gamma = backend.Storage.from_ndarray(np.asarray(gamma))
+            backend.scale_prob_for_adaptive_sdm_gamma(
+                prob=_gamma,
+                multiplicity=_multiplicity,
+                cell_id=_cell_id,
+                dt_left=_dt_left,
+                dt=dt,
+                dt_range=dt_range,
+                is_first_in_pair=_is_first_in_pair,
+                stats_n_substep=_n_substep,
+                stats_dt_min=_dt_min,
+            )
+            assert all(_gamma.data == _gamma.data.astype(int))
+            n_pair = len(gamma)
+            n_cell = len(dt_left)
+            for i in range(n_pair):
+                j, k, skip_pair = pair_indices(
+                    i=i,
+                    idx=_permutation.data,
+                    is_first_in_pair=_is_first_in_pair.indicator.data,
+                    prob_like=_gamma.data,
+                )
+                if not skip_pair:
+                    coalesce(
+                        i=i,
+                        j=j,
+                        k=k,
+                        cid=cell_id[j],
+                        multiplicity=_multiplicity.data,
+                        gamma=_gamma.data,
+                        attributes=np.empty(shape=(0,0,)),
+                        coalescence_rate=np.empty(n_cell),
+                    )
+                    if _multiplicity.data[j] == 0:
+                        _is_first_in_pair.indicator.data[j] = False
+                        gamma[i] = 0.0
+
+        # Assert
+        assert _n_substep[0] == 2
+        assert _n_substep[1] == 3
+        assert _n_substep[2] == 1
+        assert (_dt_left.to_ndarray() == 0.0).all()
+        assert all(_dt_min.data == dt / _n_substep.data)
+        assert all(_multiplicity.data == (0, 1, 25, 25, 25, 25, 12, 13, 12, 13, 50, 50, 50, 50, 50, 50))
