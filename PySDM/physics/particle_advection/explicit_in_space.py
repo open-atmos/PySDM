2e9bacfbd47db27f301e9b23bd9a734ef8b8221d
Dominik Breksa <dominikbreksa@gmail.com>
2025-06-10 16:12:35 +0200

    Update explicit_in_space.py
    Updated the Explicit in space displacement function to be on one line

--- PySDM/physics/particle_advection/explicit_in_space.py
+++ PySDM/physics/particle_advection/explicit_in_space.py
@@ -1,12 +1,12 @@
 """
 basic explicit-in-space Euler scheme
 """
-
+import numpy as np
 
 class ExplicitInSpace:  # pylint: disable=too-few-public-methods
     def __init__(self, _):
         pass
 
     @staticmethod
-    def displacement(_, position_in_cell, c_l, c_r):
-        return c_l * (1 - position_in_cell) + c_r * position_in_cell
+    def displacement(_, position_in_cell, cell_id, c_l, c_r, enable_monte_carlo, u01):
+        return (position_in_cell + (np.floor(np.abs(max(c_l, c_r))) * np.sign(np.abs(max(c_l, c_r)) / max(c_l, c_r))) + (np.abs(max(c_l, c_r)) > u01) * np.sign(np.abs(max(c_l, c_r)) / max(c_l, c_r))) if enable_monte_carlo else (c_l * (1 - position_in_cell) + c_r * position_in_cell)
