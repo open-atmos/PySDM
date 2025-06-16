811681015c5794baf419e0228c86a0c1081ffa18
Tim Luettmer <tluettm@uni-mainz.de>
2025-06-16 12:29:21 +0200

    Changed d_aw_ice range check for constant and null variation of hom. freezing nucleation rate

--- PySDM/physics/homogeneous_ice_nucleation_rate/constant.py
+++ PySDM/physics/homogeneous_ice_nucleation_rate/constant.py
@@ -11,13 +11,11 @@ class Constant:  # pylint: disable=too-few-public-methods
 
     @staticmethod
     def d_a_w_ice_within_range(const, da_w_ice):
-        return da_w_ice >= const.KOOP_MIN_DA_W_ICE
+        return True
 
     @staticmethod
     def d_a_w_ice_maximum(const, da_w_ice):
-        return np.where(
-            da_w_ice > const.KOOP_MAX_DA_W_ICE, const.KOOP_MAX_DA_W_ICE, da_w_ice
-        )
+        return da_w_ice
 
     @staticmethod
     def j_hom(const, T, a_w_ice):  # pylint: disable=unused-argument
