36811d9629559f9e990317196130b63de0fd792d
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2025-06-28 13:29:14 +0200

    terminal velocity: add docstring to PowerSeries module, simplify code (#1660)

--- PySDM/dynamics/terminal_velocity/power_series.py
+++ PySDM/dynamics/terminal_velocity/power_series.py
@@ -1,24 +1,27 @@
 """
-Power series expression
+Power series expression - a simple and mutable power-law (where the coefficients are specified
+by the user), which is a form used in many more complex terminal velocity parameterizations
+such as Rogers & Yau. This formulation emerges from balancing drag and gravitational forces on
+a spherical object, and the power depends on the drag coefficient (and thus the Reynolds number
+and flow regime). Rogers and Yau distinguishes different power law parameters for three regimes,
+whereas this much simpler formulation applies across the full range of particle sizes.
+It is introduced it as a simple option for comparison against bulk methods, think of it as the
+terminal-velocity analogue to the Geometric collision kernel.
 """
 
 import numpy as np
 
-from PySDM.physics import constants as const
+from PySDM.physics import si, constants as const
 
 
 class PowerSeries:  # pylint: disable=too-few-public-methods
     def __init__(self, particulator, *, prefactors=None, powers=None):
-        si = const.si
         self.particulator = particulator
-        prefactors = prefactors or [2.0e-1 * si.m / si.s / np.sqrt(si.m)]
-        self.prefactors = np.array(prefactors)
-        powers = powers or [1 / 6]
-        self.powers = np.array(powers)
-        for i, p in enumerate(self.powers):
-            self.prefactors[i] *= (4 / 3 * const.PI) ** (p)
-            self.prefactors[i] /= (1 * si.um**3) ** (p)
+        self.prefactors = np.array(prefactors or [2.0e-1 * si.m / si.s / np.sqrt(si.m)])
+        self.powers = np.array(powers or [1 / 6])
         assert len(self.prefactors) == len(self.powers)
+        for i, p in enumerate(self.powers):
+            self.prefactors[i] *= const.PI_4_3**p / si.um ** (3 * p)
 
     def __call__(self, output, radius):
         self.particulator.backend.power_series(
