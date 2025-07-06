62d687accad6c2331b37c44afe4cdb7959ea3362
Agnieszka Żaba <azaba@agh.edu.pl>
2025-07-06 04:27:38 +0200

    disable pylint on too long docstrings

--- PySDM/dynamics/collisions/collision_kernels/__init__.py
+++ PySDM/dynamics/collisions/collision_kernels/__init__.py
@@ -3,7 +3,7 @@ Collision kernels including
 [Golovin](https://open-atmos.github.io/PySDM/PySDM/dynamics/collisions/collision_kernels/golovin.html),
 [Geometric](https://open-atmos.github.io/PySDM/PySDM/dynamics/collisions/collision_kernels/geometric.html)
 and other...
-"""
+"""  # pylint: disable=line-too-long
 
 from .constantK import ConstantK
 from .electric import Electric
