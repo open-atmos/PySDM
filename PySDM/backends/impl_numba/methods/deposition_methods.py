conservation test for adaptivity in deposition; fix dthd calculation in adaptive logic; refactor adaptivity code; add temeprature axis do A&A fig 2 notebook; rtol logic for RH; fuse logic

PySDM/backends/impl_numba/methods/deposition_methods.py
@@ -15,6 +15,10 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
         assert self.formulae.particle_shape_and_density.supports_mixed_phase()
 
         formulae = self.formulae_flattened
+        multiplier = 2
+        midpoint = True
+        rel_tol_rh = 1e-2
+        fuse = 16
 
         @numba.njit(**{**self.default_jit_flags, **{"parallel": False}})
         def calc_saturation_ratio_ice_temperature_and_pressure(
@@ -47,15 +51,10 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             latent_heat_sub: float,
             saturation_ratio_ice: float,
             pressure: float,
-            reynolds_number: float,
-            schmidt_number: float,
         ):
             radius = formulae.particle_shape_and_density__mass_to_radius(
                 signed_mass_old
             )
-
-            diameter = radius * 2.0
-
             pvs_ice = formulae.saturation_vapour_pressure__pvs_ice(temperature)
 
             capacity = formulae.diffusion_ice_capacity__capacity(abs(signed_mass_old))
@@ -98,7 +97,74 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             mass_deposition_rate = (
                 4 * np.pi * capacity * howell_factor_x_diffcoef_x_rhovsice_x_icess
             )
+            if mass_deposition_rate > 1:
+                print(
+                    radius,
+                    Fk,
+                    Fd,
+                    mass_deposition_rate,
+                    howell_factor_x_diffcoef_x_rhovsice_x_icess,
+                    pvs_ice,
+                )
+                assert False
             return mass_deposition_rate
+
+        @numba.njit(**{**self.default_jit_flags, **{"parallel": False}})
+        def _loop(
+            fake,
+            temperature,
+            rhod,
+            thd,
+            signed_water_mass,
+            saturation_ratio_ice,
+            total_pressure,
+            multiplicity,
+            sub_time_step,
+            mass_of_dry_air,
+        ):
+            latent_heat_sub = formulae.latent_heat_sublimation__ls(temperature)
+            delta_rv = 0
+            for i in range(len(multiplicity)):
+                if not formulae.trivia__unfrozen(signed_water_mass[i]):
+                    mass_deposition_rate = mass_deposition_rate_per_droplet(
+                        temperature=temperature,
+                        rho_d=rhod,
+                        signed_mass_old=signed_water_mass[i],
+                        latent_heat_sub=latent_heat_sub,
+                        saturation_ratio_ice=saturation_ratio_ice,
+                        pressure=total_pressure,
+                    )
+                    delta_rv += (
+                        -mass_deposition_rate
+                        * multiplicity[i]
+                        * sub_time_step
+                        / mass_of_dry_air
+                    )
+                    if not fake:
+                        x_old = formulae.diffusion_coordinate__x(-signed_water_mass[i])
+                        dx_dt_old = formulae.diffusion_coordinate__dx_dt(
+                            -signed_water_mass[i], mass_deposition_rate
+                        )
+                        x_new = formulae.trivia__explicit_euler(
+                            x_old, sub_time_step, dx_dt_old
+                        )
+                        signed_water_mass[i] = -formulae.diffusion_coordinate__mass(
+                            x_new
+                        )
+                        if x_new > 1:
+                            print(x_old, dx_dt_old, x_new, signed_water_mass[i])
+            delta_thd = (
+                formulae.state_variable_triplet__dthd_dt(
+                    rhod=rhod,
+                    thd=thd,
+                    T=temperature,
+                    d_water_vapour_mixing_ratio__dt=delta_rv / sub_time_step,
+                    lv=latent_heat_sub,
+                )
+                * sub_time_step
+            )
+
+            return delta_rv, delta_thd
 
         @numba.njit(**{**self.default_jit_flags, **{"parallel": False}})
         def body(  # pylint: disable=too-many-arguments
@@ -106,15 +172,12 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             adaptive,
             multiplicity,
             signed_water_mass,
-            current_total_pressure,
             current_vapour_mixing_ratio,
             current_dry_air_density,
             current_dry_potential_temperature,
             cell_volume,
             time_step,
             cell_id,
-            reynolds_number,
-            schmidt_number,
             # to be modified
             predicted_vapour_mixing_ratio,
             predicted_dry_potential_temperature,
@@ -129,14 +192,8 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             note: condensation uses theta for tolerance, we could use RH here (and later also in cond)
             """
             # pylint: disable=too-many-locals
-            multiplier = 10
-            midpoint = True
-            rel_tol_rh = 1e-6
-
-            cid = 0  # TODO
-
-            n_sd = len(signed_water_mass)
             n_substeps = 1
+            cid = cell_id[0]  # TODO: add support for multi-cell environments
 
             # 1/dt * (post-ambient-thermodynamics & post-condensation rv/thd - previous end-of-timestep rv/thd)
             rv_tendency = (
@@ -149,11 +206,18 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             rhod_tendency = (
                 predicted_dry_air_density[cid] - current_dry_air_density[cid]
             ) / time_step
+            dry_air_mass_mean = (
+                cell_volume
+                * (predicted_dry_air_density[cid] + current_dry_air_density[cid])
+                / 2
+            )
 
             if adaptive:
                 n_substeps = 1 / multiplier
                 delta_rh_long = np.nan
-                while True:
+                for burnout in range(fuse + 1):
+                    if burnout == fuse:
+                        assert False
                     sub_time_step = time_step / n_substeps
 
                     rhod = (
@@ -176,58 +240,34 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
                             dry_air_density=rhod,
                         )
                     )
-                    for i in range(n_sd):
-                        if not formulae.trivia__unfrozen(signed_water_mass[i]):
-
-                            latent_heat_sub = formulae.latent_heat_sublimation__ls(
-                                temperature
-                            )
-
-                            mass_deposition_rate = mass_deposition_rate_per_droplet(
-                                temperature=temperature,
-                                rho_d=rhod,
-                                signed_mass_old=signed_water_mass[i],
-                                latent_heat_sub=latent_heat_sub,
-                                saturation_ratio_ice=saturation_ratio_ice,
-                                pressure=total_pressure,
-                                reynolds_number=reynolds_number[i],
-                                schmidt_number=schmidt_number[cid],
-                            )
-
-                            rv += (
-                                -mass_deposition_rate
-                                * multiplicity[i]
-                                * sub_time_step
-                                / (cell_volume * rhod)
-                            )
-                    delta_thd = formulae.state_variable_triplet__dthd_dt(
+                    delta_rv, delta_thd = _loop(
+                        fake=True,
+                        temperature=temperature,
                         rhod=rhod,
-                        thd=current_dry_potential_temperature[cid],
-                        T=temperature,
-                        d_water_vapour_mixing_ratio__dt=(
-                            rv - current_vapour_mixing_ratio[cid]
-                        )
-                        / sub_time_step,
-                        lv=latent_heat_sub,
+                        thd=thd,
+                        signed_water_mass=signed_water_mass,
+                        saturation_ratio_ice=saturation_ratio_ice,
+                        total_pressure=total_pressure,
+                        multiplicity=multiplicity,
+                        sub_time_step=sub_time_step,
+                        mass_of_dry_air=dry_air_mass_mean,
                     )
                     delta_rh_short = (
                         calc_saturation_ratio_ice_temperature_and_pressure(
-                            vapour_mixing_ratio=rv,
-                            dry_air_potential_temperature=current_dry_potential_temperature[
-                                cid
-                            ]
-                            + thd_tendency * sub_time_step
-                            + delta_thd,
+                            vapour_mixing_ratio=rv + delta_rv,
+                            dry_air_potential_temperature=thd + delta_thd,
                             dry_air_density=rhod,
                         )[0]
                         - saturation_ratio_ice
                     )
                     if (
                         n_substeps < 1
-                        or rv < 0
-                        # or abs(delta_rh_long - multiplier * delta_rh_short)
-                        # / saturation_ratio_ice
-                        # > rel_tol_rh
+                        or rv < -delta_rv
+                        or not formulae.trivia__within_tolerance(
+                            abs(delta_rh_long - multiplier * delta_rh_short),
+                            saturation_ratio_ice,
+                            rel_tol_rh,
+                        )
                     ):
                         delta_rh_long = delta_rh_short
                         n_substeps *= multiplier
@@ -241,9 +281,7 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             thd = current_dry_potential_temperature[cid]
             rhod = current_dry_air_density[cid]
 
-            print("n", n_substeps)
             for _ in range(int(n_substeps)):
-                # midpoint -> computer the sink with midpoint source
                 rv += sub_time_step * rv_tendency * (0.5 if midpoint else 1)
                 thd += sub_time_step * thd_tendency * (0.5 if midpoint else 1)
                 rhod += sub_time_step * rhod_tendency * (0.5 if midpoint else 1)
@@ -255,58 +293,22 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
                         dry_air_density=rhod,
                     )
                 )
-                for i in range(n_sd):
-                    if not formulae.trivia__unfrozen(signed_water_mass[i]):
-                        cid = cell_id[i]
+                delta_rv, delta_thd = _loop(
+                    fake=False,
+                    temperature=temperature,
+                    rhod=rhod,
+                    thd=thd,
+                    signed_water_mass=signed_water_mass,
+                    saturation_ratio_ice=saturation_ratio_ice,
+                    total_pressure=total_pressure,
+                    multiplicity=multiplicity,
+                    sub_time_step=sub_time_step,
+                    mass_of_dry_air=dry_air_mass_mean,
+                )
+                thd += delta_thd
+                rv += delta_rv
+                assert rv >= 0
 
-                        latent_heat_sub = formulae.latent_heat_sublimation__ls(
-                            temperature
-                        )
-
-                        mass_deposition_rate = mass_deposition_rate_per_droplet(
-                            temperature=temperature,
-                            rho_d=rhod,
-                            signed_mass_old=signed_water_mass[i],
-                            latent_heat_sub=latent_heat_sub,
-                            saturation_ratio_ice=saturation_ratio_ice,
-                            pressure=total_pressure,
-                            reynolds_number=reynolds_number[i],
-                            schmidt_number=schmidt_number[cid],
-                        )
-
-                        delta_rv_i = (
-                            -mass_deposition_rate
-                            * multiplicity[i]
-                            * sub_time_step
-                            / (
-                                cell_volume * rhod
-                            )  # TODO: mass of dry air should not change within timestep!
-                        )
-                        rv += delta_rv_i
-                        assert rv >= 0
-
-                        thd += (
-                            formulae.state_variable_triplet__dthd_dt(
-                                rhod=rhod,
-                                thd=thd,
-                                T=temperature,
-                                d_water_vapour_mixing_ratio__dt=delta_rv_i
-                                / sub_time_step,
-                                lv=latent_heat_sub,
-                            )
-                            * sub_time_step
-                        )
-
-                        x_old = formulae.diffusion_coordinate__x(-signed_water_mass[i])
-                        dx_dt_old = formulae.diffusion_coordinate__dx_dt(
-                            -signed_water_mass[i], mass_deposition_rate
-                        )
-                        x_new = formulae.trivia__explicit_euler(
-                            x_old, sub_time_step, dx_dt_old
-                        )
-                        signed_water_mass[i] = -formulae.diffusion_coordinate__mass(
-                            x_new
-                        )
                 if midpoint:
                     thd += sub_time_step * thd_tendency / 2
                     rv += sub_time_step * rv_tendency / 2
@@ -322,15 +324,12 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
         adaptive,
         multiplicity,
         signed_water_mass,
-        current_total_pressure,  # TODO: do we need it ?
         current_vapour_mixing_ratio,
         current_dry_air_density,
         current_dry_potential_temperature,
         cell_volume,
         time_step,
         cell_id,
-        reynolds_number,
-        schmidt_number,
         predicted_vapour_mixing_ratio,
         predicted_dry_potential_temperature,
         predicted_dry_air_density,
@@ -339,15 +338,12 @@ class DepositionMethods(BackendMethods):  # pylint:disable=too-few-public-method
             adaptive=adaptive,
             multiplicity=multiplicity.data,
             signed_water_mass=signed_water_mass.data,
-            current_total_pressure=current_total_pressure.data,
             current_vapour_mixing_ratio=current_vapour_mixing_ratio.data,
             current_dry_air_density=current_dry_air_density.data,
             current_dry_potential_temperature=current_dry_potential_temperature.data,
             cell_volume=cell_volume,
             time_step=time_step,
             cell_id=cell_id.data,
-            reynolds_number=reynolds_number.data,
-            schmidt_number=schmidt_number.data,
             predicted_vapour_mixing_ratio=predicted_vapour_mixing_ratio.data,
             predicted_dry_potential_temperature=predicted_dry_potential_temperature.data,
             predicted_dry_air_density=predicted_dry_air_density.data,

tests/unit_tests/dynamics/test_vapour_deposition_on_ice.py
@@ -1,5 +1,9 @@
 """basic water vapor deposition on ice test"""
 
+import os
+
+os.environ["NUMBA_DISABLE_JIT"] = "1"
+
 from typing import Iterable
 from functools import lru_cache
 import numpy as np
@@ -77,6 +81,7 @@ def make_particulator(
     RH_water: float = None,
     adaptive: bool = False,
     diffusion_ice_kinetics="Standard",
+    multiplicity: int = int(1e8),
 ):
     """instantiates a particulator with minimal components for testing ice depositional growth"""
     assert RH_water is None or RH_ice is None
@@ -89,7 +94,9 @@ def make_particulator(
     builder.add_dynamic(VapourDepositionOnIce(adaptive=adaptive))
     particulator = builder.build(
         attributes={
-            "multiplicity": np.full(shape=(builder.particulator.n_sd,), fill_value=1e8),
+            "multiplicity": np.full(
+                shape=(builder.particulator.n_sd,), fill_value=multiplicity
+            ),
             "signed water mass": np.asarray(signed_water_masses),
         },
         products=(IceWaterContent(),),
@@ -168,8 +175,10 @@ class TestVapourDepositionOnIce:
             np.testing.assert_approx_equal(
                 particulator.products["ice water content"].get()[0], iwc_old
             )
-            assert particulator.environment["water_vapour_mixing_ratio"][0] == rv0
-            assert particulator.environment["thd"][0] == thd0
+            np.testing.assert_approx_equal(
+                particulator.environment["water_vapour_mixing_ratio"][0], rv0
+            )
+            np.testing.assert_approx_equal(particulator.environment["thd"][0], thd0)
 
     @staticmethod
     @pytest.mark.parametrize(
@@ -266,3 +275,42 @@ class TestVapourDepositionOnIce:
         relative_growth = (water_mass_new - water_mass_init) / water_mass_init
         assert all(relative_growth > 0.0)
         assert all(np.diff(relative_growth) < 0.0)
+
+    @staticmethod
+    @pytest.mark.parametrize("rh_ice", (1.5, 1.0, 0.5))
+    def test_mass_conservation_under_adaptivity(rh_ice):
+        # arrange
+        water_mass_init = np.logspace(-15, -6, num=11) * si.kg
+        particulator = make_particulator(
+            adaptive=True,
+            dt=10 * si.s,
+            diffusion_coordinate="WaterMassLogarithm",
+            diffusion_ice_capacity="Spherical",
+            signed_water_masses=-water_mass_init,
+            temperature=250 * si.K,
+            pressure=800 * si.hPa,
+            RH_ice=rh_ice,
+            multiplicity=int(1e8),
+        )
+
+        def total_water_mass_in_the_system(attr, env):
+            return np.dot(
+                attr["water mass"].to_ndarray(),
+                attr["multiplicity"].to_ndarray(),
+            ) + (env["water_vapour_mixing_ratio"][0] * env["rhod"][0] * env.mesh.dv)
+
+        # act
+        m0 = total_water_mass_in_the_system(
+            particulator.attributes, particulator.environment
+        )
+        particulator.run(steps=1)
+        m1 = total_water_mass_in_the_system(
+            particulator.attributes, particulator.environment
+        )
+
+        # assert
+        np.testing.assert_almost_equal(m0, m1)
+
+
+# TODO: test is updraft matters
+# TODO: test if order of condensation/deposition matters