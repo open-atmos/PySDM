fix: missing (1+qv) factor in calculation of derivative of rho_d when accounting for water vapour
rho_d(z) = rho(z) / (1 + qv(z))
therefore drho_d/dz = drho/dz / (1+ qv) - dqv/dz / (1 + qv)^2
_not_ drho_d/dz = drho/dz / (1+ qv) - dqv/dz / (1 + qv)

examples/PySDM_examples/Shipway_and_Hill_2012/settings.py
@@ -127,7 +127,7 @@ class Settings:
                 1 + water_vapour_mixing_ratio
             ) - rhod * d_water_vapour_mixing_ratio__dz / (
                 1 + water_vapour_mixing_ratio
-            )
+            )**2
 
         z_span = (-self.particle_reservoir_depth, self.z_max)
         z_points = np.linspace(*z_span, 2 * self.nz + 1)