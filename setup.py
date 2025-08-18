c4b637a2b37055551a7cf2ab5e819912d363a754
Agnieszka Żaba <azaba@agh.edu.pl>
2025-08-18 10:44:52 +0200

    update dependencies requirements

--- setup.py
+++ setup.py
@@ -1,57 +1,19 @@
 # pylint:disable=missing-module-docstring
 import os
-import sys
 
 from setuptools import setup
 
 CI = "CI" in os.environ
 
 dependencies = [
-    "ThrustRTC==0.3.20",
-    "CURandRTC" + ("==0.1.6" if CI else ">=0.1.2"),
-    "numba"
-    + (
-        {
-            8: "==0.58.1",
-            9: "==0.60.0",
-            10: "==0.60.0",
-            11: "==0.60.0",
-            12: "==0.60.0",
-            13: "==0.60.0",
-        }[sys.version_info.minor]
-        if CI
-        else ">=0.51.2"
-    ),
-    # TODO #1344: (numpy 2.0.0 incompatibility in https://github.com/bjodah/chempy/issues/234)
-    "numpy"
-    + (
-        {
-            8: "==1.24.4",
-            9: "==1.24.4",
-            10: "==1.24.4",
-            11: "==1.24.4",
-            12: "==1.26.4",
-            13: "==1.26.4",
-        }[sys.version_info.minor]
-        if CI
-        else ""
-    ),
-    "Pint" + ("==0.21.1" if CI else ""),
-    "chempy" + ("==0.8.3" if CI else ""),
-    "scipy"
-    + (
-        {
-            8: "==1.10.1",
-            9: "==1.10.1",
-            10: "==1.10.1",
-            11: "==1.10.1",
-            12: "==1.13.0",
-            13: "==1.13.0",
-        }[sys.version_info.minor]
-        if CI
-        else ""
-    ),
-    "pyevtk" + ("==1.6.0" if CI else ""),
+    "ThrustRTC>=0.3.20",
+    "CURandRTC>=0.1.2",
+    "numba>=0.51.2",
+    "numpy",
+    "Pint",
+    "chempy",
+    "scipy",
+    "pyevtk",
 ]
 
 optional_dependencies = {
@@ -77,6 +39,11 @@ optional_dependencies = {
         "open-atmos-jupyter-utils>=v1.2.0",
         "jupyter-core<5.0.0",
         "ipywidgets!=8.0.3",
+        "numba==0.60.0",
+        "CURandRTC==0.1.7",
+        "Pint==0.21.1",
+        "chempy==0.8.3",
+        "pyevtk==1.6.0",
     ],
 }
 
