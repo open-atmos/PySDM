7a563207d086cdf891cf82a31dc98bfd907c0b0b
Tim Luettmer <tluettm@uni-mainz.de>
2025-09-11 13:37:59 +0200

    precommit check for newly added files

--- examples/PySDM_examples/Luettmer_homogeneous_freezing/commons.py
+++ examples/PySDM_examples/Luettmer_homogeneous_freezing/commons.py
@@ -11,56 +11,67 @@ formulae = Formulae(
     particle_shape_and_density="MixedPhaseSpheres",
 )
 
+
 def run_simulations(setting):
 
-        simulation = { "settings": setting,
-                       "ensemble_member_outputs": [],
-                       }
-        for n in range(setting["number_of_ensemble_runs"]):
-            model_setup = Settings(**simulation["settings"])
-            model_setup.formulae.seed += 1
-            model = Simulation(model_setup)
-            simulation["ensemble_member_outputs"].append( model.run() )
+    simulation = {
+        "settings": setting,
+        "ensemble_member_outputs": [],
+    }
+    for n in range(setting["number_of_ensemble_runs"]):
+        model_setup = Settings(**simulation["settings"])
+        model_setup.formulae.seed += 1
+        model = Simulation(model_setup)
+        simulation["ensemble_member_outputs"].append(model.run())
 
-        return simulation
-
+    return simulation
 
 
 def hom_pure_droplet_freezing_backend():
     backends = {
-       "threshold": CPU(formulae = Formulae(
-            particle_shape_and_density="MixedPhaseSpheres",
-            homogeneous_ice_nucleation_rate="Null",
-            saturation_vapour_pressure="MurphyKoop2005",
-            seed=time.time_ns(),
-        )),
-        "KoopMurray2016": CPU(formulae = Formulae(
-            particle_shape_and_density="MixedPhaseSpheres",
-            homogeneous_ice_nucleation_rate="KoopMurray2016",
-            saturation_vapour_pressure="MurphyKoop2005",
-            seed=time.time_ns(),
-        )),
-       "Spichtinger2023": CPU(formulae = Formulae(
-            particle_shape_and_density="MixedPhaseSpheres",
-            homogeneous_ice_nucleation_rate="Koop_Correction",
-            saturation_vapour_pressure="MurphyKoop2005",
-            seed=time.time_ns(),
-        )),
-        "Koop2000": CPU(formulae = Formulae(
-            particle_shape_and_density="MixedPhaseSpheres",
-            homogeneous_ice_nucleation_rate="Koop2000",
-            saturation_vapour_pressure="MurphyKoop2005",
-            seed=time.time_ns(),
-        )),
+        "threshold": CPU(
+            formulae=Formulae(
+                particle_shape_and_density="MixedPhaseSpheres",
+                homogeneous_ice_nucleation_rate="Null",
+                saturation_vapour_pressure="MurphyKoop2005",
+                seed=time.time_ns(),
+            )
+        ),
+        "KoopMurray2016": CPU(
+            formulae=Formulae(
+                particle_shape_and_density="MixedPhaseSpheres",
+                homogeneous_ice_nucleation_rate="KoopMurray2016",
+                saturation_vapour_pressure="MurphyKoop2005",
+                seed=time.time_ns(),
+            )
+        ),
+        "Spichtinger2023": CPU(
+            formulae=Formulae(
+                particle_shape_and_density="MixedPhaseSpheres",
+                homogeneous_ice_nucleation_rate="Koop_Correction",
+                saturation_vapour_pressure="MurphyKoop2005",
+                seed=time.time_ns(),
+            )
+        ),
+        "Koop2000": CPU(
+            formulae=Formulae(
+                particle_shape_and_density="MixedPhaseSpheres",
+                homogeneous_ice_nucleation_rate="Koop2000",
+                saturation_vapour_pressure="MurphyKoop2005",
+                seed=time.time_ns(),
+            )
+        ),
     }
     return backends
 
+
 def hom_pure_droplet_freezing_standard_setup():
-    standard = { "n_sd": int(1e3),
-        "w_updraft": 1. * si.meter / si.second,
+    standard = {
+        "n_sd": int(1e3),
+        "w_updraft": 1.0 * si.meter / si.second,
         "T0": formulae.trivia.C2K(-25),
         "dz": 0.1 * si.meter,
-        "N_dv_droplet_distribution":  5000 / si.cm**3,
+        "N_dv_droplet_distribution": 5000 / si.cm**3,
         "r_mean_droplet_distribution": 15 * si.nanometer,
         "type_droplet_distribution": "monodisperse",
         "RH_0": 0.995,
@@ -69,5 +80,5 @@ def hom_pure_droplet_freezing_standard_setup():
         "deposition_enable": True,
         "deposition_adaptive": True,
         "number_of_ensemble_runs": 1,
-                }
+    }
     return standard
