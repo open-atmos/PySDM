b2ef45f4557af6f8701eb97007ed5c85e1561ea4
olastrz <strzabalaaleksandra15@gmail.com>
2025-11-27 11:41:11 +0100

    New updates consisting of name changes and deleting left comments (https://github.com/open-atmos/PySDM/pull/1725#issuecomment-3565033048)

--- PySDM/exporters/parcel_vtk_exporter.py
+++ PySDM/exporters/parcel_vtk_exporter.py
@@ -24,26 +24,26 @@ class VTKExporterPyrcel(VTKExporter):
     def __init__(self, n_sd, output, mass_of_dry_air):
         super().__init__()
         self.output = output
-        self.x = np.random.random(n_sd)  # pylint: disable=invalid-name
-        self.y = np.random.random(n_sd)  # pylint: disable=invalid-name
-        self.z = np.random.random(n_sd)  # pylint: disable=invalid-name
+        self.x_coords = np.random.random(n_sd)
+        self.y_coords = np.random.random(n_sd)
+        self.z_coords = np.random.random(n_sd)
         self.half_diagonal = []
         self.n_levels = len(self.output["products"]["z"])
 
-        _volume = mass_of_dry_air / output["products"]["rhod"][0]
-        _dz = output["products"]["z"][1] - output["products"]["z"][0]
+        volume = mass_of_dry_air / output["products"]["rhod"][0]
+        delta_z = output["products"]["z"][1] - output["products"]["z"][0]
         for level in range(self.n_levels):
             if level > 0:
                 prev_to_curr_density_ratio = (
                     output["products"]["rhod"][level - 1]
                     / output["products"]["rhod"][level]
                 )
-                _volume *= prev_to_curr_density_ratio
-                _dz = (
+                volume *= prev_to_curr_density_ratio
+                delta_z = (
                     output["products"]["z"][level] - output["products"]["z"][level - 1]
                 )
-            _area = _volume / _dz
-            self.half_diagonal.append((2 * _area) ** 0.5)
+            area = volume / delta_z
+            self.half_diagonal.append((2 * area) ** 0.5)
 
     def write_pvd(self):
         pvd_attributes = VtkGroup(self.attributes_file_path)
@@ -63,44 +63,62 @@ class VTKExporterPyrcel(VTKExporter):
         self.exported_times["products"][path] = step * simulation.particulator.dt
 
         n_vertices = 4 * self.n_levels
-        x = np.zeros(n_vertices)  # pylint: disable=invalid-name
-        y = np.zeros(n_vertices)  # pylint: disable=invalid-name
-        z = np.zeros(n_vertices)  # pylint: disable=invalid-name
-        conn = [0, 1, 2, 3]
-        ctype = np.zeros(self.n_levels - 1)
-        ctype[:] = VtkHexahedron.tid
+        x_vertices = np.zeros(n_vertices)
+        y_vertices = np.zeros(n_vertices)
+        z_vertices = np.zeros(n_vertices)
+        connectivity = [0, 1, 2, 3]
+        cell_type = np.zeros(self.n_levels - 1)
+        cell_type[:] = VtkHexahedron.tid
 
         for level in range(self.n_levels):
-            hd = self.half_diagonal[level]
-            _z = self.output["products"]["z"][level]
-            i = level * 4
-            x[i], y[i], z[i] = -hd, -hd, _z
-            i += 1
-            x[i], y[i], z[i] = -hd, hd, _z
-            i += 1
-            x[i], y[i], z[i] = hd, hd, _z
-            i += 1
-            x[i], y[i], z[i] = hd, -hd, _z
-            conn += [*range(4 * (level + 1), 4 * (level + 2))] * 2
-        conn = np.asarray(conn[:-4])
+            half_diag = self.half_diagonal[level]
+            current_z = self.output["products"]["z"][level]
+            idx = level * 4
+            x_vertices[idx], y_vertices[idx], z_vertices[idx] = (
+                -half_diag,
+                -half_diag,
+                current_z,
+            )
+            idx += 1
+            x_vertices[idx], y_vertices[idx], z_vertices[idx] = (
+                -half_diag,
+                half_diag,
+                current_z,
+            )
+            idx += 1
+            x_vertices[idx], y_vertices[idx], z_vertices[idx] = (
+                half_diag,
+                half_diag,
+                current_z,
+            )
+            idx += 1
+            x_vertices[idx], y_vertices[idx], z_vertices[idx] = (
+                half_diag,
+                -half_diag,
+                current_z,
+            )
+            connectivity += [*range(4 * (level + 1), 4 * (level + 2))] * 2
+        connectivity = np.asarray(connectivity[:-4])
         offset = np.asarray(range(8, 8 * self.n_levels, 8))
 
-        _ = {"test_pd": np.array([44] * n_vertices)}  # pointData
+        _ = {"test_pd": np.array([44] * n_vertices)}
 
-        _RH = self.output["products"]["S_max_percent"]  # pylint: disable=invalid-name
-        cell_data = {"RH": np.full(shape=(len(_RH) - 1,), fill_value=np.nan)}
-        cell_data["RH"][:step] = (np.array(_RH[:-1] + np.diff(_RH) / 2))[:step]
+        relative_humidity = self.output["products"]["S_max_percent"]
+        cell_data = {
+            "RH": np.full(shape=(len(relative_humidity) - 1,), fill_value=np.nan)
+        }
+        cell_data["RH"][:step] = (
+            np.array(relative_humidity[:-1] + np.diff(relative_humidity) / 2)
+        )[:step]
         unstructuredGridToVTK(
             path,
-            x,
-            y,
-            z,
-            connectivity=conn,
+            x_vertices,
+            y_vertices,
+            z_vertices,
+            connectivity=connectivity,
             offsets=offset,
-            cell_types=ctype,
+            cell_types=cell_type,
             cellData=cell_data,
-            # pointData=point_data,
-            # fieldData=field_data,
         )
 
     def export_attributes(self, step, simulation):  # pylint: disable=arguments-differ
@@ -109,23 +127,22 @@ class VTKExporterPyrcel(VTKExporter):
 
         payload = {}
 
-        for k in self.output["attributes"].keys():
-            payload[k] = np.asarray(self.output["attributes"][k])[:, step].copy()
-        # payload["size"] = np.full(simulation.particulator.n_sd, 100.0)
+        for key in self.output["attributes"].keys():
+            payload[key] = np.asarray(self.output["attributes"][key])[:, step].copy()
         if step != 0:
-            dz = (
+            delta_z = (
                 self.output["products"]["z"][step]
                 - self.output["products"]["z"][step - 1]
-            )  # pylint: disable=invalid-name
+            )
             if step == 1:
-                self.z *= dz
+                self.z_coords *= delta_z
             else:
-                self.z += dz
+                self.z_coords += delta_z
 
         pointsToVTK(
             path,
-            2 * (self.x - 0.5) * self.half_diagonal[step],
-            2 * (self.y - 0.5) * self.half_diagonal[step],
-            self.z,
+            2 * (self.x_coords - 0.5) * self.half_diagonal[step],
+            2 * (self.y_coords - 0.5) * self.half_diagonal[step],
+            self.z_coords,
             data=payload,
         )
