f30615bfaf5de0de54b07666f5969d8afa753393
Agnieszka Żaba <azaba@agh.edu.pl>
2025-12-13 11:31:28 +0100

    new droplet attribute: diffusional_growth_mass_change

---
+++ PySDM/attributes/physics/diffusional_growth_mass_change.py
@@ -0,0 +1,27 @@
+from PySDM.attributes.impl import register_attribute, DerivedAttribute
+
+
+@register_attribute()
+class DiffusionalGrowthMassChange(DerivedAttribute):
+    def __init__(self, builder):
+        self.water_mass = builder.get_attribute("signed water mass")
+        super().__init__(
+            builder,
+            name="diffusional growth mass change",
+            dependencies=(self.water_mass,),
+        )
+        assert "Collision" not in builder.particulator.dynamics
+        for triggers in (
+            builder.particulator.observers,
+            builder.particulator.initialisers,
+        ):
+            triggers.append(self)
+
+    def setup(self):
+        self.notify()
+
+    def notify(self):
+        self.data[:] = -self.water_mass.data.data
+
+    def recalculate(self):
+        self.data[:] += self.water_mass.data.data
