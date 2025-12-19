ad1289fe5d8caf5ab9a7db2464a1e407ceacab9c
Agnieszka Żaba <azaba@agh.edu.pl>
2025-12-19 20:30:18 +0100

    workaround?

--- tests/smoke_tests/box/shima_et_al_2009/test_lwc_constant.py
+++ tests/smoke_tests/box/shima_et_al_2009/test_lwc_constant.py
@@ -72,7 +72,10 @@ def test_lwc_constant(backend_class, croupier, adaptive):
 
     # Act
     for step in steps:
+        if particulator.n_steps == -1:
+            particulator.n_steps = 0
         particulator.run(step - particulator.n_steps)
+        print(particulator.n_steps)
         check(
             n_part=n_part,
             dv=dv,
@@ -81,7 +84,9 @@ def test_lwc_constant(backend_class, croupier, adaptive):
             attributes=particulator.attributes,
             step=step,
         )
+        print(particulator.n_steps)
         volumes[particulator.n_steps] = particulator.attributes["volume"].to_ndarray()
+        print(particulator.n_steps)
 
     # Assert
     x_max = 0
