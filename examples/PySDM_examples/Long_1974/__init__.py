3e6865c0c5863ce6f05060790039fde30fb52ec4
dejong5 <dejong5@llnl.gov>
2026-01-16 11:30:28 -0800

    add example for Long kernel

---
+++ examples/PySDM_examples/Long_1974/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=invalid-name
+"""
+piecewise collision-coalescence example from
+[Long 1974 (JAS)](https://doi.org/10.1175/1520-0469(1974)031<1040:STTDCE>2.0.CO;2)
+"""
+from .settings import Settings
+from .simulation import Simulation
