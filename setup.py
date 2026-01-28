2ae8b3fa3059988fc9995618bc7ea5b80031234c
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2026-01-29 00:25:39 +0100

    bump scipy dependency version in setup.py to allow using scipy.optimize.elementwise

--- setup.py
+++ setup.py
@@ -26,19 +26,7 @@ dependencies = [
     ),
     "Pint",
     "chempy",
-    "scipy"
-    + (
-        {
-            8: "==1.10.1",
-            9: "==1.10.1",
-            10: "==1.10.1",
-            11: "==1.10.1",
-            12: "==1.13.0",
-            13: "==1.13.0",
-        }[sys.version_info.minor]
-        if CI
-        else ""
-    ),
+    "scipy" + ('=' if CI else '>') + "=1.15.0",  # needed for scipy.optimize.elementwise
     "pyevtk",
     "pyparsing" + ("==3.2.5" if CI else ""),
 ]
