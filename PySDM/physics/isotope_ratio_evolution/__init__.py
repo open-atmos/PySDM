dfb1c9bf19e646505b4447e3eabf2a396676df1b
Agnieszka Żaba <azaba@agh.edu.pl>
2026-04-11 18:02:09 +0200

    add saturation for zero dR condition using Fk and Fd, without b

--- PySDM/physics/isotope_ratio_evolution/__init__.py
+++ PySDM/physics/isotope_ratio_evolution/__init__.py
@@ -4,3 +4,4 @@ from PySDM.impl.null_physics_class import Null
 from .merlivat_and_jouzel_1979 import MerlivatAndJouzel1979
 from .rayleigh_distillation import RayleighDistillation
 from .gedzelman_and_arnold_1994 import GedzelmanAndArnold1994
+from .zaba_et_al import ZabaEtAl
