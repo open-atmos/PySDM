1198d67235648d8221594ad866e7e8c3293e0d03
Kacper Mazur <tasnko12@gmail.com>
2026-04-13 10:34:12 +0200

    add init.py

---
+++ examples/PySDM_examples/Arabas_and_Pawlowska_2011/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=invalid-name
+"""
+condensation example using parcel environment based on
+[Arabas Pawlowska 2011 (GMD)](https://doi.org/10.5194/gmd-4-15-2011)
+
+figs2.ipynb:
+.. include:: ./figs2.ipynb.badges.md
+"""
