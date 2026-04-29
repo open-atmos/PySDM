f1ce34bfd745c81d28c9803bdf81c4b5d334ab31
Agnieszka Żaba <azaba@agh.edu.pl>
2026-04-29 14:21:26 +0200

    add smoke tests

--- tests/smoke_tests/parcel_d/graf_et_al_2019/test_fig_4.py
+++ tests/smoke_tests/parcel_d/graf_et_al_2019/test_fig_4.py
@@ -3,11 +3,13 @@
 from pathlib import Path
 
 import pytest
+import numpy as np
 
 from open_atmos_jupyter_utils import notebook_vars
 from PySDM_examples import Graf_et_al_2019
 
 from PySDM.physics import si
+from PySDM.physics.constants import PER_MILLE
 
 PLOT = False
 
@@ -19,15 +21,46 @@ def variables_fixture():
     )
 
 
-@pytest.mark.parametrize(
-    "level_name, expected_height", (("CB", 1 * si.km), ("0C", 2.24 * si.km))
-)
-def test_fig_4(variables, level_name, expected_height):
-    # arrange
-    tolerance = 50 * si.m
+class TestFig4:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "level_name, expected_height", (("CB", 1 * si.km), ("0C", 2.24 * si.km))
+    )
+    def test_fig_4(variables, level_name, expected_height):
+        # arrange
+        tolerance = 50 * si.m
 
-    # act
-    actual = variables["levels"][level_name] + variables["alt_initial"]
+        # act
+        actual = variables["levels"][level_name] + variables["alt_initial"]
 
-    # assert
-    assert abs(expected_height - actual) < tolerance
+        # assert
+        assert abs(expected_height - actual) < tolerance
+
+    @staticmethod
+    @pytest.mark.parametrize("R_name", ("R_Rayleigh_eql", "R_Rayleigh_kin"))
+    def test_fig_4c(variables, R_name):
+        # arrange
+        below_cloud = (
+            variables["z"] < variables["levels"]["CB"] + variables["alt_initial"]
+        )
+
+        # act
+        actual = variables[R_name][below_cloud]
+        desired = variables["R0_2H"]
+
+        # assert
+        np.testing.assert_array_almost_equal(
+            actual,
+            desired,
+        )
+
+    @staticmethod
+    def test_fig_4d(variables):
+        # arrange
+        desired = 10
+
+        # act
+        actual = variables["d_excess"] / PER_MILLE
+
+        # assert
+        np.testing.assert_array_almost_equal(actual, desired, 0.1)
