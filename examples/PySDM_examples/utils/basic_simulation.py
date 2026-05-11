23d7d426e1eecf87166bd2ff910b3ab124f68178
Kacper Mazur <tasnko12@gmail.com>
2026-05-11 16:42:23 +0200

    use base simulation in Arabas_&_Pawlowska_2011

--- examples/PySDM_examples/utils/basic_simulation.py
+++ examples/PySDM_examples/utils/basic_simulation.py
@@ -2,8 +2,11 @@ import numpy as np
 
 
 class BasicSimulation:
-    def __init__(self, particulator):
+    def __init__(self, particulator, output_attributes=None):
+        for k in output_attributes or []:
+            assert k in particulator.attributes
         self.particulator = particulator
+        self.output_attributes = output_attributes
 
     def _save(self, output):
         for k, v in self.particulator.products.items():
@@ -11,11 +14,15 @@ class BasicSimulation:
             if isinstance(value, np.ndarray) and value.shape[0] == 1:
                 value = value[0]
             output[k].append(value)
+        for k in self.output_attributes or []:
+            value = self.particulator.attributes[k].to_ndarray()
+            output[k].append(value)
 
     def _run(self, nt, steps_per_output_interval):
         output = {k: [] for k in self.particulator.products}
+        output |= {k: [] for k in self.output_attributes or []}
         self._save(output)
         for _ in range(0, nt + 1, steps_per_output_interval):
             self.particulator.run(steps=steps_per_output_interval)
             self._save(output)
-        return output
+        return {k: np.asarray(v) for k, v in output.items()}
