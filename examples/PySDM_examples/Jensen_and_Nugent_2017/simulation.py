82ac5bce3dff18ee41f0d30a92e22f7f42a2e59a
Kacper Mazur <117862683+KmazuR-afk@users.noreply.github.com>
2026-05-20 10:43:09 +0200

    New example: Arabas & Pawlowska 2011; refactoring Parcel.__init__ to support both mixing-ratio and relative-humidity initialisation (#1827)
    Co-authored-by: Sylwester Arabas <sylwester.arabas@agh.edu.pl>

--- examples/PySDM_examples/Jensen_and_Nugent_2017/simulation.py
+++ examples/PySDM_examples/Jensen_and_Nugent_2017/simulation.py
@@ -28,24 +28,20 @@ class Simulation(BasicSimulation):
         gccn: bool = False,
         gravitational_coalsecence: bool = False,
     ):
-        const = settings.formulae.constants
-        pvs_water = settings.formulae.saturation_vapour_pressure.pvs_water
-        initial_water_vapour_mixing_ratio = const.eps / (
-            settings.p0 / settings.RH0 / pvs_water(settings.T0) - 1
-        )
 
         n_gccn = np.count_nonzero(table_3.NA) if gccn else 0
 
         builder = Builder(
             n_sd=N_SD_NON_GCCN + n_gccn,
             backend=CPU(
-                formulae=settings.formulae, override_jit_flags={"parallel": False}
+                formulae=settings.formulae,
+                override_jit_flags={"parallel": False},
             ),
             environment=Parcel(
                 dt=settings.dt,
                 mass_of_dry_air=666 * si.kg,
                 p0=settings.p0,
-                initial_water_vapour_mixing_ratio=initial_water_vapour_mixing_ratio,
+                initial_relative_humidity=settings.RH0,
                 T0=settings.T0,
                 w=settings.vertical_velocity,
                 z0=settings.z0,
@@ -77,7 +73,12 @@ class Simulation(BasicSimulation):
             )  # TODO #1266: check which temp, pres, RH assumed in the paper for NA???
 
         pd0 = settings.formulae.trivia.p_d(
-            settings.p0, initial_water_vapour_mixing_ratio
+            settings.p0,
+            settings.formulae.trivia.water_vapour_mixing_ratio(
+                settings.p0,
+                settings.RH0,
+                settings.formulae.saturation_vapour_pressure.pvs_water(settings.T0),
+            ),
         )
         rhod0 = settings.formulae.state_variable_triplet.rhod_of_pd_T(pd0, settings.T0)
 
