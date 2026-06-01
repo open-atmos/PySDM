978d341e4c389af26cbfcf1207635b07e2ffa672
Tim Luettmer <tluettm@uni-mainz.de>
2026-06-01 11:50:44 +0200

    Moved length_scale calculation from register to call

--- PySDM/dynamics/sedimentation_removal_0d.py
+++ PySDM/dynamics/sedimentation_removal_0d.py
@@ -17,11 +17,11 @@ class SedimentationRemoval0D:
         builder.request_attribute("relative fall velocity")
         assert builder.particulator.environment.mesh.n_dims == 0
         self.particulator = builder.particulator
-        self.length_scale = np.cbrt(self.particulator.environment.mesh.dv)
 
     def __call__(self):
         """for stochastic removal, see, e.g., the naive scheme in Algorithm 1 in
         [Curtis et al. 2016](https://doi.org/10.1016/j.jcp.2016.06.029)"""
+        self.length_scale = np.cbrt(self.particulator.environment.mesh.dv)
         self.particulator.sedimentation_removal(
             stochastic_sedimentation_removal=self.stochastic_sedimentation_removal,
             length_scale=self.length_scale,
