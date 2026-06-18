ee28fe52bed8837333670ad39d9bd3f78292df5e
Tim Luettmer <tluettm@uni-mainz.de>
2026-06-18 17:13:19 +0200

    Updated all collisions kernel to be compatible with new kernel choice logic

--- PySDM/physics/collision_kernel_liquid_liquid/__init__.py
+++ PySDM/physics/collision_kernel_liquid_liquid/__init__.py
@@ -1,9 +1,13 @@
 """
-Collision kernels including
-[Golovin](https://open-atmos.github.io/PySDM/PySDM/dynamics/collisions/collision_kernels/golovin.html),
-[Geometric](https://open-atmos.github.io/PySDM/PySDM/dynamics/collisions/collision_kernels/geometric.html)
-and other...
-"""  # pylint: disable=line-too-long
+Collision kernels physics
+"""
 
 from .neglect import Neglect
+from .constantK import ConstantK
+from .electric import Electric
+from .geometric import Geometric
 from .golovin import Golovin
+from .hydrodynamic import Hydrodynamic
+from .linear import Linear
+from .simple_geometric import SimpleGeometric
+from .long1974 import Long1974
