4b7e44e198b9a7c33b93f208295cae48570fb05b
Konrad Bodzioch <kbodzio@student.agh.edu.pl>
2026-08-07 13:49:03 +0200

    fix thrust test errors

---
+++ tests/unit_tests/backends/storage/test_row_view.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def test_row_view(backend_instance_with_jax):
+    backend = backend_instance_with_jax
+    output = backend.Storage.from_ndarray(np.zeros((3, 16)))
+    row = output.row_view(1)
+    row.fill(5)
+
+    expected = [np.zeros(16), np.full(16, 5), np.zeros(16)]
+
+    # Assert
+    np.testing.assert_array_equal(output.to_ndarray(), expected)
