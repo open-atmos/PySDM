833d2914e016387ec870bb4d3e7c23625d814fd5
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2026-09-08 00:20:50 +0200

    few tests more into operational stage

--- tests/unit_tests/products/test_effective_radii.py
+++ tests/unit_tests/products/test_effective_radii.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from PySDM import Builder
+from PySDM import Particulator
 from PySDM.environments import Box
 from PySDM.physics import si
 from PySDM.products import EffectiveRadius, ActivatedEffectiveRadius
@@ -10,20 +10,18 @@ from PySDM.products import EffectiveRadius, ActivatedEffectiveRadius
 
 def test_effective_radii(backend_class):
     # arrange
-    env = Box(dt=np.nan, dv=np.nan)
+    backend = backend_class(double_precision=True)
+    env = Box(dt=np.nan, dv=np.nan, backend=backend)
     wet_radii = np.asarray([0.01 * si.um, 0.05 * si.um, 0.09 * si.um, 1 * si.um])
     dry_radii = np.asarray([0.009 * si.um] * len(wet_radii))
     kappa = 1.666
 
-    builder = Builder(
+    dry_volume = backend.formulae.trivia.volume(radius=dry_radii)
+    particulator = Particulator(
         n_sd=len(wet_radii),
-        backend=backend_class(double_precision=True),
         environment=env,
-    )
-    dry_volume = builder.formulae.trivia.volume(radius=dry_radii)
-    particulator = builder.build(
         attributes={
-            "water mass": builder.formulae.particle_shape_and_density.radius_to_mass(
+            "water mass": backend.formulae.particle_shape_and_density.radius_to_mass(
                 wet_radii
             ),
             "dry volume": dry_volume,
