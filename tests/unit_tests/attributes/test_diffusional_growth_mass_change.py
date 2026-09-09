eeb0f6a084f4100b095d03f90162577dc3ecb228
KmazuR-afk <tasnko12@gmail.com>
2026-09-09 20:46:11 +0200

    unit/tests/attributes done

--- tests/unit_tests/attributes/test_diffusional_growth_mass_change.py
+++ tests/unit_tests/attributes/test_diffusional_growth_mass_change.py
@@ -6,6 +6,7 @@ import pytest
 from PySDM.attributes import DiffusionalGrowthMassChange
 from PySDM.physics import si
 from PySDM.dynamics import Collision
+from attributes.physics import diffusional_growth_mass_change
 from ..dummy_particulator import DummyParticulator
 
 
@@ -16,14 +17,12 @@ class TestDiffusionalGrowthMassChange:
             pytest.skip("only Numba supported - TODO #1438")
 
         # arrange
-        particulator = DummyParticulator(backend_class)
-        particulator.request_attribute("diffusional growth mass change")
-
-        # act
-        particulator.build(
-            attributes={"multiplicity": np.ones(1), "water mass": np.ones(1)}
+        particulator = DummyParticulator(
+            backend_class,
+            n_sd=1,
+            requested_attributes=("diffusional growth mass change",),
+            attributes={"multiplicity": np.ones(1), "water mass": np.ones(1)},
         )
-
         # assert
         for items in (particulator.initialisers, particulator.observers):
             assert len(items) == 1
@@ -46,9 +45,9 @@ class TestDiffusionalGrowthMassChange:
                     fragmentation_function=np.nan,
                 ),
             ),
+            attributes={},
+            requested_attributes=("diffusional growth mass change",),
         )
-        particulator.request_attribute("diffusional growth mass change")
-        particulator.build(attributes={})
 
     @staticmethod
     @pytest.mark.parametrize("steps", (0, 1, 2))
@@ -60,13 +59,15 @@ class TestDiffusionalGrowthMassChange:
         n_sd = 1
         mass_delta = np.ones(n_sd) * si.ng
 
-        particulator = DummyParticulator(backend_class, n_sd=n_sd, formulae=None)
-        particulator.request_attribute("diffusional growth mass change")
-        particulator.build(
+        particulator = DummyParticulator(
+            backend_class,
+            n_sd=n_sd,
+            formulae=None,
+            requested_attributes=("diffusional growth mass change",),
             attributes={
                 "multiplicity": np.ones(n_sd),
                 "water mass": np.ones(n_sd) * si.ug,
-            }
+            },
         )
 
         # act
--- tests/unit_tests/dummy_particulator.py
+++ tests/unit_tests/dummy_particulator.py
@@ -8,17 +8,30 @@ from .dummy_environment import DummyEnvironment
 
 
 class DummyParticulator(Particulator):
-    def __init__(self, backend_class, n_sd=0, formulae=None, grid=None, dynamics=None):
-        Particulator.__init__(
-            self,
-            n_sd=n_sd,
-            environment=DummyEnvironment(
-                grid=grid, backend=backend_class(formulae, double_precision=True)
-            ),
-            attributes={
+    def __init__(
+        self,
+        backend_class,
+        n_sd=0,
+        formulae=None,
+        grid=None,
+        dynamics=None,
+        attributes=None,
+        requested_attributes=None,
+    ):
+        if attributes is None:
+            attributes = {
                 "multiplicity": np.ones(n_sd),
                 "signed water mass": np.full(n_sd, np.nan),
-            },
+            }
+        if requested_attributes is None:
+            requested_attributes = ("cell id",)
+        super().__init__(
+            n_sd=n_sd,
+            environment=DummyEnvironment(
+                grid=grid,
+                backend=backend_class(formulae, double_precision=True),
+            ),
+            attributes=attributes,
             dynamics=dynamics,
-            requested_attributes=("cell id",),
+            requested_attributes=requested_attributes,
         )
